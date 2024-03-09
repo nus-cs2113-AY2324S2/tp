cd070bbf802da9890e355952a4f7e84f9299d07e
itsmejr257 <chanjunrong2@gmail.com>
2024-03-09 12:12:56 +0800

    Refactor BudgetBuddy to print Welcome and Goodbye Messages

--- src/main/java/seedu/duke/BudgetBuddy.java
+++ src/main/java/seedu/duke/BudgetBuddy.java
@@ -3,19 +3,22 @@ package seedu.duke;
 import java.util.Scanner;
 
 public class BudgetBuddy {
-    /**
-     * Main entry-point for the java.duke.Duke application.
-     */
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+    private Ui ui;
+
+    public BudgetBuddy() {
+        ui = new Ui();
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        ui.showWelcome();
+
+        ui.showGoodbye();
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        new BudgetBuddy().run();
     }
 }
