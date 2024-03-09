b051b5205623baa35ea359b5090e48eff1b21053
itsmejr257 <chanjunrong2@gmail.com>
2024-03-09 12:23:50 +0800

    Refactor BudgetBuddy to use Parser

--- src/main/java/seedu/duke/BudgetBuddy.java
+++ src/main/java/seedu/duke/BudgetBuddy.java
@@ -5,14 +5,25 @@ import java.util.Scanner;
 public class BudgetBuddy {
 
     private Ui ui;
+    private Parser parser;
 
     public BudgetBuddy() {
         ui = new Ui();
+        parser = new Parser();
     }
 
     public void run() {
         Scanner scanner = new Scanner(System.in);
         ui.showWelcome();
+
+        boolean isExit = false;
+        while (!isExit) {
+            String input = scanner.nextLine();
+
+            if(parser.isExitCommand(input)) {
+                isExit = true;
+            }
+        }
 
         ui.showGoodbye();
         scanner.close();
