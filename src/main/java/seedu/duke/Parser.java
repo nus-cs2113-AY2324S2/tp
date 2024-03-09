fdb64c4958ff051fc5561e7b485ba94c45557b5b
itsmejr257 <chanjunrong2@gmail.com>
2024-03-09 13:29:34 +0800

    Refactor Parser to use Command and MenuCommand classes

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -4,17 +4,29 @@ public class Parser {
     public boolean isExitCommand(String input) {
         return input.trim().equalsIgnoreCase("bye");
     }
+    public Boolean isMenuCommand(String input) {
+        return input.startsWith("menu");
+    }
 
-    public int parseCommand(String input) {
-        if (input.startsWith("menu ")) {
-            try {
-                int index = Integer.parseInt(input.substring(5));
-                return index;
-            } catch (NumberFormatException e) {
-                return -1;
-            }
+    public Command handleMenuCommand(String input) {
+        if (input.trim().equals("menu")) {
+            return new MenuCommand(0);
         }
-        return -1;
+        try {
+            int index = Integer.parseInt(input.substring(5));
+            return new MenuCommand(index);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    public Command parseCommand(String input) {
+
+        if (isMenuCommand(input)) {
+            return handleMenuCommand(input);
+        }
+
+        return null;
     }
 
 }
