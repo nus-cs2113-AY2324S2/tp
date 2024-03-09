1686a0895224ce231c44f272444c121445fad40e
Parth Sanjay Gandhi <E0957991@u.nus.edu>
2024-03-09 12:50:44 +0800

    Add Master Class for setting up masterStudentList in future

---
+++ src/main/java/classify/student/Master.java
@@ -0,0 +1,10 @@
+package classify.student;
+
+//This class is a master arraylist for all the students in our system. to be explored ig
+public class Master {
+    public static StudentList masterStudentList = new StudentList();
+
+    public Master() {
+
+    }
+}
