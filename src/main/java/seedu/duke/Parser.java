e6cebec7005548cdcfc7c33b35fdcbca5301d5a4
itsmejr257 <chanjunrong2@gmail.com>
2024-03-09 12:28:45 +0800

    Add parseCommand function for menu command

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -5,4 +5,16 @@ public class Parser {
         return input.trim().equalsIgnoreCase("bye");
     }
 
+    public int parseCommand(String input) {
+        if (input.startsWith("menu ")) {
+            try {
+                int index = Integer.parseInt(input.substring(5));
+                return index;
+            } catch (NumberFormatException e) {
+                return -1;
+            }
+        }
+        return -1;
+    }
+
 }
