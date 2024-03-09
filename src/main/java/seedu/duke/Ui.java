72433cf3ad6a51e37e8026929d916af2b41fe02f
itsmejr257 <chanjunrong2@gmail.com>
2024-03-09 12:12:10 +0800

    Add functions for Welcome and Goodbye messages

---
+++ src/main/java/seedu/duke/Ui.java
@@ -0,0 +1,23 @@
+package seedu.duke;
+
+public class Ui {
+    private static final String DIVIDER = "__________________________________________________";
+
+    public void showWelcome() {
+        System.out.println(DIVIDER);
+        System.out.println("                     BudgetBuddy");
+        System.out.println(DIVIDER);
+        System.out.println("Welcome to BudgetBuddy, to start, please type \"menu\" [list number] " +
+                "to view commands for the respective functions");
+        System.out.println("To view all menu items again, type \"menu\".");
+        System.out.println(DIVIDER);
+        System.out.println("1. Manage Expenses     3. View Expenses");
+        System.out.println("2. Manage Savings      4. View Savings");
+        System.out.println(DIVIDER);
+    }
+
+    public void showGoodbye() {
+        System.out.println("Goodbye! Thank you for using BudgetBuddy.");
+    }
+
+}
