88e9212b0d877388471e386b5a8f019527b8cb38
SuveenE <suveen.te1@gmail.com>
2024-03-10 13:36:46 +0800

    Update testing files

--- src/main/java/ActiveEdge/ActiveEdge.java
+++ src/main/java/ActiveEdge/ActiveEdge.java
@@ -7,8 +7,8 @@ public class ActiveEdge {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ACTIVE EDGE! ";
-        System.out.println("Hello from\n" + logo + "AI assistant!");
+        String logo = "ACTIVE EDGE";
+        System.out.println("Hello from\n" + logo + " AI assistant!");
         System.out.println("How can I help you today?");
 
         Scanner in = new Scanner(System.in);
