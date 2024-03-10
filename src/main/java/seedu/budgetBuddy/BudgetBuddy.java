02791364cced2a570b2f622395e687b1f2809206
Zhang Yangda <zhang.yangda16@gmail.com>
2024-03-10 19:31:11 +0800

    fix IO performance test

--- src/main/java/seedu/budgetBuddy/BudgetBuddy.java
+++ src/main/java/seedu/budgetBuddy/BudgetBuddy.java
@@ -1,19 +1,19 @@
 package seedu.budgetBuddy;
 
-import seedu.exception.budgetBuddyException;
 public class BudgetBuddy {
 
     private static final Ui ui = new Ui();
-    public static void main(String[] args) throws budgetBuddyException{
+    public static void main(String[] args){
         ui.greet();
         String input = ui.readCommand();
         while (true) {
-            Parser parser = new Parser();
-            input = ui.readCommand();
-            parser.parseInput(input);
-            ui.showAdd(input);
             if (input.equals("bye")) {
                 break;
+            } else{ 
+                Parser parser = new Parser();
+                input = ui.readCommand();
+                parser.parseInput(input);
+                ui.showAdd(input);
             }
         }
         ui.showGoodBye();
