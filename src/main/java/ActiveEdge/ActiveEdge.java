25dab1daec6a10f0333fdd98ded7dd4de30bac86
SuveenE <suveen.te1@gmail.com>
2024-03-10 13:27:38 +0800

    Setup initial folder structure

---
+++ src/main/java/ActiveEdge/ActiveEdge.java
@@ -0,0 +1,17 @@
+package ActiveEdge;
+
+import java.util.Scanner;
+
+public class ActiveEdge {
+    /**
+     * Main entry-point for the java.duke.Duke application.
+     */
+    public static void main(String[] args) {
+        String logo = " ACTIVE EDGE! ";
+        System.out.println("Hello from\n" + logo + "AI assistant!");
+        System.out.println("How can I help you today?");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello " + in.nextLine());
+    }
+}
--- src/test/java/ActiveEdge/DukeTest.java
+++ src/test/java/ActiveEdge/DukeTest.java
@@ -1,4 +1,4 @@
-package seedu.duke;
+package ActiveEdge;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
