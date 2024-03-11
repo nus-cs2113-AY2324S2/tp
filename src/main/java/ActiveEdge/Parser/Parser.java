c434fee333c9b0e75ef08c542d5db8663a09f810
wong quan wei alvin <wongquan.weialvin@gmail.com>
2024-03-11 18:29:24 +0800

    no message

--- src/main/java/ActiveEdge/Parser/Parser.java
+++ src/main/java/ActiveEdge/Parser/Parser.java
@@ -1,7 +1,5 @@
 package ActiveEdge.Parser;
 
-
-import ActiveEdge.Command.DailyCaloriesCommand;
 import ActiveEdge.Command.ActiveEdgeException;
 import ActiveEdge.Command.HelpCommand;
 import ActiveEdge.Command.LogWaterCommand;
@@ -23,7 +21,7 @@ public class Parser {
             String[] parts = input.substring(5).split(" ");
             String inputTrimmed = parts[1].trim();
             if (inputTrimmed.startsWith("c")) { //shows calorie
-                new DailyCaloriesCommand(input);
+
             } else if (inputTrimmed.startsWith("w")) { //shows water
 
             } else if (inputTrimmed.startsWith("g")) { //shows goals
