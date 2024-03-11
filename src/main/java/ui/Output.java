8f4a918415351b2387f369d9f348ed7ff4139164
l5_z <e0958707@u.nus.edu>
2024-03-11 13:30:05 +0800

    Update with print ouputs

--- src/main/java/ui/Output.java
+++ src/main/java/ui/Output.java
@@ -1,4 +1,30 @@
 package ui;
 
+import utility.Constant;
+
 public class Output {
+    /**
+     * Prints a horizontal line.
+     */
+    public static void printLine() {
+        System.out.println(Constant.PARTITION_LINE);
+    }
+
+    /**
+     * Prints the help message.
+     */
+    public static void printHelp() {
+        printLine();
+        System.out.println("Commands List:" + "\n");
+        System.out.println("list - prints out the List");
+        System.out.println("help - procures command list");
+        System.out.println("exit - terminates the bot");
+        printLine();
+        System.out.println("bmi - calculates BMI");
+        System.out.println("weight - save current weight");
+        System.out.println("height - save current height");
+        printLine();
+        System.out.println("bmi format: bmi *parameter*");
+        printLine();
+    }
 }
