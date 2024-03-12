8996c4f01d056b5cb9caeb7df864a81289fea15f
Isaac Eng <>
2024-03-12 21:54:22 +0800

    Add ArrayList into InputHandler

--- src/main/java/financemanager/InputHandler.java
+++ src/main/java/financemanager/InputHandler.java
@@ -1,4 +1,20 @@
 package financemanager;
 
+import transactions.Transaction;
+
+import java.util.ArrayList;
+
 public class InputHandler {
+
+    private ArrayList<Transaction> transactions;
+
+    public InputHandler() {
+        // Initialise ArrayList in the constructor
+        this.transactions = new ArrayList<>();
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+
 }
