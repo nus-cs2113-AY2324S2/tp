15ab9d7e2ff62cb34f21b1d159e3bb131a1cd79a
PureUsagi <ngjunhancanoe@gmail.com>
2024-03-12 19:04:06 +0800

    Changed Constant COMMAND_WORD to COMMAND_STRING.

--- src/main/java/seedu/binbash/command/DeleteCommand.java
+++ src/main/java/seedu/binbash/command/DeleteCommand.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 import seedu.binbash.ItemList;
 
 public class DeleteCommand extends Command {
-    public static final String COMMAND_WORD = "delete";
+    public static final String COMMAND_STRING = "delete";
     public static final Pattern COMMAND_FORMAT =
             Pattern.compile("delete\\s(?<index>\\d+)");
     protected int index;
