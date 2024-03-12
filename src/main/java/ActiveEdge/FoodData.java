f23ff527fab5376e76f5094c5a5cb94f22026795
wong quan wei alvin <wongquan.weialvin@gmail.com>
2024-03-12 13:11:23 +0800

    no message

---
+++ src/main/java/ActiveEdge/FoodData.java
@@ -0,0 +1,18 @@
+package ActiveEdge;
+
+public class FoodData {
+
+    public static void main(String[] args) {
+        // Define the data for each food item
+        //category, food name, calories
+        String[][] foodItems = {
+                {"western", "fried chicken", "543"},
+                {"western", "chicken burger", "357"},
+                {"japanese", "katsu don", "700"},
+                {"japanese", "sushi", "350"},
+                {"korean", "kimchi", "25"},
+                {"indian", "rasam", "151"}
+                //praneet pls add the rest
+        };
+    }
+}
