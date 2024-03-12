735b0c96161eab214a76de9756ffdb60e9454cc9
DavidTay <davidtaywd@gmail.com>
2024-03-12 22:06:51 +0800

    Add Update quantity and Price feature
    refactored new command code a bit as there was quite a bit of shared code in the parser

---
+++ src/main/java/supertracker/command/UpdateCommand.java
@@ -0,0 +1,36 @@
+package supertracker.command;
+
+import supertracker.Ui;
+import supertracker.item.Inventory;
+import supertracker.item.Item;
+
+public class UpdateCommand implements Command {
+    private String name;
+    private int newQuantity;
+    private double newPrice;
+
+    public UpdateCommand(String name, int newQuantity, double newPrice) {
+        this.name = name;
+        this.newQuantity = newQuantity;
+        this.newPrice = newPrice;
+    }
+
+    @Override
+    public void execute() {
+        Item oldItem = Inventory.get(name);
+        if (newQuantity == 0) {
+            newQuantity = oldItem.getQuantity();
+        }
+        if (newPrice == 0) {
+            newPrice = oldItem.getPrice();
+        }
+        Item newItem = new Item(name, newQuantity, newPrice);
+        Inventory.put(name, newItem);
+        Ui.updateCommandSuccess(newItem);
+    }
+
+    @Override
+    public boolean isQuit() {
+        return false;
+    }
+}
