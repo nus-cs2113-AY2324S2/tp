fd8ebd629c0d333c43bcb2de599d77a015bd463e
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-03-13 10:24:59 +0800

    Add parseTransaction method in Parser class

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -1,4 +1,27 @@
 package parser;
 
+import transactions.Transaction;
+
 public class Parser {
+
+    public static final int ADD_COMMAND_INDEX = 3;
+
+    public Transaction parseTransaction(String input) {
+        String data = input.substring(ADD_COMMAND_INDEX + 1);
+        String[] parseData = data.split("/");
+        String description = null, date = null, amount = null, category = null;
+        for(int i = 0; i < parseData.length; i++) {
+            if (parseData[i].trim() == "n"){
+                description = parseData[i + 1];
+            } else if (parseData[i].trim() == "$") {
+                amount = parseData[i + 1];
+            } else if (parseData[i].trim() == "d") {
+                date = parseData[i + 1];
+            } else if (parseData[i].trim() == "c") {
+                category = parseData[i + 1];
+            }
+        }
+        assert amount != null;
+        return new Transaction(description, Float.parseFloat(amount), date, category);
+    }
 }
