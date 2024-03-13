ce5a255ca8677bc90de49d9bea2fe8c02b0f219b
Vavinan <vavithamarai2000@gmail.com>
2024-03-13 12:53:24 +0800

    Add: Print Delete message

--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -1,4 +1,13 @@
 package ui;
 
 public class UserInterface {
+
+    public static void printDeleteMessage(String transaction){
+        System.out.println("Got it. I have removed the following transaction from the history");
+        System.out.println(transaction);
+    }
+
+    public static void printIndexOutOfBounds(String message){
+        System.out.println(message + ". Check you input properly");
+    }
 }
