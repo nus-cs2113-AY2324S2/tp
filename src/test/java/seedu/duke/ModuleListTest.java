0d8acc3a09229a0f61d695c4f9b7f555fe612700
Thaw Tun Zan <110762796+ThawTunZan@users.noreply.github.com>
2024-03-13 16:50:34 +0800

    Add JUnitTest for the moduleList class

---
+++ src/test/java/seedu/duke/ModuleListTest.java
@@ -0,0 +1,10 @@
+package seedu.duke;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ModuleListTest {
+    public void moduleListTest() {
+        assertTrue(true);
+    }
+}
+
