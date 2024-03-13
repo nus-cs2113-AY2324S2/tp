29873156a210e3cf814f7596a8a2716a8bf78710
jensonjenkins <dave.martheen@gmail.com>
2024-03-13 16:48:22 +0800

    add JUnitTest

---
+++ src/test/java/seedu/duke/command/CommandTest.java
@@ -0,0 +1,12 @@
+package seedu.duke.command;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CommandTest {
+    @Test
+    public void commandTest() {
+        assertTrue(true);
+    }
+}
