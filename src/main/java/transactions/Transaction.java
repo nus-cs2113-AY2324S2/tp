d1ad33c2ec8df2cb902fba782d6e99bd3c07e6fd
Vavinan <vavithamarai2000@gmail.com>
2024-03-13 15:18:03 +0800

    Add date & override toString in Transaction class

--- src/main/java/transactions/Transaction.java
+++ src/main/java/transactions/Transaction.java
@@ -1,14 +1,19 @@
 package transactions;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Transaction {
     private String description;
     private float amount;
     private String category;
+    private LocalDate date;
 
-    public Transaction(String description, float amount, String category) {
+    public Transaction(String description, float amount, String category,String date) {
         this.description = description;
         this.amount = amount;
         this.category = category;
+        this.date = parseDate(date);
     }
 
     public String getDescription() {
@@ -19,7 +24,22 @@ public class Transaction {
         return amount;
     }
 
+    public LocalDate getDate() {
+        return date;
+    }
+
+    private LocalDate parseDate(String by) {
+        return LocalDate.parse(by, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    }
     public String getCategory() {
         return category;
+    }
+
+    @Override
+    public String toString() {
+        return  (" Description: " + getDescription() + " | " +
+                " Date: " + getDate() + " | " +
+                " Amount: " + getAmount() + " | " +
+                " Category: " + getCategory()) ;
     }
 }
