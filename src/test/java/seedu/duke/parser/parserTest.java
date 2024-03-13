d09cde0cc6dbad5da907ef47c7be9f4e71bf7da0
sxfoo <fsx159@gmail.com>
2024-03-13 16:50:13 +0800

    Add parserTest skeleton

---
+++ src/test/java/seedu/duke/parser/parserTest.java
@@ -0,0 +1,10 @@
+package seedu.duke.parser;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class parserTest {
+    @Test
+    public void parserTestSample() {
+        assertTrue(true);
+    }
+}
