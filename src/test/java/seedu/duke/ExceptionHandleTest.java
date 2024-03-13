c271dfad1d8d350ea173a73bc9eafd13fba909dc
Tan Dexter <dextboy@outlook.com>
2024-03-13 23:19:28 +0800

    Add exceptionHandleTest method

--- src/test/java/seedu/duke/ExceptionHandleTest.java
+++ src/test/java/seedu/duke/ExceptionHandleTest.java
@@ -1,2 +1,12 @@
-package seedu.duke;public class ExceptionHandleTest {
+package seedu.duke;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ExceptionHandleTest {
+    @Test
+    public void exceptionHandleTest() {
+        assertTrue(true);
+    }
 }
