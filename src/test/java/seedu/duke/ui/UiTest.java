113689eb77a7dbb1caaf468ee0d9c14ceb11af4a
fungg0 <fungfunwenwu@gmail.com>
2024-03-13 17:04:50 +0800

    Add Junit test for UiTest

---
+++ src/test/java/seedu/duke/ui/UiTest.java
@@ -0,0 +1,11 @@
+package seedu.duke.ui;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UiTest {
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
