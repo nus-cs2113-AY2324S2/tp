b5a2c917cc5b1f5d4d1dd7b89470d52a81054a34
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-03-13 10:52:25 +0800

    Add processTransaction method in TransactionList class

--- src/main/java/financemanager/TransactionList.java
+++ src/main/java/financemanager/TransactionList.java
@@ -1,5 +1,6 @@
 package financemanager;
 
+import parser.Parser;
 import transactions.Transaction;
 
 import java.util.ArrayList;
@@ -7,10 +8,12 @@ import java.util.ArrayList;
 public class TransactionList {
 
     private ArrayList<Transaction> transactions;
+    private Parser parser;
 
     public TransactionList() {
         // Initialise ArrayList in the constructor
         this.transactions = new ArrayList<>();
+        this.parser = new Parser();
     }
 
     public ArrayList<Transaction> getTransactions() {
@@ -19,5 +22,9 @@ public class TransactionList {
 
     private void addTransaction(Transaction t) {
         transactions.add(t);
+    }
+
+    public void processTransaction(String input) {
+        addTransaction(parser.parseTransaction(input));
     }
 }
