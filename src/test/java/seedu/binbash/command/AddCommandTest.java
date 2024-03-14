1f3475cb525d942c62c6f048a5c8764f3e3ed109
imanamirshah <iman.amirshah@gmail.com>
2024-03-14 15:24:12 +0800

    Add JUnit test for execute method in AddCommand class

---
+++ src/test/java/seedu/binbash/command/AddCommandTest.java
@@ -0,0 +1,18 @@
+package seedu.binbash.command;
+
+import org.junit.jupiter.api.Test;
+import seedu.binbash.Item;
+import seedu.binbash.ItemList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AddCommandTest {
+
+    @Test
+    void execute_item_oneItemInItemList() {
+        ItemList itemList = new ItemList();
+
+        itemList.addItem("testItem", "A test item");
+        assertEquals(1, itemList.getItemCount());
+    }
+}
