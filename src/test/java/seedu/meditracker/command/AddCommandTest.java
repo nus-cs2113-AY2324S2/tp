7ac826ac1993554300d77a23c06e62ad6d25000f
nickczh <nickchua@u.nus.edu>
2024-03-14 22:46:51 +0800

    chore: Add JUnit Test for AddCommand

---
+++ src/test/java/seedu/meditracker/command/AddCommandTest.java
@@ -0,0 +1,26 @@
+package seedu.meditracker.command;
+
+import org.junit.jupiter.api.Test;
+import seedu.meditracker.exception.MediTrackerException;
+import seedu.meditracker.medication.MedicationList;
+import seedu.meditracker.ui.Ui;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AddCommandTest {
+
+    // 3 part format
+    // methodBeingTested_conditionToTest_expectedOutcome
+    @Test
+    void execute_addCommand_expectOneMedication() throws MediTrackerException {
+        // setup lines
+        String inputString = "add -n Medication_A -q 60_TAB -d 500mg -e 01/07/25 -f morning -r cause_dizziness";
+        Ui ui = new Ui();
+        MedicationList medicationList = new MedicationList();
+        AddCommand command = new AddCommand(inputString);
+        command.execute(medicationList, ui);
+
+        // actual test
+        assertEquals(1, medicationList.medications.size());
+    }
+}
