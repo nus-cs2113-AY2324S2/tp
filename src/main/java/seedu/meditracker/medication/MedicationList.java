1007bbf560d84533c267addf2f45586633935bc4
nickczh <nickchua@u.nus.edu>
2024-03-14 22:31:25 +0800

    feat: Add Medication and MedicationList class
    Let's,
    
    * Create a Medication class to represent a medicine (Note: Attributes
      are intentionally declared String type first and will be updated
    subsequently).
    
    * Create a MedicationList class to represent a list of medication.

---
+++ src/main/java/seedu/meditracker/medication/MedicationList.java
@@ -0,0 +1,26 @@
+package seedu.meditracker.medication;
+
+import java.util.ArrayList;
+
+/**
+ * The MedicationList class represents a list of medications.
+ * It contains an ArrayList of Medication objects.
+ */
+public class MedicationList {
+    /** The list of medications stored in an ArrayList. */
+    public ArrayList<Medication> medications = new ArrayList<Medication>();
+
+    /**
+     * Constructs an empty MedicationList.
+     */
+    public MedicationList() {
+    }
+
+    /**
+     * Constructs a MedicationList with the specified list of medications.
+     * @param medications The list of medications to be stored.
+     */
+    public MedicationList(ArrayList<Medication> medications) {
+        this.medications = medications;
+    }
+}
