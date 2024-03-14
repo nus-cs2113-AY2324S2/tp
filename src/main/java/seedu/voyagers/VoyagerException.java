7e8253775f915d164393a6561bbd08611d6db100
jthibault03 <98941496+jthibault03@users.noreply.github.com>
2024-03-14 00:46:14 -0700

    Added Currency, Profile, and VoyagerException class

---
+++ src/main/java/seedu/voyagers/VoyagerException.java
@@ -0,0 +1,9 @@
+package seedu.voyagers;
+/**
+ * Generic exception class for Voyagers.
+ */
+public class VoyagerException extends Exception {
+    public void printExceptionMessage() {
+        System.out.println("An error occurred");
+    }
+}
