91bddc11148b4fb095ac5fc869ee27400b624e6d
V4vern <28131050+V4Vern@users.noreply.github.com>
2024-03-14 18:05:38 +0800

    Add ExerciseManager class

---
+++ src/main/java/byteceps/exercises/ExerciseManager.java
@@ -0,0 +1,37 @@
+package byteceps.exercises;
+
+import java.util.ArrayList;
+
+public class ExerciseManager {
+    private static ExerciseManager instance;
+    private final ArrayList<Exercise> exercises;
+
+    public ExerciseManager() {
+        this.exercises = new ArrayList<>();
+    }
+
+    public static ExerciseManager getInstance() {
+        if (instance == null) {
+            instance = new ExerciseManager();
+        }
+        return instance;
+    }
+
+    public void addExercise(String exerciseName) {
+        Exercise exercise = new Exercise(exerciseName);
+        exercises.add(exercise);
+    }
+
+    public void deleteExercise(String exerciseName) {
+        for (Exercise exercise : exercises) {
+            if (exercise.exerciseName.equals(exerciseName)) {
+                exercises.remove(exercise);
+                return;
+            }
+        }
+    }
+
+    public ArrayList<Exercise> getAllExercises() {
+        return new ArrayList<>(exercises);
+    }
+}
