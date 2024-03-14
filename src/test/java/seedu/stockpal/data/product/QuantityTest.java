b7234fd75530b19c8ca4a23bdbeeadb63b58118e
leongxingyu <leongxingyu02@gmail.com>
2024-03-14 13:49:52 +0800

    Implement Unit testing

--- src/test/java/seedu/stockpal/data/product/QuantityTest.java
+++ src/test/java/seedu/stockpal/data/product/QuantityTest.java
@@ -1,2 +1,43 @@
-package seedu.stockpal.data.product;public class QuantityTest {
+package seedu.stockpal.data.product;
+
+import org.junit.jupiter.api.Test;
+import seedu.stockpal.exceptions.InsufficientAmountException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuantityTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void updateIncreaseQuantity_anyInteger_success() {
+        Quantity quantityObject = new Quantity(0);
+        quantityObject.updateIncreaseQuantity(10);
+        assertEquals(10, quantityObject.quantity);
+    }
+
+    @Test
+    public void updateDecreaseQuantity_anyInteger_success() {
+        Quantity quantityObject = new Quantity(10);
+        try {
+            quantityObject.updateDecreaseQuantity(5);
+        } catch (InsufficientAmountException e) {
+            fail();
+        }
+        assertEquals(5, quantityObject.quantity);
+    }
+
+    @Test
+    public void updateDecreaseQuantity_anyInteger_InsufficientAmountExceptionThrown() {
+        Quantity quantityObject = new Quantity(10);
+        try {
+            quantityObject.updateDecreaseQuantity(15);
+            fail("Expected InsufficientAmountException was not thrown");
+        } catch (InsufficientAmountException e) {
+            assertEquals("Insufficient amount in inventory", e.getMessage());
+        }
+        assertEquals(10, quantityObject.quantity);
+    }
 }
