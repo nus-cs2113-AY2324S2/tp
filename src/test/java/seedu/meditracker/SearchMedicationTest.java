b08e2df9d2559ee9aeeefa5094a022c4f38984d9
kyuichyi <chongkaitlyn@u.nus.edu>
2024-03-14 15:57:07 +0800

    Created class for testing searching medication from external database.

---
+++ src/test/java/seedu/meditracker/SearchMedicationTest.java
@@ -0,0 +1,12 @@
+package seedu.meditracker;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class SearchMedicationTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
