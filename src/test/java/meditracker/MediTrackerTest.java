7ff59f23259b5a3662bd872766f45d48d141accc
annoy-o-mus <55198236+annoy-o-mus@users.noreply.github.com>
2024-03-14 22:57:55 +0800

    chore: Simplify package name

--- src/test/java/meditracker/MediTrackerTest.java
+++ src/test/java/meditracker/MediTrackerTest.java
@@ -1,10 +1,10 @@
-package seedu.meditracker;
+package meditracker;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-class SearchMedicationTest {
+class MediTrackerTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
