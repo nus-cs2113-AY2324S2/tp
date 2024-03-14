197143c1600d31fc46ebfe8dc5e220d72c042b4f
a-wild-chocolate <69574098+a-wild-chocolate@users.noreply.github.com>
2024-03-14 22:55:21 +0800

    Fix gradle issue

--- src/test/java/seedu/lifetrack/DukeTest.java
+++ src/test/java/seedu/lifetrack/DukeTest.java
@@ -16,23 +16,23 @@ class DukeTest {
     }
 
     @Test
-    public void testDeleteCalorie_ValidIndex() {
+    public void testDeleteCalorieValidIndex() {
         CalorieList calorieList = new CalorieList();
-        CalorieList.calorieArrayList.add(new Entry(new Activity("2024-03-14","12:00", "Running"),new Calorie(200,true)));
+        CalorieList.calorieArrayList.add(new Entry(new Activity("2024-03-14","12:00", "Run"),new Calorie(200,true)));
         int initialSize = CalorieList.calorieArrayList.size();
         calorieList.deleteCalorie(1);
         assertEquals(initialSize - 1, CalorieList.calorieArrayList.size());
-        CalorieList.calorieArrayList.add(new Entry(new Activity("2024-03-14","12:00", "Running"),new Calorie(200,true)));
-        CalorieList.calorieArrayList.add(new Entry(new Activity("2024-03-14","13:00", "Eating"),new Calorie(200,false)));
+        CalorieList.calorieArrayList.add(new Entry(new Activity("2024-03-14","12:00", "Run"),new Calorie(200,true)));
+        CalorieList.calorieArrayList.add(new Entry(new Activity("2024-03-14","13:00", "Eat"),new Calorie(200,false)));
         initialSize = CalorieList.calorieArrayList.size();
         calorieList.deleteCalorie(2);
         assertEquals(initialSize - 1, CalorieList.calorieArrayList.size());
     }
 
     @Test
-    public void testDeleteCalorie_InvalidIndex() {
+    public void testDeleteCalorieInvalidIndex() {
         CalorieList calorieList = new CalorieList();
-        CalorieList.calorieArrayList.add(new Entry(new Activity("2024-03-14","12:00", "Running"),new Calorie(200,true)));
+        CalorieList.calorieArrayList.add(new Entry(new Activity("2024-03-14","12:00", "Run"),new Calorie(200,true)));
         int initialSize = CalorieList.calorieArrayList.size();
         calorieList.deleteCalorie(2); // Index out of bounds
         calorieList.deleteCalorie(-1);
