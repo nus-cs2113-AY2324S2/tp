78158dc290d075011ffaf99d286757088fe425fd
chenxk619 <chenxk619@gmail.com>
2024-03-15 02:48:20 +0800

    Added storage and storageTest

---
+++ src/test/java/storage/storageTest.java
@@ -0,0 +1,31 @@
+package storage;
+import org.junit.jupiter.api.Test;
+import recipeio.recipe.RecipeList;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class storageTest {
+    public static final String FILE_PATH = "recipe.txt";
+    @Test
+    public void testExist(){
+        File f = new File(FILE_PATH);
+        if (f.exists()) {
+            assertTrue(true);
+        } else {
+            fail();
+        }
+    }
+
+    @Test
+    public void testSaving(){
+        RecipeList testRecipeList = new RecipeList();
+        try {
+            Storage.loadFile(testRecipeList);
+            assertTrue(true);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+}
