d5629f3bd96d5aced218cd8fdb32162dc62908aa
LAPTOP-D31L41D6\elyovs <elyovsun@gmail.com>
2024-03-14 20:37:51 +0800

    Add help message as a function

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -18,4 +18,19 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         System.out.println("Hello " + in.nextLine());
     }
+
+    public void helpPrintMessage() {
+        System.out.println("These are the current available features and the format that you need");
+        System.out.println("to follow to use it in using this software:");
+        System.out.println("1. To add books");
+        System.out.println("   add NAME_OF_BOOK");
+        System.out.println("2. To delete current existing book");
+        System.out.println("   delete NUMBER_ACCORDING_TO_LIST");
+        System.out.println("3. To mark book as borrowed");
+        System.out.println("   borrow NAME_OF_BOOK");
+        System.out.println("4. To unmark book as returned");
+        System.out.println("   return NAME_OF_BOOK");
+        System.out.println("5. To list all the books added");
+        System.out.println("   list");
+    }
 }
