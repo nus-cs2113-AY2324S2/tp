a18c37c2bec1836eb2ec7ad89050d21540f02573
Isaac Eng <>
2024-03-15 00:21:16 +0800

    Remove unused import

--- src/test/java/financemanager/TransactionListTest.java
+++ src/test/java/financemanager/TransactionListTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import transactions.Transaction;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TransactionListTest {
 
