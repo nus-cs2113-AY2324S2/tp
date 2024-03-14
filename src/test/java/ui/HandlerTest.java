f03a9d58795ac4058a5e8ffa3012f273e3a195d9
l5_z <e0958707@u.nus.edu>
2024-03-14 20:43:05 +0800

    Fix for gradle build error

--- src/test/java/ui/HandlerTest.java
+++ src/test/java/ui/HandlerTest.java
@@ -28,31 +28,6 @@ class HandlerTest {
         System.setOut(originalOut);
     }
 
-    /*
-    @Test
-    void handleExercise_validInputRun_expectRunAdded() throws CustomExceptions.InvalidInput {
-        // Test Setup
-        String input = "new /e:run /d:10.3 /t:00:40:10 /date:15/03/2024";
-
-        // Exercise
-        Handler.handleExercise(input);
-
-        // Verify
-        String expected = "Added: run | 10.3 | 00:40:10 | 2024-03-15\n";
-        assertEquals(expected, outContent.toString());
-    }
-
-    @Test
-    void handleExercise_invalidInputMissingParameter_expectException() {
-        // Test Setup
-        String input = "new /e:run /d:10.3 /t:00:40:10"; // Missing /date parameter
-
-        // Exercise and Verify
-        assertThrows(CustomExceptions.InvalidInput.class, () -> Handler.handleExercise(input));
-    }
-
-     */
-
     @Test
     void getRun_validInput_expectCorrectParsing() throws CustomExceptions.InvalidInput {
         // Test Setup
@@ -73,61 +48,4 @@ class HandlerTest {
         // Exercise and Verify
         assertThrows(CustomExceptions.InvalidInput.class, () -> Handler.getRun(input));
     }
-
-    /*
-    @Test
-    void handleNew_validInputRun_expectRunAdded() throws CustomExceptions.InvalidInput {
-        // Test Setup
-        String input = "new /e:run /d:10.3 /t:00:40:10 /date:15/03/2024";
-
-        // Exercise
-        Handler.handleNew(input);
-
-        // Verify
-        String expected = "Added: run | 10.3 | 00:40:10 | 2024-03-15\n";
-        assertEquals(expected, outContent.toString());
-    }
-
-
-    @Test
-    void handleHistory_allFilter_expectAllWorkoutsPrinted() {
-        // Test Setup
-        new Run("40:10", "10.3", "15/03/2024");
-        new Run("01:59:10", "15.3");
-
-        // Exercise
-        Handler.handleHistory("history all");
-
-        // Verify
-        // Add the expected output string here
-        String expected = "..."; // Implement the expected output
-        assertEquals(expected, outContent.toString());
-    }
-
-    @Test
-    void handleLatest_oneRunInList_expectLatestRunPrinted() {
-        // Test Setup
-        new Run("40:10", "10.3", "15/03/2024");
-
-        // Exercise
-        Handler.handleLatest("latest");
-
-        // Verify
-        // Add the expected output string here
-        String expected = "..."; // Implement the expected output
-        assertEquals(expected, outContent.toString());
-    }
-
-    @Test
-    void handleHelp_noInput_expectHelpMessagePrinted() {
-        // Exercise
-        Handler.handleHelp("");
-
-        // Verify
-        // Add the expected output string here
-        String expected = "..."; // Implement the expected help message
-        assertEquals(expected, outContent.toString());
-    }
-
-     */
 }
