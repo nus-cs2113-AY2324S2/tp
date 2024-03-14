15f3f6202afcacef0b49386192c88017c304db99
yzs <yeozhishen@gmail.com>
2024-03-14 23:57:56 +0800

    Add Exception Handling in Logic Class Added general exception handling in the run method in Logic as a temporary catchall. This is to be changed later when the individual classes are more adept at handling errors

--- src/main/java/seedu/brokeculator/Logic.java
+++ src/main/java/seedu/brokeculator/Logic.java
@@ -3,11 +3,20 @@ package seedu.brokeculator;
 public class Logic {
     public Logic() {
         GeneralInputParser mainParser = new GeneralInputParser();
+        FileManager fileManager = new FileManager();
     }
     public void run() {
         while (true) {
-            Command command = mainParser.getCommandFromUserInput();
-            command.execute();
+            try {
+                Command command = mainParser.getCommandFromUserInput();
+                command.execute();
+                fileMager.save();
+            } catch (Exception e) {
+                UI.print("error occured, sus. " + e.getMessage());
+            }
         }
+    }
+    private void loadTasksFromFile() {
+
     }
 }
