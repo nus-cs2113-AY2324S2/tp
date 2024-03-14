4d96b490e9f5a192e70fd3cb6330411204178b53
cyhjason29 <jackplok29@gmail.com>
2024-03-14 22:40:42 +0800

    Add support to record results from all rounds

--- src/main/java/seedu/duke/ResultsList.java
+++ src/main/java/seedu/duke/ResultsList.java
@@ -1,4 +1,24 @@
 package seedu.duke;
 
+import java.util.ArrayList;
+
 public class ResultsList {
+    protected static ArrayList<Results> sessionResults;
+
+    public ResultsList() {
+        sessionResults = new ArrayList<>();
+    }
+
+    public static void addResult(Results roundResults) {
+        sessionResults.add(roundResults);
+    }
+    
+    public static Results getSpecifiedResult(int index) {
+        return sessionResults.get(index);
+    }
+
+    public static ArrayList<Results> getAllResults() {
+        return sessionResults;
+    }
 }
+
