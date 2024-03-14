41a5fa7c5d0c58657dad1a2024d231e0c524e0dd
nur-haziq <nhaziq01@gmail.com>
2024-03-14 22:17:05 +0800

    feat: Add ability to add items into the list

--- src/main/java/seedu/binbash/Parser.java
+++ src/main/java/seedu/binbash/Parser.java
@@ -2,10 +2,9 @@ package seedu.binbash;
 
 import java.util.regex.Matcher;
 
-import seedu.binbash.command.ByeCommand;
-import seedu.binbash.command.Command;
-import seedu.binbash.command.DeleteCommand;
-import seedu.binbash.command.ListCommand;
+import seedu.binbash.command.*;
+
+import javax.xml.namespace.QName;
 
 public class Parser {
     private final ItemList itemList;
@@ -20,10 +19,12 @@ public class Parser {
         String arguments = tokens.length > 1 ? tokens[1] : "";
 
         switch (commandString) {
+        case "add":
+            return parseAddCommand(userInput);
         case "delete":
-            return parseDeleteCommand(arguments);
+            return parseDeleteCommand(userInput);
         case "list":
-            return parseListCommand(arguments);
+            return parseListCommand(userInput);
         default:
             return new ByeCommand(itemList);
         }
@@ -34,6 +35,17 @@ public class Parser {
         if (matcher.matches()) {
             int index = Integer.parseInt(matcher.group("index"));
             return new DeleteCommand(itemList, index);
+        } else {
+            return null;
+        }
+    }
+
+    private Command parseAddCommand(String arguments) {
+        Matcher matcher = AddCommand.COMMAND_FORMAT.matcher(arguments);
+        if (matcher.matches()) {
+            String itemName = matcher.group("itemName");
+            String itemDescription = matcher.group("itemDescription");
+            return new AddCommand(itemList, itemName, itemDescription);
         } else {
             return null;
         }
