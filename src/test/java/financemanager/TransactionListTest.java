325180dbb671de51e89c395dbe4514aba7b7e6b9
vibes-863 <vaibhavdpillai863@outlook.com>
2024-03-15 00:05:30 +0800

    Add removeTransaction IndexOutOfBounds test case

--- src/test/java/financemanager/TransactionListTest.java
+++ src/test/java/financemanager/TransactionListTest.java
@@ -15,11 +15,11 @@ public class TransactionListTest {
     public void setUp() {
         transactionList = new TransactionList();
     }
-    
+
     @Test
     public void removeTransaction_removesCorrectTransaction() {
-        Transaction testTransaction1 = new Transaction("Test1", 100,"Category1", "14-03-2024");
-        Transaction testTransaction2 = new Transaction("Test2", 200,"Category2", "16-03-2024");
+        Transaction testTransaction1 = new Transaction("Test1", 100, "Category1", "14-03-2024");
+        Transaction testTransaction2 = new Transaction("Test2", 200, "Category2", "16-03-2024");
         transactionList.addTransaction(testTransaction1);
         transactionList.addTransaction(testTransaction2);
 
@@ -27,5 +27,13 @@ public class TransactionListTest {
 
         assertEquals(1, transactionList.getTransactions().size());
         assertEquals(testTransaction2, transactionList.getTransactions().get(0));
+    }
+
+    @Test
+    public void removeTransaction_withInvalidIndex_throwsIndexOutOfBoundsException() {
+        Transaction testTransaction = new Transaction("Test", 200, "Personal", "14-03-2024");
+        transactionList.addTransaction(testTransaction);
+
+        assertThrows(IndexOutOfBoundsException.class, () -> transactionList.removeTransaction("delete 2"));
     }
 }
