be12808916b0b5b1f61b66eb5fede53e32de96fe
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-03-14 21:27:43 +0800

    test: Add JUnit tests for `DailyMedicationManager`

---
+++ src/test/java/seedu/meditracker/DailyMedicationManagerTest.java
@@ -0,0 +1,48 @@
+package seedu.meditracker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class DailyMedicationManagerTest {
+    @Test
+    public void addDailyMedication_genericDailyMedication_dailyMedicationAdded() {
+        DailyMedicationManager dailyMedicationManager = new DailyMedicationManager();
+        DailyMedication dailyMedication = new DailyMedication("TestMedication");
+        dailyMedicationManager.addDailyMedication(dailyMedication);
+
+        int actualIndex = 1; // 1-based indexing
+        DailyMedication dailyMedicationTest = dailyMedicationManager.getDailyMedication(actualIndex);
+        assertEquals(dailyMedication.toString(), dailyMedicationTest.toString());
+    }
+
+    @Test
+    public void takeDailyMedication_genericDailyMedication_dailyMedicationTaken() {
+        DailyMedicationManager dailyMedicationManager = new DailyMedicationManager();
+        DailyMedication dailyMedication = new DailyMedication("TestMedication");
+        assertFalse(dailyMedication.isTaken());
+        dailyMedicationManager.addDailyMedication(dailyMedication);
+
+        int actualIndex = 1; // 1-based indexing
+        dailyMedicationManager.takeDailyMedication(actualIndex);
+        DailyMedication dailyMedicationTest = dailyMedicationManager.getDailyMedication(actualIndex);
+        assertTrue(dailyMedicationTest.isTaken());
+    }
+
+    @Test
+    public void untakeDailyMedication_genericDailyMedication_dailyMedicationNotTaken() {
+        DailyMedicationManager dailyMedicationManager = new DailyMedicationManager();
+        DailyMedication dailyMedication = new DailyMedication("TestMedication");
+        dailyMedication.take();
+        assertTrue(dailyMedication.isTaken());
+        dailyMedicationManager.addDailyMedication(dailyMedication);
+
+        int actualIndex = 1; // 1-based indexing
+        dailyMedicationManager.untakeDailyMedication(actualIndex);
+        DailyMedication dailyMedicationTest = dailyMedicationManager.getDailyMedication(actualIndex);
+        assertFalse(dailyMedicationTest.isTaken());
+    }
+}
