acd44464933c6dbde4ba9abaf59dc64ad91471c6
nidhi-nayak <nidhirnayak@gmail.com>
2024-03-14 15:57:49 +0800

    Add meal categories and finished addRecipe test

--- src/test/java/recipeio/RecipeIOTest.java
+++ src/test/java/recipeio/RecipeIOTest.java
@@ -3,10 +3,26 @@ package recipeio;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import recipeio.enums.MealCategory;
+import recipeio.recipe.Recipe;
+import recipeio.recipe.RecipeList;
+
+import java.util.ArrayList;
 
 class RecipeIOTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
+    }
+
+    @Test
+    public void testAddRecipe() {
+        RecipeList recipes = new RecipeList();
+        ArrayList allergies = new ArrayList<String>();
+        allergies.add("eggs");
+        Recipe newRecipe = new Recipe("cookies", 40, 350, allergies,
+                MealCategory.DESSERT, "THIS IS MY URL");
+        recipes.addRecipe(newRecipe);
+        assertTrue(recipes.getSize() == 1);
     }
 }
