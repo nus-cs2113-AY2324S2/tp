629fb32a70476c4c5c50d0dbb4bdf842f2986443
yzs <yeozhishen@gmail.com>
2024-03-14 23:25:53 +0800

    Clean up imports in UITest Removed unnecessary imports

--- src/test/java/seedu/brokeculator/UITest.java
+++ src/test/java/seedu/brokeculator/UITest.java
@@ -1,8 +1,5 @@
 package seedu.brokeculator;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 public class UITest {
     @Test
