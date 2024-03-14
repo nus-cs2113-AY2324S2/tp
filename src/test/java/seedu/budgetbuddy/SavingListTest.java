2eabb79b32ab413a9d7ab4eb60510a22661fc99e
Dheekshitha2 <shreedhee0212@gmail.com>
2024-03-14 23:28:40 +0800

    Add junit test for reduce savings

--- src/test/java/seedu/budgetbuddy/SavingListTest.java
+++ src/test/java/seedu/budgetbuddy/SavingListTest.java
@@ -1,6 +1,9 @@
 package seedu.budgetbuddy;
 
 import org.junit.jupiter.api.Test;
+
+import javax.management.RuntimeMBeanException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -41,5 +44,22 @@ public class SavingListTest {
 
         // Verify that the saving was edited successfully
         assertEquals(150, savingList.getSavings().get(0).getAmount(), 0.001);
+    }
+
+    @Test
+    public void reduceSavings_validIndexAndAmount_success() {
+
+        SavingList savingList = new SavingList();
+        savingList.addSaving("Salary", "500"); // Adding initial savings to work with
+        savingList.addSaving("Investments", "300");
+
+        int indexToReduce = 2;
+        double amountToReduce = 100;
+        double expectedAmountAfterReduction = 200;
+
+        savingList.reduceSavings(indexToReduce - 1, amountToReduce);
+
+        // Assert that the amount after reduction is as expected
+        assertEquals(expectedAmountAfterReduction, savingList.getSavings().get(indexToReduce - 1).getAmount());
     }
 }
