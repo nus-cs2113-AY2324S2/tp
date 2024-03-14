08c74a0a9c2b3bc342a4a1dada612417695df5a5
Isaac Eng <>
2024-03-15 00:03:20 +0800

    Add JUnit test for TransactionList class

--- src/test/java/financemanager/TransactionListTest.java
+++ src/test/java/financemanager/TransactionListTest.java
@@ -1,4 +1,35 @@
 package financemanager;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import transactions.Transaction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TransactionListTest {
+
+    private TransactionList transactionList;
+
+    @BeforeEach
+    public void setUp() {
+        transactionList = new TransactionList();
+    }
+
+    @Test
+    public void getTransactions_initiallyEmpty() {
+        assertEquals(0, transactionList.getTransactions().size());
+    }
+
+    @Test
+    public void processTransaction_addsTransaction() {
+        Transaction testTransaction = new Transaction("Test", 200,"Personal", "14-03-2024");
+        transactionList.processTransaction("add /n/Test /$/200 /d/14-03-2024 /c/Personal");
+
+        assertEquals(1, transactionList.getTransactions().size());
+        assertEquals(testTransaction.getDescription(), transactionList.getTransactions().get(0).getDescription());
+        assertEquals(testTransaction.getAmount(), transactionList.getTransactions().get(0).getAmount());
+        assertEquals(testTransaction.getCategory(), transactionList.getTransactions().get(0).getCategory());
+        assertEquals(testTransaction.getDate(), transactionList.getTransactions().get(0).getDate());
+    }
 }
