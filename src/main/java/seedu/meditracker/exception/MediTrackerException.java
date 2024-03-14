f2271251a90ec6036be998d7716dd4c75bdf3112
nickczh <nickchua@u.nus.edu>
2024-03-14 22:34:10 +0800

    feat: Add MediTrackerException class

---
+++ src/main/java/seedu/meditracker/exception/MediTrackerException.java
@@ -0,0 +1,11 @@
+package seedu.meditracker.exception;
+
+/**
+ * The MediTrackerException class represents an exception specific to the MediTracker application.
+ * It extends the Exception class.
+ */
+public class MediTrackerException extends Exception {
+    public MediTrackerException() {
+
+    }
+}
