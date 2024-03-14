9143a06a074712f1a7ed1cf73077370167b6529c
CXIA17 <E0985916@u.nus.edu>
2024-03-14 19:33:22 +0800

    Add user profile

---
+++ src/main/java/seedu/duke/userProfile/Health.java
@@ -0,0 +1,21 @@
+package seedu.duke.userProfile;
+
+public class Health {
+    private static int healthBar = 100;
+
+    public void addhealth(int amount) {
+        healthBar += amount;
+    }
+
+    public void deductAmount(int amount) {
+        healthBar -= amount;
+    }
+
+    public boolean checkHealth() {
+        return healthBar <= 0;
+    }
+
+    public String outputHealth() {
+        return "" + healthBar;
+    }
+}
