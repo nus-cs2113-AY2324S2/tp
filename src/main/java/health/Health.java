ad3d2a470ef89ee1a9f35806a910527ebbfcf298
Cheong Jun Ning Jolene <jolenecheong211003@gmail.com>
2024-03-14 10:25:52 +0800

    Create Health

---
+++ src/main/java/health/Health.java
@@ -0,0 +1,31 @@
+package health;
+
+/*
+ * The Health class represents health information of the user.
+ */
+public class Health {
+    /*
+     * The height of the user in meters.
+     */
+    protected static double height;
+
+    /*
+     * The weight of the user in kilograms.
+     */
+    protected static double weight;
+
+    /*
+     * Constructor for Health object.
+     */
+    public Health() {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    /*
+     * Prints a new line.
+     */
+    public static void printNewLine() {
+        System.out.println();
+    }
+}
