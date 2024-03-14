4982fbc69b9767a44df6302a9e23b6c9fcde1f33
cyhjason29 <jackplok29@gmail.com>
2024-03-14 22:17:19 +0800

    Add support to record score of each round

--- src/main/java/seedu/duke/Results.java
+++ src/main/java/seedu/duke/Results.java
@@ -1,4 +1,32 @@
 package seedu.duke;
 
 public class Results {
+    protected static int numberOfCorrectAnswers;
+    protected static int totalNumberOfQuestions;
+    protected static String score;
+
+    private static final int HUNDRED_PERCENT = 100;
+    private static final int ZERO_QUESTIONS = 0;
+
+    public Results() {
+        numberOfCorrectAnswers = ZERO_QUESTIONS;
+        totalNumberOfQuestions = ZERO_QUESTIONS;
+    }
+
+    private static void calculateScore() {
+        int scorePercentage = numberOfCorrectAnswers / totalNumberOfQuestions * HUNDRED_PERCENT;
+        score = numberOfCorrectAnswers + "/" + totalNumberOfQuestions + " (" + scorePercentage + "%)";
+    }
+
+    public static String getScore() {
+        return score;
+    }
+
+    public static void increaseCorrectAnswers() {
+        numberOfCorrectAnswers++;
+    }
+
+    public static void increaseNumberOfQuestions() {
+        totalNumberOfQuestions++;
+    }
 }
