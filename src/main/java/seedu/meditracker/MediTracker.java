e5b60956d2eb89330f551116ab6174eb8d15b375
nickczh <nickchua@u.nus.edu>
2024-03-14 22:44:04 +0800

    refactor: Refactor run method

--- src/main/java/seedu/meditracker/MediTracker.java
+++ src/main/java/seedu/meditracker/MediTracker.java
@@ -1,5 +1,9 @@
 package seedu.meditracker;
 
+import seedu.meditracker.command.Command;
+import seedu.meditracker.exception.MediTrackerException;
+import seedu.meditracker.medication.MedicationList;
+import seedu.meditracker.parser.Parser;
 import seedu.meditracker.ui.Ui;
 
 /**
@@ -9,46 +13,45 @@ import seedu.meditracker.ui.Ui;
 public class MediTracker {
 
     private Ui ui;
+    private MedicationList medicationList;
 
     /**
      * Constructs a new MediTracker object and initializes the user interface.
      */
     public MediTracker() {
         ui = new Ui();
+        medicationList = new MedicationList();
     }
 
     /**
      * Runs the MediTracker application.
      * This method displays a welcome message, reads user commands, and processes them until the user exits the
      * application.
+     * @throws MediTrackerException If an error occurs during the execution of the application.
      */
-    public void run() {
-
+    public void run() throws MediTrackerException {
         //@@author nickczh-reused
         //Reused from https://github.com/nickczh/ip
         //with minor modifications
-        ui.showWelcome();
+        ui.showWelcomeMessage();
         boolean isExit = false;
-
         while (!isExit) {
-            String command = ui.readCommand();
-            if (command.equals("exit")) {
-                isExit = true;
-                ui.showExitMessage();
-                continue;
-            }
-
-            ui.showLine(); // show the divider line ("_________")
-            System.out.println(command + "\n");
+            String fullCommand = ui.readCommand();
+            ui.showLine();
+            Command command = Parser.parse(fullCommand);
+            command.execute(medicationList, ui);
+            isExit = command.isExit();
         }
+        ui.showExitMessage();
     }
 
     /**
      * Starts the MediTracker application.
      * It creates a new MediTracker object and calls its run() method.
      * @param args Command-line arguments.
+     * @throws MediTrackerException If an error occurs during the execution of the application.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MediTrackerException {
         new MediTracker().run();
     }
 }
