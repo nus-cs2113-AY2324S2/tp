73d1d7138886f8a8f2e2970f51ca161206eacd4d
YHWong20 <yihao.wong@u.nus.edu>
2024-03-14 16:05:12 +0800

    Update Ui helper methods

--- src/main/java/seedu/binbash/ui/Ui.java
+++ src/main/java/seedu/binbash/ui/Ui.java
@@ -3,11 +3,17 @@ package seedu.binbash.ui;
 import java.util.Scanner;
 
 public class Ui {
+    private static final String LOGO = "  ____  _       ____            _\n" +
+            " | __ )(_)_ __ | __ )  __ _ ___| |__\n" +
+            " |  _ \\| | '_ \\|  _ \\ / _` / __| '_ \\\n" +
+            " | |_) | | | | | |_) | (_| \\__ \\ | | |\n" +
+            " |____/|_|_| |_|____/ \\__,_|___/_| |_|\n" +
+            "\n";
     private static final String WELCOME_MESSAGE = "Welcome to BinBash!";
     private static final String GOODBYE_MESSAGE = "Bye!";
     private static final String LINE_DIVIDER = "-------------------------------------------------------------";
 
-    private Scanner in;
+    private final Scanner in;
     private boolean isUserActive;
 
     public Ui() {
@@ -19,8 +25,16 @@ public class Ui {
         return isUserActive;
     }
 
+    public void setUserAsInactive() {
+        isUserActive = false;
+    }
+
+    public String readUserCommand() {
+        return in.nextLine();
+    }
+
     public void greet() {
-        talk(WELCOME_MESSAGE);
+        talk(LOGO + WELCOME_MESSAGE);
     }
 
     public void farewell() {
