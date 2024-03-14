6e5c8e9444224481299bbc78c52164b935765dae
yzs <yeozhishen@gmail.com>
2024-03-14 23:51:43 +0800

    Add Javadoc comments Added comments for public functions in the UI class for clarity

--- src/main/java/seedu/brokeculator/UI.java
+++ src/main/java/seedu/brokeculator/UI.java
@@ -4,13 +4,27 @@ import java.util.Scanner;
 
 public class UI {
     private static final String STRING_DECORATION = "------------------------------------";
+
+    /**
+     * appends topLineDecoration and bottomLineDecoration to the top and bottom of message respectively
+     * @param String message message to encapsulate
+     * @param String topLineDecoration string to append to top of message
+     * @param String bottomLineDecoration string to append to bottom of message
+     * @return message appended with topLineDecoration and bottomLineDecoration at top and bottom respectively
+     */
     public static String prettify(String message, String topLineDecoration, String bottomLineDecoration) {
         return topLineDecoration + System.lineSeparator()
                 + message + System.lineSeparator() + bottomLineDecoration;
     }
+
+    /**
+     * function for retrieving user input line by line
+     * @return String singleLineUserInput single line of user input
+     */
     public static String getUserInput() {
         Scanner stdinScanner = new Scanner(System.in);
-        return stdinScanner.nextLine();
+        String singleLineUserInput = stdinScanner.nextLine();
+        return singleLineUserInput;
     }
 
     /**
