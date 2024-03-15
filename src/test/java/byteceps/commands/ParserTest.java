c7fc18863dd4edcb1ca46a15c56625db5ca0e012
pqienso <buckslimyuan@gmail.com>
2024-03-15 14:28:53 +0800

    Add JUnit test for parseCommand() method

--- src/test/java/byteceps/commands/ParserTest.java
+++ src/test/java/byteceps/commands/ParserTest.java
@@ -2,7 +2,11 @@ package byteceps.commands;
 
 import org.junit.jupiter.api.Test;
 
+import javax.naming.OperationNotSupportedException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ParserTest {
 
     @Test
@@ -18,5 +22,35 @@ public class ParserTest {
         assertEquals(command, "exercise");
         assertEquals(action, "add");
         assertEquals(parameter, "deadlift");
+    }
+
+    @Test
+    public void parseCommand_addExercise_ExerciseCommand() {
+        Parser testParser = new Parser();
+        String validInput = "exercise /add deadlift";
+        testParser.parseInput(validInput);
+
+        Command outputCommand = testParser.parseCommand();
+        assertEquals(outputCommand.getClass(), ExerciseCommand.class);
+    }
+
+    @Test
+    public void parseCommand_listWorkout_WorkoutCommand() {
+        Parser testParser = new Parser();
+        String validInput = "workout /list";
+        testParser.parseInput(validInput);
+
+        Command outputCommand = testParser.parseCommand();
+        assertEquals(outputCommand.getClass(), WorkoutCommand.class);
+    }
+
+    @Test
+    public void parseCommand_invalidCommand_throwException() {
+        Parser testParser = new Parser();
+        String validInput = "aaaaa /list";
+        testParser.parseInput(validInput);
+
+
+        Exception exception = assertThrows(UnsupportedOperationException.class, testParser::parseCommand);
     }
 }
