d8ec88431c2f2997443553810d178ad4c7f06676
JackieNeoCEG <e0959140@u.nus.edu>
2024-03-15 15:38:58 +0800

    Hilarious CheckStyleTest error

--- src/test/java/seedu/duke/UiTest.java
+++ src/test/java/seedu/duke/UiTest.java
@@ -1,11 +1,15 @@
 package seedu.duke;
+
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 class UiTest {
+
     @Test
-    public void sampleTest() {
+    public void sampleUiTest() {
         assertTrue(true);
     }
+
 }
