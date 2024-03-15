64a8fa65ca00540fa62e7fb581e8d83e312fda1d
nicknamenic <110780382+nicknamenic@users.noreply.github.com>
2024-03-15 22:00:03 +0800

    Create TicTacToeTest.java

---
+++ src/test/java/seedu/duke/TicTacToeTest.java
@@ -0,0 +1,12 @@
+package seedu.duke;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TicTacToeTest {
+    @Test
+    public void sampleTicTacToeTest() {
+        assertTrue(true);
+    }
+}
