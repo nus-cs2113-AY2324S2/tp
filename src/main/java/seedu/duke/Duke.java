5cc396e1e281b540318cdfdf92202ea6eefc1d66
JiahaoGan <u3585924@connect.hku.hk>
2024-03-15 11:20:10 +0800

    Add main logic function for NUSFC24 v1.0

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,21 +1,43 @@
 package seedu.duke;
 
-import java.util.Scanner;
+import seedu.duke.Ui.Ui;
 
 public class Duke {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
+    private final Ui u1;
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+    public Duke() {
+        this.u1 = new Ui();
+    }
+
+    public static void main(String[] args) {
+        Duke d1=new Duke();
+        d1.runLogic();
+
+    }
+
+    private void runLogic(){
+        u1.printGreet();
+        String userInput = u1.readUserInput();
+        while (isTerminate(userInput)){
+            shootFunction();
+            userInput = u1.readUserInput();
+        }
+        u1.printBye();
+
+    }
+
+    private void shootFunction(){
+        //        void viewgoal function;
+        //        int userShoot function;
+        //        int AI_goalkeeper;
+        //        void viewgoal (int shoot, boolean goalCheck)
+        System.out.println("Function");
+    }
+
+    private boolean isTerminate(String userInput){
+        return !userInput.equalsIgnoreCase("bye");
     }
 }
