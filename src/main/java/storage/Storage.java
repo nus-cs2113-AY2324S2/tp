4173428abbb91da31939c181df244992e900aba4
R M Raajamani <>
2024-03-15 11:45:06 +0800

    Storage Class and StorageTest

---
+++ src/main/java/storage/Storage.java
@@ -0,0 +1,15 @@
+package storage;
+
+import health.Health;
+
+public class Storage {
+    private Health health;
+
+    public void save(int id, Health health) {
+    }
+
+    public static Storage load(int id) {
+        // return new Storage(...)
+        return null;
+    }
+}
