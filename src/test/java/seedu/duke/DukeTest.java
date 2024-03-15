b080c9bc6077425e61b9923d95dc389a344d6519
z-wenqing <zhangwenqing0824@gmail.com>
2024-03-15 12:42:29 +0800

    Add Junit Test

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -1,12 +1,31 @@
 package seedu.duke;
+import seedu.duke.UserList;
+import
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
 
 class DukeTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
+    }
+    @Test
+    public void testInvalidCommand() {
+        // Arrange
+        UserList userList = new UserList();
+        ByteArrayInputStream in = new ByteArrayInputStream("invalid\nbye\n".getBytes());
+        System.setIn(in);
+        String expectedOutput = "Welcome to Duke!\nInvalid command!\nGoodbye!\n";
+
+        // Act
+        Duke.main(new String[0]);
+
+        // Assert
+        assertEquals(expectedOutput, UI.printInvalidCommand());
     }
 }
