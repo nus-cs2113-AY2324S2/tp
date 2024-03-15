f8572c553c38cb4c22b5ce99c65e4d3e59d8e577
yzs <yeozhishen@gmail.com>
2024-03-15 13:51:54 +0800

    Add task saving method in Logic

--- src/main/java/seedu/brokeculator/Logic.java
+++ src/main/java/seedu/brokeculator/Logic.java
@@ -4,19 +4,33 @@ public class Logic {
     public Logic() {
         GeneralInputParser mainParser = new GeneralInputParser();
         FileManager fileManager = new FileManager();
+        ExpenseManager expenseManager = new ExpenseManager();
     }
     public void run() {
         while (true) {
             try {
                 Command command = mainParser.getCommandFromUserInput();
                 command.execute();
-                fileMager.save();
+                fileManager.save();
             } catch (Exception e) {
                 UI.print("error occured, sus. " + e.getMessage());
             }
         }
     }
     private void loadTasksFromFile() {
-
+        boolean hasNoFileErrors = fileManager.openFile();
+        if (!hasNoFileErrors) {
+            UI.print("continuing without file");
+            return;
+        }
+        while (fileManager.hasNextLine()) {
+            String line = fileManager.readNextLine();
+            Command loadCommand = mainParser.getCommandFromFileInput(line);
+            loadCommand.execute();
+        }
+    }
+    private void saveTasksToFile() {
+        String expenseListToSave = expenseManager.getExpensesStringRepresentations();
+        fileManager.save(expenseListToSave);
     }
 }
