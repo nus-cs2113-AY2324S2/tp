2d5fb443e3aed335293ffb849d33a007bd723982
fxe025 <e0968867@u.nus.edu>
2024-03-15 16:17:47 +0800

    Remove unused import

--- src/test/java/Storage/StorageTest.java
+++ src/test/java/Storage/StorageTest.java
@@ -1,10 +1,11 @@
 package Storage;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.fail;
 
-import java.io.FileNotFoundException;
+//import java.io.FileNotFoundException;
+
 public class StorageTest {
     @Test
     public void fileToReadNotFound() {
