9a7f83aade6a8b8ba0cacab12f92c8b84a1871d2
ChongXern <chongxern@gmail.com>
2024-03-15 15:46:51 +0800

    Add outflow junit test

---
+++ src/main/java/financialtransactions/OutflowTest.java
@@ -0,0 +1,13 @@
+package financialtransactions;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class OutflowTest {
+    @Test
+    public void testSetCategory() {
+        Outflow outflow = new Outflow("2024 Sem 2 School Fees", 999999.99, "2024-02-01");
+        outflow.setCategory(Outflow.Category.EDUCATION);
+        assertEquals(Outflow.Category.EDUCATION, outflow.getCategory());
+    }
+}
