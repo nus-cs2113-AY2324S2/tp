4877f3d12e0c0c5cc799b5b66b9f744bbf7b719e
raajamani <36617948+raajamani@users.noreply.github.com>
2024-03-15 14:16:16 +0800

    Update StorageTest.java

--- src/test/java/storage/StorageTest.java
+++ src/test/java/storage/StorageTest.java
@@ -5,18 +5,5 @@ import org.junit.jupiter.api.Test;
 import storage.Storage;
 
 public class StorageTest {
-
-    /*
-     * The StorageTest class tests the functionality of the Storage class.
-     * More specifically, the load method of the Storage class is under testing.
-     */
-
-    @Test
-    public void testLoad() {
-        Storage storage = Storage.load(123);
-        Assertions.assertNotNull(storage, "Checking if a loaded Storage object is not null");
-
-        // You can add more assertions depending on the actual behaviour 
-        // of the load method in the Storage class.
     }
-}
+
