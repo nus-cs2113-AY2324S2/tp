6026578f2645183c14c2c06b80b972b9b83a4efc
luozihui2003 <e0969601@u.nus.edu>
2024-03-15 13:30:22 +0800

    Added test case for add amount

--- src/test/java/grocery/GroceryListTest.java
+++ src/test/java/grocery/GroceryListTest.java
@@ -57,4 +57,17 @@ public class GroceryListTest {
             assertEquals("The grocery does not exist!", e.getMessage());
         }
     }
+    @Test
+    public void setAmount_wrongFormat_exceptionThrown() {
+        try {
+            GroceryList gl = new GroceryList();
+            gl.addGrocery(new Grocery("Meat", "", ""));
+            gl.setAmount("Meat");
+        } catch (GitException e) {
+            String message = "Command is in the wrong format, type \"help\" for more information." +
+                    System.lineSeparator() +
+                    "amt needs 'a/'";
+            assertEquals(message, e.getMessage());
+        }
+    }
 }
