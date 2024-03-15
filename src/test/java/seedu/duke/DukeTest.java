65fcad0b726a942892c0cd27add58e5c7c2cb171
annnniexu <annie.xu2025@berkeley.edu>
2024-03-15 23:51:47 +0800

    added getNoTasksTest

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -26,6 +26,29 @@ class DukeTest {
     }
 
     @Test
+    public void getNoTasksTest() {
+        //add the first plan
+        TravelActivityList travelActivityList = new TravelActivityList();
+        travelActivityList.addTask(new TravelActivity("visit museum"));
+        assertEquals("visit museum", travelActivityList.getDescription("visit museum"));
+        //add the second and third plan
+        travelActivityList.addTask(new TravelActivity("go to beach"));
+        assertEquals("go to beach", travelActivityList.getDescription("go to beach"));
+        travelActivityList.addTask(new TravelActivity("shopping"));
+        assertEquals("shopping", travelActivityList.getDescription("shopping"));
+        //check number of tasks
+        assertEquals(3, travelActivityList.getNoOfTasks());
+        //delete the first plan
+        travelActivityList.removeTask(1);
+        assertEquals("cant be found", travelActivityList.getDescription("visit museum"));
+        //check number of tasks
+        assertEquals(2, travelActivityList.getNoOfTasks());
+
+
+
+    }
+
+    @Test
     public void trueTest(){
         assertTrue(true);
     }
