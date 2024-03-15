b0a7a88e6492669a142186f4e68556f52f46ec96
fxe025 <e0968867@u.nus.edu>
2024-03-15 15:18:05 +0800

    Create StorageTest

--- src/test/java/Storage/StorageTest.java
+++ src/test/java/Storage/StorageTest.java
@@ -1,2 +1,12 @@
-package Storage;public class StorageTest {
+package Storage;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.FileNotFoundException;
+public class StorageTest {
+    @Test
+    public void fileToReadNotFound() {
+    }
 }
