56d1d8371a073be082db838119ac12b81db56787
YoengKokLeong <e0968894@u.nus.edu>
2024-03-15 20:24:43 +0800

    Added Game class

---
+++ src/test/java/seedu/duke/GameTest.java
@@ -0,0 +1,12 @@
+package seedu.duke;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+    @Test
+    public void sampleGameTest() {
+        assertTrue(true);
+    }
+}
