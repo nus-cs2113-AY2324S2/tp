c6f31ca97a59a52dffcaa26f47c2101425950404
sweijie24 <e0958828@u.nus.edu>
2024-03-16 23:12:55 +0800

    Add JUnit tests for parsing input for list commands

--- src/test/java/seedu/budgetbuddy/ParserTest.java
+++ src/test/java/seedu/budgetbuddy/ParserTest.java
@@ -2,11 +2,17 @@ package seedu.budgetbuddy;
 
 import org.junit.jupiter.api.Test;
 import seedu.budgetbuddy.command.Command;
+import seedu.budgetbuddy.command.ListExpenseCommand;
+import seedu.budgetbuddy.command.ListSavingsCommand;
 import seedu.budgetbuddy.command.MenuCommand;
 
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+
 public class ParserTest {
     @Test
     public void testHandleMenuCommandWithoutIndex() {
@@ -48,5 +54,93 @@ public class ParserTest {
         Command invalidCommand = parser.parseCommand(expenses, savings, "notACommand");
 
         assertNull(invalidCommand);
+    }
+
+    @Test
+    public void handleListCommand_listExpenses_success() {
+        Parser parser = new Parser();
+        ExpenseList expenseList = new ExpenseList();
+        SavingList savingList = new SavingList();
+        expenseList.addExpense("Transport", "50", "Bus Fare");
+        expenseList.addExpense("Housing", "3000", "BTO");
+
+        String input = "list expenses";
+
+        Command command = parser.handleListCommand(input, expenseList, savingList);
+
+        assertEquals(ListExpenseCommand.class, command.getClass());
+    }
+
+    @Test
+    public void handleListCommand_listExpensesWithCategory_success() {
+        Parser parser = new Parser();
+        ExpenseList expenseList = new ExpenseList();
+        SavingList savingList = new SavingList();
+        expenseList.addExpense("Transport", "50", "Bus Fare");
+        expenseList.addExpense("Housing", "3000", "BTO");
+
+        String input = "list expenses housing";
+
+        Command command = parser.handleListCommand(input, expenseList, savingList);
+
+        assertEquals(ListExpenseCommand.class, command.getClass());
+    }
+
+    @Test
+    public void handleListCommand_listExpensesWithCategory_invalidCategory() {
+        Parser parser = new Parser();
+        ExpenseList expenseList = new ExpenseList();
+        SavingList savingList = new SavingList();
+        expenseList.addExpense("Transport", "50", "Bus Fare");
+        expenseList.addExpense("Housing", "3000", "BTO");
+
+        String input = "list expenses qweqwe";
+
+        Command command = parser.handleListCommand(input, expenseList, savingList);
+        assertNull(command);
+    }
+
+    @Test
+    public void handleListCommand_listSavings_success() {
+        Parser parser = new Parser();
+        ExpenseList expenseList = new ExpenseList();
+        SavingList savingList = new SavingList();
+        savingList.addSaving("Salary", "1150");
+        savingList.addSaving("Investment", "300");
+
+        String input = "list savings";
+
+        Command command = parser.handleListCommand(input, expenseList, savingList);
+
+        assertEquals(ListSavingsCommand.class, command.getClass());
+    }
+
+    @Test
+    public void handleListCommand_listSavingsWithCategory_success() {
+        Parser parser = new Parser();
+        ExpenseList expenseList = new ExpenseList();
+        SavingList savingList = new SavingList();
+        savingList.addSaving("Salary", "1150");
+        savingList.addSaving("Investment", "300");
+
+        String input = "list savings salary";
+
+        Command command = parser.handleListCommand(input, expenseList, savingList);
+
+        assertEquals(ListSavingsCommand.class, command.getClass());
+    }
+
+    @Test
+    public void handleListCommand_listSavingsWithCategory_invalidCategory() {
+        Parser parser = new Parser();
+        ExpenseList expenseList = new ExpenseList();
+        SavingList savingList = new SavingList();
+        savingList.addSaving("Salary", "1150");
+        savingList.addSaving("Investment", "300");
+
+        String input = "list savings qweqwe";
+
+        Command command = parser.handleListCommand(input, expenseList, savingList);
+        assertNull(command);
     }
 }
