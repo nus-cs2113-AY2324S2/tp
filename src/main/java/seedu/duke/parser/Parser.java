5e7880f3847e09e2fad7a08d477af41043d49833
sxfoo <fsx159@gmail.com>
2024-03-16 22:02:36 +0800

    Fix GRADE_PATTERN minor bug & Change argument data structure
    GRADE_PATTERN allows [+-] grade without a character before.
    Change parser command arguments to use map<string,string> type instead of arrays to reduce chances of using magic literals.

--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -36,11 +36,11 @@ public class Parser {
                     "\\s+w/(?<semester>[1-9]|10)\\s+m/(?<mc>[1-9]|1[0-2])", Pattern.CASE_INSENSITIVE);
     private static final Pattern GRADE_PATTERN =
             Pattern.compile("grade\\s+c/(?<courseCode>[A-Za-z]{2,3}\\d{4}[A-Za-z]?)" +
-                    "\\s+g/(?<grade>[ab]?[+-]?|[cd][+]?|f|[1-5](?:\\.0|\\.5)?|0\\.0?)", Pattern.CASE_INSENSITIVE);
+                    "\\s+g/(?<grade>[ab][+-]?|[cd][+]?|f|[1-5](?:\\.0|\\.5)?|0\\.0?)", Pattern.CASE_INSENSITIVE);
 
     private static Map<CommandType, Pattern> commandPatternMap = initCommandPatternMap();
     private static Map<CommandType, String[]> commandGroupArgumentMap = initCommandGroupArgumentMap();
-    private static Map<CommandType, Function<String[], Command>> commandClassMap = initCommandClassMap();
+    private static Map<CommandType, Function<Map<String, String>, Command>> commandClassMap = initCommandClassMap();
 
     private static Map<CommandType, Pattern> initCommandPatternMap() {
         Map<CommandType, Pattern> commandPatternMap = new HashMap<>();
@@ -68,8 +68,8 @@ public class Parser {
         return commandGroupArgumentMap;
     }
 
-    private static Map<CommandType, Function<String[], Command>> initCommandClassMap() {
-        Map<CommandType, Function<String[], Command>> commandClassMap = new HashMap<>();
+    private static Map<CommandType, Function<Map<String, String>, Command>> initCommandClassMap() {
+        Map<CommandType, Function<Map<String, String>, Command>> commandClassMap = new HashMap<>();
 
         // CURRENTLY UNAVAILABLE: Command Classes to execute
         // commandClassMap.put(CommandType.INIT, args -> new InitCommand());
@@ -89,8 +89,8 @@ public class Parser {
 
             if (matcher.matches()) {
                 CommandType commandType = entry.getKey();
-                // Function<String[], Command> commandCreator = commandClassMap.get(commandType);
-                String[] arguments = getCommandArguments(commandType, matcher);
+                // Function<Map<String, String>, Command> commandCreator = commandClassMap.get(commandType);
+                Map<String, String> arguments = getCommandArguments(commandType, matcher);
 
                 // return commandCreator.apply(arguments);
                 return;
@@ -99,12 +99,12 @@ public class Parser {
         return;
     }
 
-    private static String[] getCommandArguments(CommandType command, Matcher matcher) {
+    private static Map<String, String> getCommandArguments(CommandType command, Matcher matcher) {
         String[] groupNames = commandGroupArgumentMap.get(command);
-        String[] arguments = new String[groupNames.length];
+        Map<String, String> arguments = new HashMap<>();
 
         for (int i = 0; i < groupNames.length; i++) {
-            arguments[i] = matcher.group(groupNames[i]);
+            arguments.put(groupNames[i], matcher.group(groupNames[i]));
         }
 
         return arguments;
