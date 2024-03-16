ddcf4ac36fb4d1b9d215c5c510b1438c78788bf4
Mmaxx15 <leongxwmax@gmail.com>
2024-03-16 13:40:45 +0800

    Created Expenditure Class which stores description, amount and date of purchase

--- src/main/java/seedu/duke/Expenditure.java
+++ src/main/java/seedu/duke/Expenditure.java
@@ -2,5 +2,45 @@ package seedu.duke;
 
 public class Expenditure {
     protected String description;
-    protected float amount;
+    protected Float amount;
+    protected String date;
+
+    public Expenditure(String description, Float amount, String date) {
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Float amount) {
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Expenditure{" +
+                "description='" + description + '\'' +
+                ", amount=" + amount +
+                ", date='" + date + '\'' +
+                '}';
+    }
 }
