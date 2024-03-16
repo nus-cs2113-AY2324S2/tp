3aa573a4ba20c9c8012a9d3ff991c5f995a17625
Mmaxx15 <leongxwmax@gmail.com>
2024-03-16 13:43:55 +0800

    combined previous classes to run program in CantVasMain

--- src/main/java/seedu/duke/CantVasMain.java
+++ src/main/java/seedu/duke/CantVasMain.java
@@ -1,28 +1,15 @@
 package seedu.duke;
-
-import java.util.Scanner;
-
+import CantVasUI.UI;
 
 public class CantVasMain {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
-
-
-        Scanner in = new Scanner(System.in);
-        String name = in.nextLine();
-        System.out.println("Hello " + name);
-
-        // Call the Echo functionality
-        Echo echo = new Echo();
-        Echo.startEcho();
+        UI ui = new UI();
+        ui.printLogo();
+        ExpenditureList expenseList = new ExpenditureList();
+        ProcessCommand processCommand = new ProcessCommand(expenseList);
+        processCommand.UserCommand();
     }
 }
