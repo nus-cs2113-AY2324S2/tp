fa20a992fa08442f394bd5fbf6b7f18b20bfd97e
JianJiaT <tjjtjn@gmail.com>
2024-03-16 16:02:53 +0800

    Revert "Implement InvalidCommand"
    This reverts commit 6ac94ace97ad990f213eacb70b91766acf3ef869.

--- src/main/java/brokeculator/command/InvalidCommand.java
+++ src/main/java/brokeculator/command/InvalidCommand.java
@@ -1,12 +1,8 @@
 package brokeculator.command;
 
 public class InvalidCommand extends Command{
-
-    public InvalidCommand() {
-        super();
-    }
     @Override
     public void execute() {
-        System.out.println("Invalid command received");
+        // TODO
     }
 }
