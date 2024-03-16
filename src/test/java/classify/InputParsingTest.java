d12d7b1fddfddc2e06ebcfc52471dbc52ccc9c2f
Shui Hon Siew <shuihon@gmail.com>
2024-03-16 23:13:00 +0800

    Add JUnit test

--- src/test/java/classify/InputParsingTest.java
+++ src/test/java/classify/InputParsingTest.java
@@ -1,6 +1,7 @@
 package classify;
 
 import classify.student.Student;
+import classify.student.StudentAttributes;
 import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -8,9 +9,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//@@author tayponghee
+
 public class InputParsingTest {
 
+    //@@author tayponghee
     @Test
     public void testByeCommand() {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -25,6 +27,20 @@ public class InputParsingTest {
         String expectedOutput = "Hope you've had a productive day. See you again! Goodbye!" +
                 System.lineSeparator() + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
 
+        assertEquals(expectedOutput.trim(), printedOutput);
+    }
+
+    //@author blackmirag3
+    @Test
+    public void testUnknownCommand() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        ArrayList<Student> students = new ArrayList<>();
+        InputParsing.parseUserCommand("yeet", students, new Scanner(System.in));
+        System.setOut(System.out);
+        String printedOutput = outputStream.toString().trim();
+        String expectedOutput = "Wrong Input! Please try again!" +
+                System.lineSeparator() + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
         assertEquals(expectedOutput.trim(), printedOutput);
     }
 }
