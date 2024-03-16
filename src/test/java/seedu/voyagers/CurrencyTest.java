4cd9c933be098740d9616f380edec1a21103a8a4
jthibault03 <98941496+jthibault03@users.noreply.github.com>
2024-03-15 23:31:26 -0700

    currencytests

--- src/test/java/seedu/voyagers/CurrencyTest.java
+++ src/test/java/seedu/voyagers/CurrencyTest.java
@@ -1,29 +1,33 @@
 package seedu.voyagers;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Random;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class CurrencyTest {
-
+    List<Currency> items = List.of(Currency.USD, Currency.EUR, Currency.GBP, Currency.JPY, Currency.AUD,
+            Currency.CAD, Currency.CHF, Currency.CNY, Currency.SEK, Currency.NZD, Currency.MXN, Currency.SGD,
+            Currency.HKD, Currency.NOK, Currency.KRW, Currency.TRY, Currency.RUB, Currency.INR, Currency.BRL,
+            Currency.ZAR);
+    Random random = new Random();
     @Test
     public void hasSymbol() {
-        Random random = new Random();
+
         int randomNumber = random.nextInt(19);
-
-        List<Currency> items = List.of(Currency.USD, Currency.EUR, Currency.GBP, Currency.JPY, Currency.AUD,
-                Currency.CAD, Currency.CHF, Currency.CNY, Currency.SEK, Currency.NZD, Currency.MXN, Currency.SGD,
-                Currency.HKD, Currency.NOK, Currency.KRW, Currency.TRY, Currency.RUB, Currency.INR, Currency.BRL,
-                Currency.ZAR);
-
         Currency randomItem = items.get(randomNumber);
-        assertNotNull(randomItem.getName());
+
         assertNotNull(randomItem.getSymbol());
         assertTrue(randomItem.getSymbol() instanceof String);
+
+    }
+
+    @Test
+    public void hasName() {
+        int randomNumber = random.nextInt(19);
+        Currency randomItem = items.get(randomNumber);
+        assertNotNull(randomItem.getName());
         assertTrue(randomItem.getName() instanceof String);
-
-
     }
 }
