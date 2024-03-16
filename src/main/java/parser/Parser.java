60b831f63439847d27905253571c4c2eef29ffd4
genexus85 <arilimeelik@gmail.com>
2024-03-16 17:54:10 +0800

    Add Command Interface

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -1,30 +1,25 @@
 package parser;
 
+
 public class Parser {
 
-    public static Command determineCommand(ReflectionTime reflectionTime, HabitTracker habitTracker,
-                                           SleepTracker sleepTracker, FitnessMotivator fitnessMotivator,
-                                           FocusTimer focusTimer, String userInput) {
+    //TODO: to add individual classes for each feature in the determineCommand method input parameters
+    public static void determineCommand(String userInput) {
         String[] userWords = userInput.trim().split("\\s+", 2);
         String userCommandSection = userWords[0];
 
         String commandArgs = userWords.length == 2 ? userWords[1] : "";
 
         switch (userCommandSection) {
+        //TODO: to add individual return commands for each feature
         case "reflect":
-            return determineReflectionCommand(reflectionTime, commandArgs);
         case "habit":
-            return determineHabitCommand(habitTracker, commandArgs);
         case "sleep":
-            return determineSleepCommand(sleepTracker, commandArgs);
         case "fitness":
-            return determineFitnessCommand(fitnessMotivator, commandArgs);
         case "focus":
-            return determineFocusCommand(focusTimer, commandArgs);
         case "exit":
-            return new ExitCommand(commandArgs);
         default:
-            throw new UnknownCommandException("Unknown command");
+        //TODO: to add throw exception for unknown command
         }
     }
 }
