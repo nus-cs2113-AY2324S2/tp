c49ff16026752239bdc3b68e8ea12d4b46ffb8cc
mihirheda02 <87570498+mihirheda02@users.noreply.github.com>
2024-03-17 15:19:12 +0800

    Pull and merge master into ExpenseManager

--- src/main/java/brokeculator/expense/ExpenseManager.java
+++ src/main/java/brokeculator/expense/ExpenseManager.java
@@ -1,16 +1,39 @@
 package brokeculator.expense;
 
+import java.util.ArrayList;
+
 public class ExpenseManager {
+    private final ArrayList<Expense> expenses;
+
     public ExpenseManager() {
-        // TODO
+        expenses = new ArrayList<>();
+    }
+
+    public ExpenseManager(ArrayList<Expense> expenses) {
+        this.expenses = expenses;
     }
 
     public void add(Expense expense) {
-        // TODO
+        expenses.add(expense);
     }
 
-    public String getExpensesStringRepresentations() {
-        // TODO
-        return null;
+    public void delete(int index) {
+        expenses.remove(index);
+    }
+
+    public double summariseExpenses() {
+        double total = 0;
+        for (Expense expense : expenses) {
+            total += expense.getAmount();
+        }
+        return total;
+    }
+
+    public String getExpensesStringRepresentation() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < expenses.size(); i++) {
+            sb.append(String.format("%d. %s\n", i + 1, expenses.get(i).getStringRepresentation()));
+        }
+        return sb.toString();
     }
 }
