5fecb764206dca82167d74a1a71d55fdeab67dc8
z-wenqing <zhangwenqing0824@gmail.com>
2024-03-17 15:53:30 +0800

    Add timetable class

--- src/main/java/seedu/duke/Timetable.java
+++ src/main/java/seedu/duke/Timetable.java
@@ -1,5 +1,19 @@
 package seedu.duke;
 
+import java.util.ArrayList;
+
 public class Timetable {
     //todo
+    private ArrayList<String>[] daysOfWeek;
+    public Timetable() {
+        daysOfWeek = new ArrayList[7];
+        initializeTimetable();
+    }
+
+    private void initializeTimetable() {
+        for (int i = 0; i < 7; i++) {
+            daysOfWeek[i] = new ArrayList<>();
+        }
+    }
+    protected String name;
 }
