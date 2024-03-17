9aa07aff14f304010a8cd5f2f57eb57d882beb68
Thaw Tun Zan <110762796+ThawTunZan@users.noreply.github.com>
2024-03-17 19:56:43 +0800

    Add implementation for adding of modules

--- src/main/java/seedu/duke/command/AddCommand.java
+++ src/main/java/seedu/duke/command/AddCommand.java
@@ -1,8 +1,28 @@
 package seedu.duke.command;
 
+import seedu.duke.modules.Module;
+import seedu.duke.modules.ModuleList;
+
 public class AddCommand extends Command{
+    private String moduleCode;
+    private String moduleGrade;
+    private String moduleMC;
+    private ModuleList moduleList;
+
+    public AddCommand(String moduleCode, String moduleGrade, String moduleMC, ModuleList moduleList) {
+        this.moduleCode = moduleCode;
+        this.moduleGrade = moduleGrade;
+        this.moduleMC = moduleMC;
+    }
+
+    //to not throw error
+    public AddCommand() {
+
+    }
 
     @Override
     public void execute(String userInput) {
+        Module newModule = new Module(moduleCode, moduleGrade, moduleMC);
+        moduleList.addModule(newModule);
     }
 }
--- src/main/java/seedu/duke/modules/Module.java
+++ src/main/java/seedu/duke/modules/Module.java
@@ -3,10 +3,12 @@ package seedu.duke.modules;
 public class Module {
     private String moduleCode;
     private String moduleGrade;
+    private String moduleMC;
 
-    public Module(String moduleCode, String moduleGrade) {
+    public Module(String moduleCode, String moduleGrade, String moduleMC) {
         this.moduleCode = moduleCode;
         this.moduleGrade = moduleGrade;
+        this.moduleMC = moduleMC;
     }
 
     public String getModuleCode() {
@@ -25,11 +27,20 @@ public class Module {
         this.moduleGrade = moduleGrade;
     }
 
+    public String getModuleMC() {
+        return moduleMC;
+    }
+
+    public void setModuleMC(String moduleMC) {
+        this.moduleMC = moduleMC;
+    }
+
     @Override
     public String toString() {
         return "Module{" +
                 "moduleCode='" + moduleCode + '\'' +
                 ", moduleGrade='" + moduleGrade + '\'' +
+                ", moduleMC='" + moduleMC + '\'' +
                 '}';
     }
 }
