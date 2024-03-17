abfc98dc09ca542af82dd93e7f00d18c8c3ae668
imanamirshah <iman.amirshah@gmail.com>
2024-03-17 20:37:05 +0800

    Modify AddCommand class to include new parameters

--- src/main/java/seedu/binbash/command/AddCommand.java
+++ src/main/java/seedu/binbash/command/AddCommand.java
@@ -7,21 +7,32 @@ public class AddCommand extends Command {
     public static final String COMMAND_STRING = "add";
 
     public static final Pattern COMMAND_FORMAT =
-            Pattern.compile("add\\s+n/(?<itemName>.+?)\\s+d/(?<itemDescription>.+)");
+            Pattern.compile("add\\s+n/(?<itemName>.+?)\\s+d/(?<itemDescription>.+)\\s+q/(?<itemQuantity>.+)\\s"
+                    + "+e/(?<itemExpirationDate>.+)+s/(?<itemSalePrice>.+)+c/(?<itemCostPrice>.+)");
 
 
 
     private String itemName;
     private String itemDescription;
+    private int itemQuantity;
+    private String itemExpirationDate;
+    private  double itemSalePrice;
+    private double itemCostPrice;
 
-    public AddCommand(ItemList itemList, String itemName, String itemDescription) {
+    public AddCommand(ItemList itemList, String itemName, String itemDescription, int itemQuantity,
+                      String itemExpirationDate, double itemSalePrice, double itemCostPrice) {
         super(itemList);
         this.itemName = itemName;
         this.itemDescription = itemDescription;
+        this.itemQuantity = itemQuantity;
+        this.itemExpirationDate = itemExpirationDate;
+        this.itemSalePrice = itemSalePrice;
+        this.itemCostPrice = itemCostPrice;
     }
 
     @Override
     public String execute() {
-        return itemList.addItem(itemName, itemDescription);
+        return itemList.addItem(itemName, itemDescription, itemQuantity, itemExpirationDate,
+                itemSalePrice, itemCostPrice);
     }
 }
