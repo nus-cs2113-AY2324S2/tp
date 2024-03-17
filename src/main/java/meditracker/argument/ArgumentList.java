1a76ae45b2451a4d46391847f317cdb4a5a3e181
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-03-18 00:51:04 +0800

    feat: Add `ArgumentList` for Command classes
    Command classes can specify supported `Argument`

---
+++ src/main/java/meditracker/argument/ArgumentList.java
@@ -0,0 +1,23 @@
+package meditracker.argument;
+
+import meditracker.exception.ArgumentNotFoundException;
+
+import java.util.List;
+import java.util.Map;
+
+public class ArgumentList {
+    private final List<Argument> ARGUMENTS;
+
+    public ArgumentList(Argument... arguments) {
+        ARGUMENTS = List.of(arguments);
+    }
+
+    public Map<String, String> parse(String rawInput) throws ArgumentNotFoundException {
+        ArgumentParser argumentParser = new ArgumentParser(this, rawInput);
+        return argumentParser.PARSED_ARGUMENTS;
+    }
+
+    public List<Argument> getArguments() {
+        return ARGUMENTS;
+    }
+}
