ed67c414cf9f107650f9f2fb251fb66e0babeb7a
JianJiaT <tjjtjn@gmail.com>
2024-03-17 15:12:54 +0800

    Tweak AddParser

--- src/main/java/brokeculator/parser/AddParser.java
+++ src/main/java/brokeculator/parser/AddParser.java
@@ -14,20 +14,20 @@ public class AddParser {
     }
 
     public AddCommand parseInput(String userInput) {
-        int expenseDescriptionEndIndex = userInput.indexOf(":");
-        int expenseDateBeginIndex = userInput.indexOf("(");
-        int expenseDateEndIndex = userInput.indexOf(")");
-        int expenseAmountBeginIndex = userInput.indexOf("$");
-        int expenseAmountEndIndex = expenseDateBeginIndex - 2;
-        int expenseCategoryBeginIndex = userInput.indexOf("[");
-        int expenseCategoryEndIndex = userInput.indexOf("]");
+        int expenseCategoryBeginIndex = userInput.indexOf("/c");
+        int expenseAmountBeginIndex = userInput.indexOf("/a");
+        int expenseAmountEndIndex = expenseCategoryBeginIndex - 2;
+        int expenseDateBeginIndex = userInput.indexOf("/d");
+        int expenseDateEndIndex = expenseAmountBeginIndex - 2;
+        int expenseDescriptionBeginIndex = userInput.indexOf("/n");
+        int expenseDescriptionEndIndex = expenseDateEndIndex - 2;
 
-        String expenseDescription = userInput.substring(0, expenseDescriptionEndIndex);
+        String expenseDescription = userInput.substring(expenseDescriptionBeginIndex, expenseDescriptionEndIndex);
         String expenseDateAsString = userInput.substring(expenseDateBeginIndex, expenseDateEndIndex);
         LocalDateTime expenseDate = LocalDateTime.parse(expenseDateAsString);
         String expenseAmountAsString = userInput.substring(expenseAmountBeginIndex, expenseAmountEndIndex);
         double expenseAmount = Double.parseDouble(expenseAmountAsString);
-        String expenseCategory = userInput.substring(expenseCategoryBeginIndex, expenseCategoryEndIndex);
+        String expenseCategory = userInput.substring(expenseCategoryBeginIndex);
 
         Expense expenseToAdd = new Expense(expenseDescription, expenseAmount, expenseDate, expenseCategory);
         return new AddCommand(expenseToAdd, expenseManager);
