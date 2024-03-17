73cca85928cde0729c7224cb75c42092b2b354e8
imanamirshah <iman.amirshah@gmail.com>
2024-03-17 20:29:48 +0800

    Add additional attributes to Item class

--- src/main/java/seedu/binbash/Item.java
+++ src/main/java/seedu/binbash/Item.java
@@ -3,10 +3,21 @@ package seedu.binbash;
 public class Item {
     private final String itemName;
     private final String itemDescription;
+    private final int itemQuantity;
+    private final String itemExpirationDate;
+    private final double itemSalePrice;
+    private final double itemCostPrice;
 
-    public Item(String itemName, String itemDescription) {
+
+
+    public Item(String itemName, String itemDescription, int itemQuantity, String itemExpirationDate,
+                double itemSalePrice, double itemCostPrice) {
         this.itemName = itemName;
         this.itemDescription = itemDescription;
+        this.itemQuantity = itemQuantity;
+        this.itemExpirationDate = itemExpirationDate;
+        this.itemSalePrice = itemSalePrice;
+        this.itemCostPrice = itemCostPrice;
     }
 
     public String getItemName() {
@@ -17,8 +28,31 @@ public class Item {
         return itemDescription;
     }
 
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+    public String getItemExpirationDate() {
+        return itemExpirationDate;
+    }
+
+    public double getItemSalePrice() {
+        return itemSalePrice;
+    }
+
+    public double getItemCostPrice() {
+        return itemCostPrice;
+    }
+
     @Override
     public String toString() {
-        return itemName + ": " + itemDescription;
+        return String.format(
+                "%s\n \tdescription: %s\n \tquantity: %d\n \texpiry date: %s\n \tsale price: $%.2f\n \tcost price: $%.2f",
+                itemName,
+                itemDescription,
+                itemQuantity,
+                itemExpirationDate,
+                itemSalePrice,
+                itemCostPrice
+        );
     }
 }
