dde4c2555e6751c933a3aff6a96471c5097f20c8
wjunjie01 <junjiework48@gmail.com>
2024-03-18 00:28:37 +0800

    Add JUnit test for empty list

--- src/test/java/seedu/stockpal/commands/ListCommandTest.java
+++ src/test/java/seedu/stockpal/commands/ListCommandTest.java
@@ -1,12 +1,37 @@
 package seedu.stockpal.commands;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import seedu.stockpal.data.ProductList;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ListCommandTest {
+    private final ProductList emptyProductList = new ProductList();
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    //Standard output stream will be redirected to variable output, temporarily
+    @BeforeEach
+    public void setUpStream() {
+        System.setOut(new PrintStream(output));
+    }
+
+    //Reset to the original state
+    @AfterEach
+    public void restoreStream() {
+        System.setOut(originalOut);
+    }
     @Test
-    public void sampleTest() {
-        assertTrue(true);
+    public void emptyListTest() {
+        ListCommand command = new ListCommand(emptyProductList);
+        command.execute();
+        String expected = "ProductList is empty\n".trim();
+        //System.out.println(output);
+        assertEquals(expected, output.toString().trim());
     }
 }
