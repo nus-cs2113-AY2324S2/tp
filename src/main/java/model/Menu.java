291dc2eacb712861e10845e67f85d962cb23a947
zhengwinter <e0958239@u.nus.edu>
2024-03-17 17:41:36 +0800

    Refactor wildcard import of logging

--- src/main/java/model/Menu.java
+++ src/main/java/model/Menu.java
@@ -1,7 +1,14 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.logging.*;
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.FileHandler;
+
+
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
