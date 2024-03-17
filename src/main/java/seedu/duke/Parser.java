936174e5653bd6a923bdc1372965efd7e7e16e7e
edwardhumi <edward.humianto@gmail.com>
2024-03-17 16:31:32 +0800

    Add Parser for Info and Edit

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -6,12 +6,17 @@ public class Parser {
     public static String drinkDescription;
     public static int drinkSize;
     public static int waterSize;
+    public static int editMealIndex;
+    public static int editMealSize;
+    public static int editDrinkIndex;
+    public static int editDrinkSize;
+    public static int editWaterSize;
 
     public void parseMeal(String command) {
         int descriptionIndex = command.indexOf("m/") + 2;
         int sizeIndex = command.indexOf("s/") + 2;
         mealDescription = command.substring(descriptionIndex, sizeIndex - 2);
-        mealSize = Integer.parseInt(command.substring(sizeIndex));
+        mealSize = Integer.parseInt(command.substring(sizeIndex).trim());
     }
 
     public void parseDrink(String command) {
@@ -24,5 +29,34 @@ public class Parser {
     public void parseWater(String command) {
         int sizeIndex = command.indexOf("s/") + 2;
         waterSize = Integer.parseInt(command.substring(sizeIndex).trim());
+    }
+
+    public String parseInfoMeal(String command) {
+        int mealIndex = 9;
+        return command.substring(mealIndex).trim();
+    }
+
+    public String parseInfoDrink(String command) {
+        int drinkIndex = 10;
+        return command.substring(drinkIndex).trim();
+    }
+
+    public void parseEditMeal(String command) {
+        int mealSizePosition = command.indexOf("s/");
+        int mealIndexPosition = 20;
+        editMealIndex = Integer.parseInt(command.substring(mealIndexPosition, mealSizePosition).trim());
+        editMealSize = Integer.parseInt(command.substring(mealSizePosition + 2).trim());
+    }
+
+    public void parseEditDrink(String command) {
+        int drinkSizePosition = command.indexOf("s/");
+        int drinkIndexPosition = 21;
+        editDrinkIndex = Integer.parseInt(command.substring(drinkSizePosition, drinkIndexPosition).trim());
+        editDrinkSize = Integer.parseInt(command.substring(drinkIndexPosition + 2).trim());
+    }
+
+    public void parseEditWater(String command) {
+        int waterSizePosition = command.indexOf("s/") + 2;
+        editWaterSize = Integer.parseInt(command.substring(waterSizePosition).trim());
     }
 }
