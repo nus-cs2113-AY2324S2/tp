83fa72b8953fb10528d7a9ac49dcec21620a7f8f
JingHaoooo <jinghaoooo17@gmail.com>
2024-03-18 01:57:24 +0800

    Add method to calculate total time elapsed for focus feature

--- src/main/java/focus/FocusTimer.java
+++ src/main/java/focus/FocusTimer.java
@@ -2,6 +2,7 @@ package focus;
 
 import ui.Ui;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class FocusTimer {
@@ -18,5 +19,16 @@ public class FocusTimer {
     public static void setStopTiming() {
         stopTiming = LocalDateTime.now();
         isStarted = false;
+        totalTimeSpent();
+    }
+
+    public static void totalTimeSpent() {
+        Duration timeElapsed = Duration.between(startTiming,stopTiming);
+        long hours = timeElapsed.toHours();
+        long minutes = timeElapsed.toMinutes();
+        long seconds = timeElapsed.toSeconds();
+        Ui.printMessageWithSepNewLine("Your focus session has ended.\n" + " Time spent: " +
+                hours + " hours, " + minutes + " minutes, " + seconds + " seconds" + "\n" +
+                "To start a new session, use ‘focustimer start’ ");
     }
 }
