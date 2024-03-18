26fdad1f73d079acc4b69fb52f763f5035561bf8
LAPTOP-D31L41D6\elyovs <elyovsun@gmail.com>
2024-03-18 23:51:22 +0800

    Factor out code from main to Parser class

--- src/main/java/bookmarked/BookMarked.java
+++ src/main/java/bookmarked/BookMarked.java
@@ -1,12 +1,11 @@
 package bookmarked;
 
 import bookmarked.Book;
+import bookmarked.parser.Parser;
 import bookmarked.ui.Ui;
 
 import java.util.Scanner;
 import java.util.ArrayList;
-
-import static bookmarked.ui.Ui.greetings;
 
 public class BookMarked {
 
@@ -20,60 +19,9 @@ public class BookMarked {
         Scanner in = new Scanner(System.in);
         String newItem = in.nextLine();
 
-        while (!newItem.equalsIgnoreCase("bye")) {
-            String[] splitItem = newItem.split(" ");
-            if (splitItem[0].matches("/help")) {
-                Ui.printHelpMessage();
-            } else if (splitItem[0].matches("list")) {
-                printList(numberOfBooks);
-            } else if (splitItem[0].matches("add")) {
-                String[] newSplitBook = newItem.split("add");
-                listOfBooks.add(new Book(splitItem[1]));
-                System.out.println("Added!");
-                numberOfBooks++;
-            } else if (splitItem[0].matches("delete")) {
-                deleteBook(splitItem);
-            } else if (splitItem[0].matches("borrow")) {
-                borrowBook(splitItem);
-            } else if (splitItem[0].matches("return")) {
-                returnBook(splitItem);
-            }
-
-            Ui.separateNextInput();
-            newItem = in.nextLine();
-
-        }
+        Parser.runCommand(newItem, in, listOfBooks);
         Ui.exitProgramme();
 
-    }
-    
-    public static void printList(int numberOfBooks) {
-        System.out.println("Here are your list items!");
-        for (int i = 0; i < numberOfBooks; i++) {
-            System.out.println((i + 1) + ". " + listOfBooks.get(i).toString());
-        }
-    }
-
-    public static void deleteBook(String[] bookToDelete) {
-        int inputtedIndex = Integer.parseInt(bookToDelete[1]);
-        int listNumberIndex = (inputtedIndex - 1);
-        System.out.println("Deleted!");
-        listOfBooks.remove(listNumberIndex);
-        numberOfBooks--;
-    }
-
-    public static void borrowBook(String[] bookToBorrow) {
-        int inputtedIndex = Integer.parseInt(bookToBorrow[1]);
-        int listNumberIndex = (inputtedIndex - 1);
-        System.out.println("Borrowed!");
-        listOfBooks.get(listNumberIndex).isBorrowed = true;
-    }
-
-    public static void returnBook(String[] bookToReturn) {
-        int inputtedIndex = Integer.parseInt(bookToReturn[1]);
-        int listNumberIndex = (inputtedIndex - 1);
-        System.out.println("Returned!");
-        listOfBooks.get(listNumberIndex).isBorrowed = false;
     }
 
 }
