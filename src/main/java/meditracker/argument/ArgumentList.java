c39d1bde8d433805fa6e5f3d5d0027e9dad8d9f7
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-03-18 10:09:05 +0800

    test: Add assertion to test for flag collision
    Useful in identifying commands with flag collisions

--- src/main/java/meditracker/argument/ArgumentList.java
+++ src/main/java/meditracker/argument/ArgumentList.java
@@ -2,14 +2,24 @@ package meditracker.argument;
 
 import meditracker.exception.ArgumentNotFoundException;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class ArgumentList {
     private final List<Argument> arguments;
 
     public ArgumentList(Argument... arguments) {
         this.arguments = List.of(arguments);
+
+        // assertion test: check for flag collisions
+        Set<String> flags = new HashSet<>();
+        for (Argument argument: arguments) {
+            String flag = argument.getFlag();
+            assert !flags.contains(flag);
+            flags.add(flag);
+        }
     }
 
     public Map<ArgumentName, String> parse(String rawInput) throws ArgumentNotFoundException {
