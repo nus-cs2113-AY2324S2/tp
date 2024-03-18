b6274370743734ff482d4341b1406c668a3f19a2
leongxingyu <leongxingyu02@gmail.com>
2024-03-18 20:10:31 +0800

    Add JUnit testing for InflowCommand

--- src/test/java/seedu/stockpal/data/product/QuantityTest.java
+++ src/test/java/seedu/stockpal/data/product/QuantityTest.java
@@ -2,6 +2,7 @@ package seedu.stockpal.data.product;
 
 import org.junit.jupiter.api.Test;
 import seedu.stockpal.exceptions.InsufficientAmountException;
+import seedu.stockpal.exceptions.InventoryQuantityOverflowException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -16,7 +17,23 @@ class QuantityTest {
     @Test
     public void updateIncreaseQuantity_anyInteger_success() {
         Quantity quantityObject = new Quantity(0);
-        quantityObject.updateIncreaseQuantity(10);
+        try {
+            quantityObject.updateIncreaseQuantity(10);
+        } catch (InventoryQuantityOverflowException IQOE) {
+            fail();
+        }
+        assertEquals(10, quantityObject.quantity);
+    }
+
+    @Test
+    public void updateIncreaseQuantity_anyInteger_inventoryQuantityOverflowExceptionThrown() {
+        Quantity quantityObject = new Quantity(10);
+        try {
+            quantityObject.updateIncreaseQuantity(Integer.MAX_VALUE);
+            fail("Expected InventoryQuantityOverflowException was not thrown");
+        } catch (InventoryQuantityOverflowException IQOE) {
+            assertEquals("Overflow detected. No Change to quantity.", IQOE.getMessage());
+        }
         assertEquals(10, quantityObject.quantity);
     }
 
@@ -25,7 +42,7 @@ class QuantityTest {
         Quantity quantityObject = new Quantity(10);
         try {
             quantityObject.updateDecreaseQuantity(5);
-        } catch (InsufficientAmountException e) {
+        } catch (InsufficientAmountException IAE) {
             fail();
         }
         assertEquals(5, quantityObject.quantity);
@@ -37,8 +54,8 @@ class QuantityTest {
         try {
             quantityObject.updateDecreaseQuantity(15);
             fail("Expected InsufficientAmountException was not thrown");
-        } catch (InsufficientAmountException e) {
-            assertEquals("Insufficient amount in inventory", e.getMessage());
+        } catch (InsufficientAmountException IAE) {
+            assertEquals("Insufficient amount in inventory", IAE.getMessage());
         }
         assertEquals(10, quantityObject.quantity);
     }
