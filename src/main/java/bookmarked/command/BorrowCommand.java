d189959ef44090c3a5c4c6f93cf632919ac673f1
LAPTOP-D31L41D6\elyovs <elyovsun@gmail.com>
2024-03-19 00:29:02 +0800

    Add class for mark as borrow command

---
+++ src/main/java/bookmarked/command/BorrowCommand.java
@@ -0,0 +1,23 @@
+package bookmarked.command;
+
+import bookmarked.Book;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class BorrowCommand extends Command {
+    String[] bookToBorrow;
+    ArrayList<Book> listOfBooks;
+    public BorrowCommand(String[] bookToBorrow, ArrayList<Book> listOfBooks) {
+        this.bookToBorrow = bookToBorrow;
+        this.listOfBooks = listOfBooks;
+    }
+
+    @Override
+    public void handleCommand() {
+        int inputtedIndex = Integer.parseInt(this.bookToBorrow[1]);
+        int listNumberIndex = (inputtedIndex - 1);
+        System.out.println("Borrowed!");
+        this.listOfBooks.get(listNumberIndex).setBorrowed();
+    }
+}
