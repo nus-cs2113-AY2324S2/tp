4e6a90567974aac710215bc0ae7e551ab998c102
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-03-18 22:45:38 +0800

    docs: Edit typo in javadoc

--- src/main/java/meditracker/medication/MedicationManager.java
+++ src/main/java/meditracker/medication/MedicationManager.java
@@ -46,7 +46,7 @@ public class MedicationManager {
      * Gets the Medication object from the medications list.
      * Also converts the index to 0-based indexing before being used.
      *
-     * @param listIndex Index of the medications list to update (1-based indexing)
+     * @param listIndex Index of the medications list to get (1-based indexing)
      * @return Medication object at the corresponding index (0-based indexing)
      * @throws IndexOutOfBoundsException Out of range index specified
      */
