ebccd8edf610610fda9978da851cdaaedb6fa168
Kishen271828 <kg240904kg@gmail.com>
2024-03-18 21:38:34 +0800

    Wrap long lines in ParserTest class

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -11,14 +11,18 @@ public class ParserTest {
     @Test
     public void addInflow_success() {
         Parser test1 = new Parser();
-        assertEquals("Ok. Added inflow", test1.parseCommand("add-inflow n/Salary a/5000 d/14/03/2024 t/1700", manager));
-        assertEquals("Ok. Added inflow", test1.parseCommand("add-inflow n/Investment a/600 d/03/03/2024 t/1900", manager));
+        assertEquals("Ok. Added inflow",
+                     test1.parseCommand("add-inflow n/Salary a/5000 d/14/03/2024 t/1700", manager));
+        assertEquals("Ok. Added inflow",
+                     test1.parseCommand("add-inflow n/Investment a/600 d/03/03/2024 t/1900", manager));
     }
 
     public void addOutflow_success() {
         Parser test1 = new Parser();
-        assertEquals("Ok. Added outflow", test1.parseCommand("add-outflow n/Rent a/1000 d/18/02/2024 t/1100", manager));
-        assertEquals("Ok. Added outflow", test1.parseCommand("add-outflow n/Shopping a/150 d/30/01/2024 t/1430", manager));
+        assertEquals("Ok. Added outflow",
+                     test1.parseCommand("add-outflow n/Rent a/1000 d/18/02/2024 t/1100", manager));
+        assertEquals("Ok. Added outflow",
+                     test1.parseCommand("add-outflow n/Shopping a/150 d/30/01/2024 t/1430", manager));
     }
 
 }
