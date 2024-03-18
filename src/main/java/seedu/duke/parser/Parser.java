e484d7fa6290c4b4ee56f94c5d4f451c06f331f7
sxfoo <fsx159@gmail.com>
2024-03-18 11:06:46 +0800

    Remove grade command pattern to detect double values
    Removal reason: used to detect 5.0, 4.5 grade points etc, but not possible to differentiate A and A+

--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -30,7 +30,7 @@ public class Parser {
                     "\\s+w/(?<semester>[1-9]|10)\\s+m/(?<mc>[1-9]|1[0-2])", Pattern.CASE_INSENSITIVE);
     private static final Pattern GRADE_PATTERN =
             Pattern.compile("grade\\s+c/(?<courseCode>[A-Za-z]{2,3}\\d{4}[A-Za-z]?)" +
-                    "\\s+g/(?<grade>[ab][+-]?|[cd][+]?|f|[1-4](?:\\.0|\\.5)?|[05]\\.0?)", Pattern.CASE_INSENSITIVE);
+                    "\\s+g/(?<grade>[ab][+-]?|[cd][+]?|f|cs|cu)", Pattern.CASE_INSENSITIVE);
 
     // Argument Group captures
     private static final String[] INIT_ARGUMENTS = {"name"};
