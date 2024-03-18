fbb8d12ece53ff6408923531c0b431217f72471b
Jason Lienardi <lienardijason@gmail.com>
2024-03-18 23:15:08 +0800

    Add FitNus class
    Added FitNus class and made the necessary modifications to other classes

--- src/main/java/seedu/duke/FitNus.java
+++ src/main/java/seedu/duke/FitNus.java
@@ -1,4 +1,26 @@
 package seedu.duke;
 
+import seedu.duke.user.User;
+
 public class FitNus {
+
+    private static Ui ui;
+
+    public FitNus() {
+        ui = new Ui();
+    }
+
+    /** Runs the program until termination.  */
+    public void run() {
+        ui.printWelcomeMessage();
+        while (!ui.isExit) {
+            ui.readCommand();
+        }
+    }
+
+    public static void main(String[] args) {
+        new FitNus().run();
+    }
+
+
 }
--- src/main/java/seedu/duke/user/User.java
+++ src/main/java/seedu/duke/user/User.java
@@ -18,7 +18,7 @@ public class User {
         totalWaterIntake = new ArrayList<>();
     }
 
-    public static void handleMeal(String command) {
+    public void handleMeal(String command) {
         Parser.parseMeal(command);
         String mealName = Parser.mealDescription;
         int servingSize = Parser.mealSize;
@@ -27,16 +27,23 @@ public class User {
         System.out.println("Added " + servingSize + " serving of " + mealName);
     }
 
-    public static void handleDrink(String command) {
+    public void handleDrink(String command) {
         Parser.parseDrink(command);
         String drinkName = Parser.drinkDescription;
         int servingSize = Parser.drinkSize;
 
-        drinkList.add(new Drink(drinkName, servingSize)); //TODO: Drink constructor needs 8 arguments
-        System.out.println("Added " + servingSize + " serving of " + drinkName);
+        drinkList.add(new Drink(drinkName, servingSize));
+        System.out.println("Added " + servingSize + " ml of " + drinkName);
     }
 
-    public static void handleViewCalories() {
+    public void handleWater(String command) {
+        Parser.parseWater(command);
+        int volume = Parser.waterSize;
+
+        totalWaterIntake.add(new Water(volume));
+        System.out.println("Added " + volume + " ml of water");
+    }
+    public void handleViewCalories() {
         int caloriesCount = 0;
         for (Meal meal: mealList) {
             caloriesCount += meal.getCalories();
@@ -47,7 +54,7 @@ public class User {
         System.out.println("Total Calories: " + caloriesCount);
     }
 
-    public static void handleViewCarbohydrates() {
+    public void handleViewCarbohydrates() {
         int carbohydratesCount = 0;
         for (Meal meal: mealList) {
             carbohydratesCount += meal.getCarbs();
@@ -58,7 +65,7 @@ public class User {
         System.out.println("Total Carbohydrates: " + carbohydratesCount);
     }
 
-    public static void handleViewProteins() {
+    public void handleViewProteins() {
         int proteinCount = 0;
         for (Meal meal: mealList) {
             proteinCount += meal.getProtein();
@@ -69,29 +76,52 @@ public class User {
         System.out.println("Total Proteins: " + proteinCount);
     }
 
-    public static void handleViewWaterIntake() {
+    public void handleViewWaterIntake() {
         int waterIntake = 0;
         for (Water water: totalWaterIntake) {
             waterIntake += water.getWater();
         }
-        System.out.println("Total water intake: " + waterIntake);
+        System.out.println("Total water intake: " + waterIntake + " ml");
     }
 
-    public static void handleViewFiber() {
+    public void handleViewFiber() {
         int fibreCount = 0;
         for (Meal meal: mealList) {
             fibreCount += meal.getFiber();
         }
-        System.out.println("Total Proteins: " + fibreCount);
+        System.out.println("Total Fiber: " + fibreCount);
     }
 
-    public static void printMealList(int startingIndex) {
-        for (int i = startingIndex; i <= mealList.size(); i++) {
-            Meal currentMeal = mealList.get(i);
-            System.out.print(i + ". " + currentMeal.getName());
+    public void handleViewFat() {
+        int fatCount = 0;
+        for (Meal meal: mealList) {
+            fatCount += meal.getFat();
         }
+        for (Drink drink: drinkList) {
+            fatCount += drink.getFat();
+        }
+        System.out.println("Total Fat: " + fatCount);
     }
-    public static void handleListMeals() {
+
+    public void handleViewSugar() {
+        int sugarCount = 0;
+        for (Meal meal: mealList) {
+            sugarCount += meal.getSugar();
+        }
+        for (Drink drink: drinkList) {
+            sugarCount += drink.getSugar();
+        }
+        System.out.println("Total Sugar: " + sugarCount);
+    }
+
+    public void printMealList(int startIndex) {
+        for (int i = 0; i < mealList.size(); i++) {
+            Meal currentMeal = mealList.get(i);
+            System.out.print((startIndex+i) + ". " + currentMeal.getName());
+        }
+        System.out.println();
+    }
+    public void handleListMeals() {
         System.out.println("here's what you have eaten today");
         if (mealList.isEmpty()) {
             System.out.println("  >> nothing so far :o");
@@ -100,35 +130,37 @@ public class User {
         }
     }
 
-    public static void printDrinkList(int startingIndex) {
-        for (int i = startingIndex; i <= drinkList.size(); i++) {
+    public void printDrinkList(int startIndex) {
+        for (int i = 0; i < drinkList.size(); i++) {
             Drink currentDrink = drinkList.get(i);
-            System.out.print(i + ". " + currentDrink.getName());
+            System.out.print((startIndex+i) + ". " + currentDrink.getName());
         }
     }
 
-    public static void handleListDrinks() {
+    public void handleListDrinks() {
         System.out.println("here's what you have drank today");
         if (drinkList.isEmpty()) {
             System.out.println("  >> nothing so far :o");
         } else {
             printDrinkList(1);
+            System.out.println();
             handleViewWaterIntake();
         }
     }
 
-    public static void handleListEverything() {
+    public void handleListEverything() {
         System.out.println("here's what you have drank today");
         if (drinkList.isEmpty() && mealList.isEmpty()) {
             System.out.println("  >> nothing so far :o");
         } else {
             printMealList(1);
-            printDrinkList(mealList.size() + 1);
+            printDrinkList(mealList.size()+1);
+            System.out.println();
             handleViewWaterIntake();
         }
     }
 
-    public static void handleDeleteMeal(String command) {
+    public void handleDeleteMeal(String command) {
         int mealIndex = Integer.parseInt(command.substring(11));
         String mealName = mealList.get(mealIndex).getName();
         mealList.remove(mealIndex);
@@ -136,14 +168,14 @@ public class User {
         System.out.println("Removed " + mealName + " from Meals");
     }
 
-    public static void handleDeleteDrink(String command) {
+    public void handleDeleteDrink(String command) {
         int drinkIndex = Integer.parseInt(command.substring(12));
         String drinkName = drinkList.get(drinkIndex).getName();
         drinkList.remove(drinkIndex);
         System.out.println("Removed " + drinkName + " from Meals");
     }
 
-    public static void handleClear() {
+    public void handleClear() {
         mealList.clear();
         drinkList.clear();
         System.out.println("All entries have been deleted");
