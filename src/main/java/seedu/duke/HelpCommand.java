48caa9eab52a14d3c0d91edb9f9aa70ab4e279fa
soongensayo <e0959972@u.nus.edu>
2024-03-18 13:51:44 +0800

    Fix bugs in Itinerary

--- src/main/java/seedu/duke/HelpCommand.java
+++ src/main/java/seedu/duke/HelpCommand.java
@@ -6,7 +6,6 @@ public class HelpCommand extends Command {
     @Override
     public void execute(FavouritesList favourites, FoodList foods, ActivityList activities, Ui ui,
                         Storage storage) throws FlirtForkException {
-        
         System.out.println("I know you’re excited to Flirt & Fork :) Here's how: \n");
         ui.helpMessage();
     }
