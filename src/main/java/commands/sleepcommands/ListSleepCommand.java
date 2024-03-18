b7e6a67096bac3a491bef17c638be5bcdee10117
OKW32 <e0969674@u.nus.edu>
2024-03-18 09:32:10 +0800

    Add exception handling for SleepTracker
    Listing of sleep cycles could be called with improper list format.
    
    Let's check if proper format is keyed in when user wants to list sleep
    cycles by throwing an exception when improper format is detected.

--- src/main/java/commands/sleepcommands/ListSleepCommand.java
+++ src/main/java/commands/sleepcommands/ListSleepCommand.java
@@ -1,13 +1,20 @@
 package commands.sleepcommands;
 
 import commands.Command;
+import exceptions.SleepException;
 import sleep.SleepTracker;
 
-public class ListSleepCommand implements Command  {
-    SleepTracker sleepTracker;
-    public ListSleepCommand(SleepTracker sleepTracker) {
+public class ListSleepCommand implements Command {
+    private SleepTracker sleepTracker;
+
+    public ListSleepCommand(SleepTracker sleepTracker, String sleepCommandArgs) throws SleepException {
+        if (!sleepCommandArgs.isEmpty()) {
+            throw new SleepException("Please use proper format: " + System.lineSeparator()
+                    + "sleep list");
+        }
         this.sleepTracker = sleepTracker;
     }
+
     @Override
     public void execute() {
         sleepTracker.listSleepCycles();
