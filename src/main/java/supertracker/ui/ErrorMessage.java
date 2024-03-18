5db17e083628839d6ec443465c9502aa81add4f7
awesomesjh <simjunhong@gmail.com>
2024-03-18 19:26:53 +0800

    Move error message to new line

--- src/main/java/supertracker/ui/ErrorMessage.java
+++ src/main/java/supertracker/ui/ErrorMessage.java
@@ -7,9 +7,11 @@ public class ErrorMessage {
     public static final String INVALID_DELETE_FORMAT = "Invalid delete command format!";
     public static final String INVALID_LIST_FORMAT = "Invalid list command format!";
     public static final String INVALID_NEW_ITEM_FORMAT = "Invalid new command format!";
-    public static final String ITEM_NOT_IN_LIST_UPDATE = " does not exist in inventory. Unable to update its values. =(";
+    public static final String ITEM_NOT_IN_LIST_UPDATE =
+            " does not exist in inventory. Unable to update its values. =(";
 
-    public static final String ITEM_NOT_IN_LIST_DELETE = " does not exist in inventory. Unable to delete something that does not exist. =(";
+    public static final String ITEM_NOT_IN_LIST_DELETE =
+            " does not exist in inventory. Unable to delete something that does not exist. =(";
     public static final String ITEM_IN_LIST_NEW = " already exists in inventory. Use the update command instead.";
 
 
