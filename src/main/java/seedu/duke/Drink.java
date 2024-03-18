9bfb7b5d6ba4a3871f93df4429afb93500f03bfd
Bryvo <bryancastorius@gmail.com>
2024-03-18 21:09:56 +0800

    Edit drink  and meal class
    Convert the calculation from per milliliter to per 100 milliliter in drink class.

--- src/main/java/seedu/duke/Drink.java
+++ src/main/java/seedu/duke/Drink.java
@@ -1,24 +1,39 @@
 package seedu.duke;
 
+import java.util.HashMap;
+
 public class Drink {
+    private static HashMap<String, int[]> nutrientDetails = new HashMap<>();
     private String name;
     private int drinkVolume;
-    private int caloriesPerMilliliter;
-    private int carbsPerMilliliter;
-    private int sugarPerMilliliter;
-    private int proteinPerMilliliter;
-    private int fatPerMilliliter;
-    private int sodiumPerMilliliter;
+    private int calories;
+    private int carbs;
+    private int sugar;
+    private int protein;
+    private int fat;
+    private int sodium;
 
-    public Drink(String name, int volume, int calories, int carbs, int protein, int fat, int sugar, int sodium) {
+    public Drink(String name, int volume) {
         this.name = name;
         this.drinkVolume = volume;
-        this.caloriesPerMilliliter = calories;
-        this.carbsPerMilliliter = carbs;
-        this.proteinPerMilliliter = protein;
-        this.fatPerMilliliter = fat;
-        this.sugarPerMilliliter = sugar;
-        this.sodiumPerMilliliter = sodium;
+        setNutrientValues(name);
+    }
+
+    // Add nutrient details per 100 milliliter to the static HashMap
+    static {
+        nutrientDetails.put("sprite", new int[]{40, 50, 30, 20, 2, 5});
+        nutrientDetails.put("lemon tea", new int[]{150, 30, 25, 1, 20, 3});
+        nutrientDetails.put("milk coffee", new int[]{20, 27, 25, 4, 3, 2});
+    }
+
+    private void setNutrientValues(String name) {
+        int[] nutrients = nutrientDetails.get(name);
+        calories = nutrients[0] * drinkVolume / 100;
+        carbs = nutrients[1] * drinkVolume / 100;
+        sugar = nutrients[2] * drinkVolume / 100;
+        protein = nutrients[3] * drinkVolume / 100;
+        fat = nutrients[4] * drinkVolume / 100;
+        sodium = nutrients[5]  * drinkVolume / 100;
     }
 
     public void infoDrink() {
@@ -42,26 +57,26 @@ public class Drink {
     }
 
     public int getCalories() {
-        return caloriesPerMilliliter * drinkVolume;
+        return calories;
     }
 
     public int getCarbs() {
-        return carbsPerMilliliter * drinkVolume;
+        return carbs;
     }
 
     public int getSugar() {
-        return sugarPerMilliliter * drinkVolume;
+        return sugar;
     }
 
     public int getProtein() {
-        return proteinPerMilliliter * drinkVolume;
+        return protein;
     }
 
     public int getFat() {
-        return fatPerMilliliter* drinkVolume;
+        return fat;
     }
 
     public int getSodium() {
-        return sodiumPerMilliliter * drinkVolume;
+        return sodium;
     }
 }
