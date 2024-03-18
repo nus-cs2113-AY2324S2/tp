f38551bb6b6925726c8c9c0aad51f1fbf0aebef6
sxfoo <fsx159@gmail.com>
2024-03-18 09:40:43 +0800

    Add functionality for ViewCommand

--- src/main/java/seedu/duke/command/ViewCommand.java
+++ src/main/java/seedu/duke/command/ViewCommand.java
@@ -1,8 +1,42 @@
 package seedu.duke.command;
 
+import java.util.ArrayList;
+import seedu.duke.modules.Module;
+
 public class ViewCommand extends Command{
+
+    private String mode;
+
+    public ViewCommand(String mode) {
+        this.mode = mode.toLowerCase();
+    }
+
+    private void printModuleList(ArrayList<Module> list) {
+        for (Module module : list) {
+            System.out.print(module.getModuleCode() + ", ");
+        }
+        System.out.println("");
+    }
+
     @Override
     public void execute(String userInput) {
-        System.out.println();
+        switch (mode) {
+        case "all":
+            System.out.println("Taken modules: ");
+            //printModuleList(takenModuleList);
+            System.out.println("Planned modules: ");
+            //printModuleList(planModuleList);
+            break;
+        case "taken":
+            System.out.println("Taken modules: ");
+            //printModuleList(takenModuleList);
+            break;
+        case "plan":
+            System.out.println("Planned modules: ");
+            //printModuleList(planModuleList);
+            break;
+        default:
+            System.out.println("Invalid mode.");
+        }
     }
 }
