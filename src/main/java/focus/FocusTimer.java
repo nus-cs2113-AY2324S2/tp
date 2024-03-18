778286723cc72edaaaa0084674e287ad420f9df9
JingHaoooo <jinghaoooo17@gmail.com>
2024-03-18 20:38:29 +0800

    Resolve seconds and minutes display bug in focus timer

--- src/main/java/focus/FocusTimer.java
+++ src/main/java/focus/FocusTimer.java
@@ -6,6 +6,8 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class FocusTimer {
+    private static final int MINUTES_DIVISION = 60;
+    private static final int SECONDS_DIVISION = 60;
     public LocalDateTime startTiming;
     public LocalDateTime stopTiming;
     public boolean isStarted = false;
@@ -29,8 +31,8 @@ public class FocusTimer {
     public void totalTimeSpent() {
         Duration timeElapsed = Duration.between(startTiming,stopTiming);
         long hours = timeElapsed.toHours();
-        long minutes = timeElapsed.toMinutes();
-        long seconds = timeElapsed.toSeconds();
+        long minutes = timeElapsed.toMinutes() % MINUTES_DIVISION;
+        long seconds = timeElapsed.toSeconds() % SECONDS_DIVISION;
         Ui.printMessageWithSepNewLine("Your focus session has ended.\n" + " Time spent: " +
                 hours + " hours, " + minutes + " minutes, " + seconds + " seconds" + "\n" +
                 "To start a new session, use ‘focus start’ ");
