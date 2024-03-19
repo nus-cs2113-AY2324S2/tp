365c920026d96a59fa79d2ec4a2d3e52bed01dd8
XavierLiau34 <xavyliau@gmail.com>
2024-03-19 22:10:00 +0800

    no message

--- src/main/java/seedu/binbash/Parser.java
+++ src/main/java/seedu/binbash/Parser.java
@@ -1,9 +1,9 @@
 package seedu.binbash;
 
 import java.util.Objects;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 
 import seedu.binbash.command.Command;
 import seedu.binbash.command.AddCommand;
@@ -16,6 +16,7 @@ import seedu.binbash.exceptions.InvalidArgumentException;
 import seedu.binbash.exceptions.InvalidFormatException;
 
 public class Parser {
+    private static final Logger LOGGER = Logger.getLogger(Parser.class.getName());
     private final ItemList itemList;
 
     public Parser(ItemList itemList) {
@@ -23,6 +24,8 @@ public class Parser {
     }
 
     public Command parseCommand(String userInput) throws InvalidCommandException {
+        assert userInput != null : "User input should not be null";
+
         String[] tokens = userInput.trim().split("\\s+", 2);
         String commandString = tokens[0].toLowerCase();
         String arguments = tokens.length > 1 ? tokens[1] : "";
@@ -30,27 +33,36 @@ public class Parser {
         try {
             switch (commandString) {
             case "bye":
+                LOGGER.info("Parsing Bye command");
                 return new ByeCommand(itemList);
             case "add":
+                LOGGER.info("Parsing Add command");
                 return parseAddCommand(userInput);
             case "delete":
+                LOGGER.info("Parsing Delete command");
                 return parseDeleteCommand(userInput);
             case "list":
+                LOGGER.info("Parsing List command");
                 return parseListCommand(userInput);
             case "search":
+                LOGGER.info("Parsing Search command");
                 return parseSearchCommand(userInput);
             default:
                 throw new InvalidCommandException("Invalid command!");
             }
         } catch (InvalidCommandException e) {
+            LOGGER.warning("Invalid command: " + e.getMessage());
             throw e;
         }
     }
 
     private Command parseDeleteCommand(String userInput) throws InvalidFormatException,
             InvalidArgumentException {
+        assert userInput != null : "User input should not be null";
+
         Matcher argumentMatcher = DeleteCommand.COMMAND_FORMAT.matcher(userInput);
         if (!argumentMatcher.matches()) {
+            LOGGER.warning("Delete command is not properly formatted!");
             throw new InvalidFormatException("Delete command is not properly formatted!");
         }
 
@@ -72,8 +84,11 @@ public class Parser {
     }
 
     private Command parseAddCommand(String userInput) throws InvalidFormatException {
+        assert userInput != null : "User input should not be null";
+
         Matcher matcher = AddCommand.COMMAND_FORMAT.matcher(userInput);
         if (!matcher.matches()) {
+            LOGGER.warning("Add command is not properly formatted!");
             throw new InvalidFormatException("Add command is not properly formatted!");
         }
         String itemName = matcher.group("itemName");
@@ -89,12 +104,15 @@ public class Parser {
         double itemCostPrice = Double.parseDouble(matcher.group("itemCostPrice"));
 
         return new AddCommand(itemList, itemName, itemDescription, itemQuantity, itemExpirationDate, itemSalePrice,
-                    itemCostPrice);
+                itemCostPrice);
     }
 
     private Command parseSearchCommand(String userInput) throws InvalidFormatException {
+        assert userInput != null : "User input should not be null";
+
         Matcher matcher = SearchCommand.COMMAND_FORMAT.matcher(userInput);
         if (!matcher.matches()) {
+            LOGGER.warning("Search command is not properly formatted!");
             throw new InvalidFormatException("Search command is not properly formatted!");
         }
         String keyword = matcher.group("keyword");
@@ -102,7 +120,7 @@ public class Parser {
     }
 
     private Command parseListCommand(String arguments) {
+        assert arguments != null : "Arguments should not be null";
         return new ListCommand(itemList);
     }
 }
-
