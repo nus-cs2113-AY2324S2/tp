5479a7a0b81f5c4e0506cee806a109375a5c101f
E0958902 <E0958902@u.nus.edu>
2024-03-19 22:52:21 +0800

    Add ListCommand

---
+++ src/main/java/meditracker/command/ListCommand.java
@@ -0,0 +1,40 @@
+package meditracker.command;
+
+import meditracker.argument.ArgumentList;
+import meditracker.argument.ArgumentName;
+import meditracker.argument.ListTypeArgument;
+import meditracker.exception.ArgumentNotFoundException;
+import meditracker.medication.MedicationManager;
+import meditracker.ui.Ui;
+
+import java.util.Map;
+
+public class ListCommand extends Command {
+
+    public ArgumentList argumentList = new ArgumentList(
+            new ListTypeArgument(false));
+
+    private final Map<ArgumentName, String> parsedArguments;
+
+    public ListCommand(String arguments) throws ArgumentNotFoundException {
+        parsedArguments = argumentList.parse(arguments);
+    }
+
+    /**
+     * Executes the list command
+     * @param medicationManager List of medicines.
+     * @param ui                Ui object.
+     */
+    @Override
+    public void execute(MedicationManager medicationManager, Ui ui) {
+        String listTypeString = parsedArguments.get(ArgumentName.LIST_TYPE);
+        
+        switch (listTypeString) {
+        case "all":
+            medicationManager.printAllMedications();
+            break;
+        default:
+            throw new IllegalStateException("Unexpected value: " + listTypeString);
+        }
+    }
+}
