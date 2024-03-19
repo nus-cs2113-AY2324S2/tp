2d203939efcd8bd46de4b8a02ef74a3a3e85badf
eliz <tan_eliz@hotmail.com>
2024-03-20 03:17:03 +0800

    Add exceptions to command classes

--- src/main/java/bookmarked/command/DeleteCommand.java
+++ src/main/java/bookmarked/command/DeleteCommand.java
@@ -1,6 +1,10 @@
 package bookmarked.command;
 
 import bookmarked.Book;
+import bookmarked.exceptions.emptyArgumentsException;
+import bookmarked.exceptions.emptyListException;
+import bookmarked.exceptions.indexOutOfListBounds;
+import bookmarked.ui.Ui;
 
 import java.util.ArrayList;
 
@@ -14,9 +18,40 @@ public class DeleteCommand extends Command {
 
     @Override
     public void handleCommand() {
+        try {
+            processDeleteCommand(listOfBooks);
+            assert this.bookToDelete.length >= 1 : "There should be an argument to the command";
+            assert !this.listOfBooks.isEmpty() : "The current list of books should not be empty";
+        } catch (emptyListException e) {
+            Ui.printEmptyListMessage();
+        } catch (emptyArgumentsException e) {
+            Ui.printEmptyArgumentsMessage();
+        } catch (indexOutOfListBounds e) {
+            Ui.printOutOfBoundsMessage();
+        } catch (NumberFormatException e) {
+            Ui.printNotNumberMessage();
+        }
+    }
+
+    public void processDeleteCommand(ArrayList<Book> listOfBooks)
+            throws emptyListException, emptyArgumentsException, indexOutOfListBounds {
+        if (listOfBooks.isEmpty()) {
+            throw new emptyListException();
+        }
+
+        // checks if bookToDelete contains only the word "delete" or if there are only white spaces after it
+        if (this.bookToDelete.length < 1 || this.bookToDelete[1].isBlank()) {
+            throw new emptyArgumentsException();
+        }
+
         int inputtedIndex = Integer.parseInt(this.bookToDelete[1]);
+        if (inputtedIndex <= 0 || inputtedIndex > listOfBooks.size()) {
+            throw new indexOutOfListBounds();
+        }
+
         int listNumberIndex = (inputtedIndex - 1);
         System.out.println("Deleted!");
         this.listOfBooks.remove(listNumberIndex);
     }
+
 }
