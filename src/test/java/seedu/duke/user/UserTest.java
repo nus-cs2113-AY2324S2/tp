ff86106ebeedb143be1d50115e78d4da5c087baa
Claribel Ho <hoclaribel@gmail.com>
2024-03-19 22:05:51 +0800

    Add JUnit test for some user class methods

---
+++ src/test/java/seedu/duke/user/UserTest.java
@@ -0,0 +1,48 @@
+package seedu.duke.user;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.Drink;
+import seedu.duke.Meal;
+import seedu.duke.Parser;
+import seedu.duke.Water;
+import seedu.duke.exception.IncompleteMealException;
+import seedu.duke.exception.IncompleteWaterException;
+import seedu.duke.exception.UnregisteredMealException;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserTest {
+    ArrayList<Meal> testMealList = new ArrayList<>();
+    ArrayList<Drink> testDrinkList = new ArrayList<>();
+
+    @Test
+    public void sampleUser() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void handleMeal_validInputs_correctlyAddMeal() throws IncompleteMealException, UnregisteredMealException {
+        Meal newMeal = new Meal("pizza", 3);
+        testMealList.add(newMeal);
+
+        assertEquals("pizza", testMealList.get(0).getName());
+        assertEquals(3, testMealList.get(0).getServingSize());
+    }
+
+    @Test
+    public void handleWater_unknownServingSize_addWaterFailed() throws IncompleteWaterException {
+        try {
+            Parser.parseWater("water 1");
+            new Water(Parser.waterSize);
+        } catch (IncompleteWaterException e) {
+            return;
+        }
+
+        String error = "Incomplete command, the format must be [water s/SERVING_SIZE].";
+        fail(error);
+    }
+}
