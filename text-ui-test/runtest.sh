bebf3e0e96845ac86d2ef6c20b7692e35e1c8b07
EugeneChanJiajun <e0969415@u.nus.edu>
2024-03-19 21:41:05 +0800

    fix test

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -8,7 +8,7 @@ cd ..
 
 cd text-ui-test
 
-java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL-UNIX.TXT
+java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
 
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix EXPECTED-UNIX.TXT ACTUAL.TXT
