e4fe2d509310be7977dedacf74398d8ead32851f
Ying Jia Soh <>
2024-03-19 18:35:05 +0800

    Add ArrayList<Period> to track all periods added

--- src/main/java/health/HealthList.java
+++ src/main/java/health/HealthList.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class HealthList extends ArrayList<Health> {
     private static final ArrayList<Bmi> bmis = new ArrayList<>();
+    private static final ArrayList<Period> periods = new ArrayList<>();
 
     public static void addBmi(Bmi bmi) {
         bmis.add(bmi);
@@ -18,5 +19,14 @@ public class HealthList extends ArrayList<Health> {
         for (Bmi bmi : bmis) {
             System.out.println(bmi);
         }
+    }
+
+    public static void addPeriod(Period period) {
+        periods.add(period);
+    }
+
+    public static void showLatestPeriod() {
+        int currentIndex = periods.size();
+        System.out.println(periods.get(currentIndex - 1));
     }
 }
