9df754284c09d74e73f9bd3da10f0d923254de2f
Zhang Yangda <zhang.yangda16@gmail.com>
2024-03-19 16:38:15 +0800

    Add logger to addSavingCommand

--- src/main/java/seedu/budgetbuddy/command/AddSavingCommand.java
+++ src/main/java/seedu/budgetbuddy/command/AddSavingCommand.java
@@ -2,7 +2,12 @@ package seedu.budgetbuddy.command;
 
 import seedu.budgetbuddy.SavingList;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 public class AddSavingCommand extends Command {
+    private static final Logger LOGGER = Logger.getLogger(AddSavingCommand.class.getName());
+
     private SavingList savings;
     private final String category;
     private final String amount;
@@ -15,7 +20,15 @@ public class AddSavingCommand extends Command {
 
     @Override
     public void execute() {
-        savings.addSaving(this.category,this.amount);
-        System.out.println("Savings Added to:" + category + " of $" + amount);
+        try {
+            LOGGER.log(Level.INFO, "Adding savings to category: {0} with amount: ${1}", new Object[]{category, amount});
+
+            savings.addSaving(this.category, this.amount);
+
+            LOGGER.log(Level.INFO, "Savings added to: {0} of ${1}", new Object[]{category, amount});
+            System.out.println("Savings Added to:" + category + " of $" + amount);
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Exception while adding savings", e);
+        }
     }
 }
