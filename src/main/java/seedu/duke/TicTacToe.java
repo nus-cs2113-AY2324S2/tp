3c201eee3b141e37a9ac4e08b373f4f0a19ff618
nicknamenic <ranjnicoli1@gmail.com>
2024-03-20 00:07:23 +0800

    Updated Tic Tac Toe Class to fulfill v1.0

--- src/main/java/seedu/duke/TicTacToe.java
+++ src/main/java/seedu/duke/TicTacToe.java
@@ -6,12 +6,13 @@ import java.util.Scanner;
 
 import static seedu.duke.Parser.readTTMove;
 
-public class TicTacToe extends Game{
+public class TicTacToe extends Game {
     protected static String[] board = new String[9];
 
     public TicTacToe(String line) {
         super(line);
     }
+
     public static void printBoard() {
         System.out.println("  " + board[0] + " | " + board[1] + " | " + board[2] + "  ");
         System.out.println("-------------");
@@ -24,32 +25,32 @@ public class TicTacToe extends Game{
         for (int a = 0; a < 8; a++) {
             String line = null;
             switch (a) {
-                case 0:
-                    line = board[0] + board[1] + board[2];
-                    break;
-                case 1:
-                    line = board[3] + board[4] + board[5];
-                    break;
-                case 2:
-                    line = board[6] + board[7] + board[8];
-                    break;
-                case 3:
-                    line = board[0] + board[3] + board[6];
-                    break;
-                case 4:
-                    line = board[1] + board[4] + board[7];
-                    break;
-                case 5:
-                    line = board[2] + board[5] + board[8];
-                    break;
-                case 6:
-                    line = board[0] + board[4] + board[8];
-                    break;
-                case 7:
-                    line = board[2] + board[4] + board[6];
-                    break;
-                default:
-                    //this never happens
+            case 0:
+                line = board[0] + board[1] + board[2];
+                break;
+            case 1:
+                line = board[3] + board[4] + board[5];
+                break;
+            case 2:
+                line = board[6] + board[7] + board[8];
+                break;
+            case 3:
+                line = board[0] + board[3] + board[6];
+                break;
+            case 4:
+                line = board[1] + board[4] + board[7];
+                break;
+            case 5:
+                line = board[2] + board[5] + board[8];
+                break;
+            case 6:
+                line = board[0] + board[4] + board[8];
+                break;
+            case 7:
+                line = board[2] + board[4] + board[6];
+                break;
+            default:
+                //this never happens
             }
             if (line.equals("XXX")) {
                 return "X";
