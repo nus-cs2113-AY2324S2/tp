05d3dc5acad11b2b0ff5a8c208857258eab222b8
Ningsong Shen <ningsongshen123@outlook.com>
2024-03-19 12:36:43 +0800

    Change logo to NewsOnTheGo and update text-ui-test to check for logo using LF line endings

--- src/main/java/newsonthego/NewsOnTheGo.java
+++ src/main/java/newsonthego/NewsOnTheGo.java
@@ -7,11 +7,13 @@ public class NewsOnTheGo {
      * Main entry-point for the java.newsonthego.NewsOnTheGo application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "\n" +
+                ",-,-.                 ,---.     ,--,--'.       ,---.      \n" +
+                "` | |   ,-. . , , ,-. |   | ,-. `- |   |-. ,-. |  -'  ,-. \n" +
+                "  | |-. |-' |/|/  `-. |   | | |  , |   | | |-' |  ,-' | | \n" +
+                " ,' `-' `-' ' '   `-' `---' ' '  `-'   ' ' `-' `---|  `-' \n" +
+                "                                                ,-.|      \n" +
+                "                                                `-+'      \n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
 
