b09f8adb286ed404feb9ba2edbd11f2fe8199658
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-03-19 12:02:25 +0800

    Add changes to texui tests

--- text-ui-test/changeExpected.sh
+++ text-ui-test/changeExpected.sh
@@ -7,9 +7,9 @@ then
 fi
 
 # delete output from previous run
-if [ -e "./ACTUAL.TXT" ]
+if [ -e "./EXPECTED.TXT" ]
 then
-    rm ACTUAL.TXT
+    rm EXPECTED.TXT
 fi
 
 # compile the code into the bin folder, terminates if error occurred
@@ -20,19 +20,8 @@ then
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin BobBot/BobBot < input.txt > ACTUAL.TXT
+java -classpath ../bin BobBot/BobBot < input.txt > EXPECTED.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
-
-# compare the output to the expected output
-diff ACTUAL.TXT EXPECTED-UNIX.TXT
-if [ $? -eq 0 ]
-then
-    echo "Test result: PASSED"
-    exit 0
-else
-    echo "Test result: FAILED"
-    exit 1
-fi
