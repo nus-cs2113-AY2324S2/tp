ec41f8b44632a19d781e38a7fda1a9acc5cd5d89
nicknamenic <ranjnicoli1@gmail.com>
2024-03-19 23:38:17 +0800

    Updated Tic Tac Toe Class to fulfill v1.0

--- src/main/java/seedu/duke/TicTacToe.java
+++ src/main/java/seedu/duke/TicTacToe.java
@@ -1,15 +1,73 @@
 package seedu.duke;
-public class TicTacToe extends Game{
+
+import seedu.duke.exceptions.InvalidTTMoveException;
+
+import java.util.Scanner;
+
+import static seedu.duke.Parser.readTTMove;
+
+public class TicTacToe extends Game {
     protected static String[] board = new String[9];
 
     public TicTacToe(String line) {
         super(line);
     }
+
     public static void printBoard() {
         System.out.println("  " + board[0] + " | " + board[1] + " | " + board[2] + "  ");
         System.out.println("-------------");
         System.out.println("  " + board[3] + " | " + board[4] + " | " + board[5] + "  ");
         System.out.println("-------------");
         System.out.println("  " + board[6] + " | " + board[7] + " | " + board[8] + "  ");
+    }
+
+    public static String checkWinner() {
+        for (int a = 0; a < 8; a++) {
+            String line = null;
+            switch (a) {
+                case 0:
+                    line = board[0] + board[1] + board[2];
+                    break;
+                case 1:
+                    line = board[3] + board[4] + board[5];
+                    break;
+                case 2:
+                    line = board[6] + board[7] + board[8];
+                    break;
+                case 3:
+                    line = board[0] + board[3] + board[6];
+                    break;
+                case 4:
+                    line = board[1] + board[4] + board[7];
+                    break;
+                case 5:
+                    line = board[2] + board[5] + board[8];
+                    break;
+                case 6:
+                    line = board[0] + board[4] + board[8];
+                    break;
+                case 7:
+                    line = board[2] + board[4] + board[6];
+                    break;
+                default:
+                    // Will never execute
+            }
+            if (line.equals("XXX")) {
+                return "X";
+            }
+        }
+        return "unending";
+    }
+
+    public static void runTicTacToe() throws InvalidTTMoveException {
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        while (checkWinner().equals("unending") || line.equals("exit")) {
+            readTTMove(line);
+            board[Integer.parseInt(line) + 1] = "X";
+            printBoard();
+            line = in.nextLine();
+        }
+        System.out.println("Thank you for playing. See you next time!");
     }
 }
