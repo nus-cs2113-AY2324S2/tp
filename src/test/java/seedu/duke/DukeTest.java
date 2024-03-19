53e87fe1633a96bd49880ac594f3f862eb6e304f
Eugene Chan <eugenechan2001@gmail.com>
2024-03-20 00:40:34 +0800

    Fix code for CI build test

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -2,8 +2,9 @@ package seedu.duke;
 
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class DukeTest {
 
