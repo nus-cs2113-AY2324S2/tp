54eee110b2973d00e42f0743d691ab35cd6215ca
ttasnim5 <tahsintasnim6@gmail.com>
2024-03-19 12:20:20 -0400

    updates readGame and readTTMove for nulls (again)

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -34,7 +34,7 @@ public class Parser {
      * whatever makes it flow easier
      */
     public static void readGame(String input) throws InvalidGameException {
-        if (!input.equals("Tic Tac Toe") && !input.equals("Hangman")) {
+        if (input != null && !input.equals("Tic Tac Toe") && !input.equals("Hangman")) {
             throw new InvalidGameException();
         }
     }
@@ -46,6 +46,10 @@ public class Parser {
      * whatever makes it flow easier
      */
     public static void readTTMove(String input) throws InvalidTTMoveException {
+        if (input == null) {
+            return;
+        }
+
         int markBox;
         try {
             markBox = Integer.parseInt(input);
