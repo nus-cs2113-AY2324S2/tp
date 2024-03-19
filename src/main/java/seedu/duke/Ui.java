d95be236b03968cb5da9c46794e46d385531d25f
Andy123qq4 <1394568964@qq.com>
2024-03-19 16:37:57 +0800

    Fix code style and typos.

--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -7,28 +7,24 @@ import java.util.Scanner;
  */
 public class Ui {
 
-    private final String name;
-    private final Scanner scanner = new Scanner(System.in);
-
     // Pre-defined sentences
     private static final String PROBLEM_FORM =
             "Please type the number and difficulty you like in following form: \n" +
                     "< generate -t [type] -n [number] -d [maximum digit] >";
     private static final String INPUT_INSTRUCTION =
-            """
-                    Input Instructions:\s
-                    [type]: can be + - * /, you can combine any of them.\s
-                    [number]: number of problem set generated\s
-                    [maximum digit]: how big can the calculation be\s
-
-                    For example: generate -t + -n 10 -d 2\s
-                    -> generate 10 problems with + and - operator, and the maximum number of digits is 2 (99 max)
-                    """;
-
+            "Input Instructions:\n" +
+                    "[type]: can be + - * /, you can combine any of them.\n" +
+                    "[number]: number of problem set generated\n" +
+                    "[maximum digit]: how big can the calculation be\n\n" +
+                    "For example: generate -t + -n 10 -d 2\n" +
+                    "-> generate 10 problems with + and - operator, and the maximum number of digits is 2 (99 max)";
     private static final String GEN_COMMAND =
             "Generate problem sets: \t" + "gen -t [type] -n [number] -d [maximum digits]";
     private static final String HELP_COMMAND =
             "Help function: \t" + "help [type], type can be 'gen'/'command'/...";
+    private final String name;
+    private final Scanner scanner = new Scanner(System.in);
+
     /**
      * Creates a new Ui with the given name.
      *
@@ -43,13 +39,11 @@ public class Ui {
      */
     public void greet() {
         this.showLine();
-        String logo = """
-                 ____        _       \s
-                |  _ \\ _   _| | _____\s
-                | | | | | | | |/ / _ \\
-                | |_| | |_| |   <  __/
-                |____/ \\__,_|_|\\_\\___|
-                """;
+        String logo = " ____        _       \n" +
+                "|  _ \\ _   _| | _____\n" +
+                "| | | | | | | |/ / _ \\\n" +
+                "| |_| | |_| |   <  __/\n" +
+                "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo);
         System.out.println("Hello! I'm " + name);
         System.out.println("Type 'help' to see the instructions. \n");
@@ -66,16 +60,17 @@ public class Ui {
 
     public void help(String helpType) {
         switch (helpType) {
-            case "": // by default, user asks Input Instruction
-            case "gen":
-            case "generate":
-                System.out.println(GEN_COMMAND);
-                System.out.println(INPUT_INSTRUCTION);
-                break;
-            case "command":
-                System.out.println(HELP_COMMAND);
-            default:
-                break;
+        case "": // by default, user asks Input Instruction
+        case "gen":
+        case "generate":
+            System.out.println(GEN_COMMAND);
+            System.out.println(INPUT_INSTRUCTION);
+            break;
+        case "command":
+            System.out.println(HELP_COMMAND);
+            break;
+        default:
+            break;
         }
     }
 
@@ -91,7 +86,7 @@ public class Ui {
     /**
      * Displays an error message for an invalid index.
      */
-    
+
     public void exit() {
         System.out.println("Bye. Hope to see you again soon!");
         showLine();
