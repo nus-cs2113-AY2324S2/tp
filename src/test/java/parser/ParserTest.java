3633f2d912a1817060c3715097736caf8fff4477
Kishen271828 <kg240904kg@gmail.com>
2024-03-19 19:24:02 +0800

    Include category input for ParserTest

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -12,17 +12,17 @@ public class ParserTest {
     public void addInflow_success() {
         Parser test1 = new Parser();
         assertEquals("Ok. Added inflow",
-                     test1.parseCommand("add-inflow n/Salary a/5000 d/14/03/2024 t/1700", manager));
+                     test1.parseCommand("add-inflow n/Salary a/5000 d/14/03/2024 t/1700 c/income", manager));
         assertEquals("Ok. Added inflow",
-                     test1.parseCommand("add-inflow n/Investment a/600 d/03/03/2024 t/1900", manager));
+                     test1.parseCommand("add-inflow n/Stocks a/600 d/03/03/2024 t/1900 c/investment", manager));
     }
 
     public void addOutflow_success() {
         Parser test1 = new Parser();
         assertEquals("Ok. Added outflow",
-                     test1.parseCommand("add-outflow n/Rent a/1000 d/18/02/2024 t/1100", manager));
+                     test1.parseCommand("add-outflow n/Rent a/1000 d/18/02/2024 t/1100 c/rent", manager));
         assertEquals("Ok. Added outflow",
-                     test1.parseCommand("add-outflow n/Shopping a/150 d/30/01/2024 t/1430", manager));
+                     test1.parseCommand("add-outflow n/Shopping a/150 d/30/01/2024 t/1430 c/shopping", manager));
     }
 
 }
