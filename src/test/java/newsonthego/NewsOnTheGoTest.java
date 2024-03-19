9a91f020a47cc5143d75689b8cc8ec4c4ece0e1c
HiteshriAcharya <hiteshriacharya@gmail.com>
2024-03-20 02:41:16 +0800

    JUnit test case for 'infoNews'

--- src/test/java/newsonthego/NewsOnTheGoTest.java
+++ src/test/java/newsonthego/NewsOnTheGoTest.java
@@ -7,8 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.List;
 
 class NewsOnTheGoTest {
