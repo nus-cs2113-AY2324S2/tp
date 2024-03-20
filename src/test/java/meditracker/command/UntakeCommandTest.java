4b8202ed4e9496e71a4d4bc182cd650dd66e7742
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-03-20 15:08:07 +0800

    test: Add tests for TakeCommand and UntakeCommand

---
+++ src/test/java/meditracker/command/UntakeCommandTest.java
@@ -0,0 +1,27 @@
+package meditracker.command;
+
+import meditracker.DailyMedication;
+import meditracker.DailyMedicationManager;
+import meditracker.exception.ArgumentNotFoundException;
+import meditracker.medication.MedicationManager;
+import meditracker.ui.Ui;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class UntakeCommandTest {
+    @Test
+    void execute_inOrderArgument_expectDailyMedicationTaken() throws ArgumentNotFoundException {
+        MedicationManager medicationManager = new MedicationManager();
+        DailyMedicationManager dailyMedicationManager = new DailyMedicationManager(medicationManager);
+        DailyMedication dailyMedication = new DailyMedication("Medication_A");
+        dailyMedicationManager.addDailyMedication(dailyMedication);
+
+        String inputString = "untake -l 1";
+        UntakeCommand command = new UntakeCommand(inputString);
+        Ui ui = new Ui();
+        command.execute(null, dailyMedicationManager, ui);
+
+        assertFalse(dailyMedication.isTaken());
+    }
+}
