d095c8e87613678991f931570d5bfb119c88221f
Parth Sanjay Gandhi <E0957991@u.nus.edu>
2024-03-20 12:10:58 +0800

    Add new printCurrentArrayMessage method

--- src/main/java/classify/student/StudentList.java
+++ src/main/java/classify/student/StudentList.java
@@ -6,8 +6,12 @@ import java.util.ArrayList;
  * Class to represent a list of Students.
  */
 public class StudentList {
-    //@@ParthGandhiNUS
+    //@@authorParthGandhiNUS
     public static final String DOT = ".";
+    public static final String ZERO_STUDENT_MESSAGE = "Currently, there are 0 students in the list.";
+    public static final String ONE_STUDENT_MESSAGE = "Currently, there is 1 student in the list.";
+    public static final String CURRENTLY_THERE_ARE = "Currently, there are ";
+    public static final String STUDENTS_IN_THE_LIST = " students in the list.";
     public static ArrayList<Student> masterStudentList = new ArrayList<>();
 
     //@@author Cryolian
@@ -21,11 +25,35 @@ public class StudentList {
     //@@author ParthGandhiNUS
     /**
      * Used in the "list" command to print all the Current Tasks in the proper format
+     * @param currentList takes in an array to print out the name and other attributes of the Student
      */
     public static void printCurrentArrayList(ArrayList<Student>currentList){
+        assert currentList.size() >= 0;
         for (int i = 1; i <= currentList.size(); i++){
             System.out.println(i + DOT + currentList.get(i-1));
         }
+    }
+
+    //@@author ParthGandhiNUS
+    /**
+     * Prints a statement displaying the number of students in the  Student List
+     * @param currentList takes in a Student list to get its size
+     */
+    public static void printCurrentArrayMessage(ArrayList<Student>currentList){
+        int numberOfStudents = currentList.size();
+        assert numberOfStudents >= 0;
+        switch (numberOfStudents){
+        case 0:
+            System.out.println(ZERO_STUDENT_MESSAGE);
+            break;
+        case 1:
+            System.out.println(ONE_STUDENT_MESSAGE);
+            break;
+        default:
+            System.out.println(CURRENTLY_THERE_ARE + numberOfStudents + STUDENTS_IN_THE_LIST);
+            break;
+        }
+
     }
 
     //@@author Cryolian
