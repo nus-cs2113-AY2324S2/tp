af9b4f457f9e9827fc6e2d28ebac9f34961ba6d1
KRISHNAAYAGARI\kak36 <mukund1403@yahoo.com>
2024-03-20 18:39:02 +0800

    Add Javadoc comments and JUnit test for expense

--- src/main/java/seedu/duke/Expense.java
+++ src/main/java/seedu/duke/Expense.java
@@ -11,6 +11,13 @@ public class Expense {
     private float total_amount;
 
     private ArrayList<String> payees = new ArrayList<>();
+
+    /**
+     * Constructor to create new Expense
+     * @param payer_name : The name of the user who paid for the Expense
+     * @param total_amount : The total amount before being divided
+     * @param payee_list : String array of people who owe the payer money (Index 0 is the payer and will not be added to the payee list)
+     */
     Expense(String payer_name, String total_amount, String[] payee_list){
         total_amount = removeWhitespaces(total_amount);
         payer_name = removeWhitespaces(payer_name);
@@ -20,12 +27,19 @@ public class Expense {
         this.payer_name = payer_name;
         this.total_amount = Float.parseFloat(total_amount);
         System.out.printf("Added new expense %.2f owed to %s by:",this.total_amount,this.payer_name);
-        for(String payee : payees){
+        for(String payee : this.payees){
             System.out.print(payee + ", ");
         }
         System.out.println();
     }
 
+    /**
+     *
+     * @return : float showing the total amount before division
+     */
+    public float getTotal_amount(){
+        return this.total_amount;
+    }
     private String removeWhitespaces(String item){
         String itemWithoutWhitespaces = item.replaceAll("\\s+", " ").trim();
         return itemWithoutWhitespaces;
---
+++ src/test/java/seedu/duke/ExpenseTest.java
@@ -0,0 +1,14 @@
+package seedu.duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class ExpenseTest{
+    @Test
+    public void NewExpenseTest(){
+        Expense testExpense = new Expense("Mukund"," 10 ", new String[]{"Mukund", " JX", "hehe"});
+        assertEquals((float) 10, testExpense.getTotal_amount());
+    }
+}
