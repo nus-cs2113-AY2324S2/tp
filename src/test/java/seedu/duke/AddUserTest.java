d6c0677f7aa6ef25c13171ba4c567ead5cdee6ee
Hafizuddin Bin Aminuddin <hafizuddinaminuddin1@gmail.com>
2024-03-20 15:19:01 +0800

    Add test file

---
+++ src/test/java/seedu/duke/AddUserTest.java
@@ -0,0 +1,31 @@
+package seedu.duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AddUserTest {
+
+    @Test
+    public void testUser() {
+        try {
+            User user = new User("John");
+            assertEquals("John", user.getName());
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testAddUserToGroup() {
+        try {
+            Group group = new Group("TestGroup");
+            User user = new User("John");
+            group.addUsers(user);
+            assertEquals("John", group.users.get(0).getName());
+        } catch (Exception e) {
+            fail();
+        }
+    }
+}
