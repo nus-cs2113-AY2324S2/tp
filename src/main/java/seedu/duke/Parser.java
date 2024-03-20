649d4157f01f73139efd574142ab6d430a42345e
Cohii <hengjunxiangg@gmail.com>
2024-03-20 23:23:23 +0800

    Fix checkstyle error
    variable declaration order

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -1,9 +1,8 @@
 package seedu.duke;
 
 public class Parser {
+    protected static Group currentGroup = new Group("");
     protected String userInput;
-    public static Group currentGroup = new Group("");
-
     public static class EndProgramException extends Exception {
 
     }
