771893a69e418ee031043b961d13ce0e2b89c97e
fxe025 <e0968867@u.nus.edu>
2024-03-20 21:10:28 +0800

    Add writeToFile test in StorageTest

--- src/test/java/storage/StorageTest.java
+++ src/test/java/storage/StorageTest.java
@@ -9,10 +9,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class StorageTest {
     @Test
-    public void testReadFile_FileNotFound() {
+    public void readFromFile_fileNotFound() {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
 
@@ -25,6 +26,23 @@ public class StorageTest {
             assertEquals("File does not exist." + System.lineSeparator(), outputStream.toString());
         } catch (IOException e) {
             fail("failed to create a file.");
+        }
+    }
+
+    @Test
+    public void writeToFile_aLine_writeSuccessful() {
+        String directory = "./testFile.txt";
+        File testFile = new File(directory);
+        String aLine = "A line";
+        try {
+            Storage.writeToFile(directory, aLine, true);
+            Scanner scanner = new Scanner(testFile);
+            String lineSkipped = scanner.nextLine();
+            scanner.close();
+            testFile.delete();
+            assertEquals(aLine, lineSkipped);
+        } catch (IOException e) {
+            fail("File not found");
         }
     }
 }
