b6998e919a8f5d7e298c42b70631aa9613d1c3e4
fxe025 <e0968867@u.nus.edu>
2024-03-20 12:29:45 +0800

    Update StorageTest

--- src/test/java/storage/StorageTest.java
+++ src/test/java/storage/StorageTest.java
@@ -1,13 +1,30 @@
 package storage;
 
 import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.fail;
 
-//import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class StorageTest {
     @Test
-    public void fileToReadNotFound() {
+    public void testReadFile_FileNotFound() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        String directory = "./testFile.txt";
+        File testFile = new File(directory);
+        try {
+            Storage.writeToFile(directory, "Created", true);
+            testFile.delete();
+            Storage.readFromFile(testFile);
+            assertEquals("File does not exist." + System.lineSeparator(), outputStream.toString());
+        } catch (IOException e) {
+            fail("failed to create a file.");
+        }
     }
 }
