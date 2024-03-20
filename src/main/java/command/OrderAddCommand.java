9d1fc3b01a36e800997393629df3218ee0d3251c
webster <webster.tan01@gmail.com>
2024-03-20 22:45:31 +0800

    Add Classes for sub-commands under "create order"

--- src/main/java/command/OrderAddCommand.java
+++ src/main/java/command/OrderAddCommand.java
@@ -1,5 +1,21 @@
 package command;
 
-public class OrderAddCommand implements OrderCommand{
+import model.MenuItem;
+import model.Order;
 
+public class OrderAddCommand implements OrderCommand{
+    /**
+     * Executes the command to add a specified quantity of an item to an order.
+     *
+     * @param order     the order to which the item should be added
+     * @param item      the item to be added to the Order
+     * @param quantity  the amount of items to add to the order
+     * @return          always returns false, as order is not completed
+     */
+    public boolean execute(Order order, MenuItem item, int quantity) {
+        for (int i = 0; i < quantity; i ++) {
+            order.add(item);
+        }
+        return false;
+    }
 }
