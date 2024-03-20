fedc835937ec1eb9a1c99cf21fca0017029a2c69
yuki-zmstr <yukihide99@gmail.com>
2024-03-20 11:41:40 +0800

    Redirect logging to log file

--- src/main/java/recipeio/RecipeIO.java
+++ src/main/java/recipeio/RecipeIO.java
@@ -3,7 +3,10 @@ package recipeio;
 import recipeio.recipe.RecipeList;
 import recipeio.ui.UI;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 /**
  * Main entry-point for the Recipe.IO application.
@@ -37,7 +40,22 @@ public class RecipeIO {
         }
     }
 
+    public static void setUpLogger() {
+        LogManager.getLogManager().reset();
+        logger.setLevel(Level.ALL);
+        try {
+            FileHandler fileHandler = new FileHandler("logger.log", false);
+            fileHandler.setLevel(Level.INFO);
+            logger.setLevel(Level.INFO);
+            logger.addHandler(fileHandler);
+            assert logger.getHandlers().length > 0 : "File handler not added to logger";
+        } catch (IOException e){
+            logger.log(Level.SEVERE, "File logger not working");
+        }
+    }
+
     public static void main(String[] args) {
+        setUpLogger();
         new RecipeIO().run();
     }
 }
