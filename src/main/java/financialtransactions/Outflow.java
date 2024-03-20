65329cc2db9f616ca2cef15e2210c9f91ff2e6f7
Chong Xern <104547209+ChongXern@users.noreply.github.com>
2024-03-20 17:45:36 +0800

    Add taxes to outflow

--- src/main/java/financialtransactions/Outflow.java
+++ src/main/java/financialtransactions/Outflow.java
@@ -1,6 +1,7 @@
 package financialtransactions;
 
 public class Outflow extends Transaction<Outflow.Category> {
+    private static final double TAX_AMOUNT = 0.09;
     public enum Category {
         RENT, DEBT, SHOPPING, TREAT, EDUCATION, OTHER
     }
@@ -8,6 +9,7 @@ public class Outflow extends Transaction<Outflow.Category> {
     public Outflow(String name, double amount, String date) {
         super(name, -1.00 * amount, date);
         super.transactionType = "O";
+        super.amount *= TAX_AMOUNT;
     }
 
     public void setCategory(Category category) {
