913bc5a498d07fa28af5b54b619b12254cbdaa8b
BenjoBurger <benjaminggr@gmail.com>
2024-03-20 08:00:25 +0800

    Create daily Function

--- src/main/java/newsonthego/NewsOnTheGo.java
+++ src/main/java/newsonthego/NewsOnTheGo.java
@@ -3,11 +3,12 @@ package newsonthego;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class NewsOnTheGo {
 
@@ -112,6 +113,22 @@ public class NewsOnTheGo {
     }
 
     private static void dailyNews(String line, List<NewsArticle> list) {
+        String[] split = line.split(" ", 2);
+        String date = split[1];
+         List<NewsArticle> articles = list.stream()
+                .filter(a->a.getDate().equals(date))
+                .collect(Collectors.toList());
+
+        if (articles.isEmpty()) {
+            System.out.println("Nothing is found on this day: " + date);
+        } else {
+            System.out.println("Sure! Here are the headlines for today:");
+            System.out.println();
+            for (NewsArticle a : articles) {
+                System.out.println(a.getHeadline());
+            }
+            System.out.println();
+        }
     }
 
     private static void getNews(String line, List<NewsArticle> list) {
