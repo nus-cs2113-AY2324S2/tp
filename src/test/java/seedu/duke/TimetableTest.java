e239cb19ff47b3fda54fe85a14e9475b79433251
z-wenqing <zhangwenqing0824@gmail.com>
2024-03-20 22:09:37 +0800

    Clean up code

--- src/test/java/seedu/duke/TimetableTest.java
+++ src/test/java/seedu/duke/TimetableTest.java
@@ -26,22 +26,4 @@ class TimetableTest {
 
         Timetable.compareTimetable(timetable1, timetable2);
     }
-    @Test
-    public void testDeleteUserTask() {
-        Timetable timetable = new Timetable();
-        Task task1 = new Task("Study", "2024-03-18", "09:00", "11:00");
-        Task task2 = new Task("Exercise", "2024-03-18", "15:00", "16:30");
-
-        // Add tasks to the timetable
-        timetable.addUserTask("tuesday", task1);
-        timetable.addUserTask("tuesday", task2);
-
-        // Delete the first task
-        timetable.deleteUserTask("tuesday", 0);
-
-        // Check if the task is deleted
-        assertEquals(1, timetable.getWeeklyTasks().get("tuesday").size());
-        assertEquals(task2, timetable.getWeeklyTasks().get("tuesday").get(0));
-
-    }
 }
