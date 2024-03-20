034289683eb769be2e896f4b6fcc19d4036fd477
Hafizuddin Bin Aminuddin <hafizuddinaminuddin1@gmail.com>
2024-03-20 21:51:51 +0800

    Fix line more than 120 char in GroupTest

--- src/test/java/seedu/duke/GroupTest.java
+++ src/test/java/seedu/duke/GroupTest.java
@@ -9,6 +9,6 @@ public class GroupTest {
     public void groupTest() {
         String expectedName = "GroupName";
         Group group = new Group(expectedName);
-        assertTrue(group.groupName.equals(expectedName), "Group name should be snt to the provided constructor argument.");
+        assertTrue(group.groupName.equals(expectedName), "Group name is not the same as expected");
     }
 }
