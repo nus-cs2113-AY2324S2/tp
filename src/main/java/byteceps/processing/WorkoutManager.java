1de99c61e28aecddc6c39d17c5acd3faac333357
V4vern <28131050+V4Vern@users.noreply.github.com>
2024-03-21 10:53:34 +0800

    add assertions in WorkoutManager

--- src/main/java/byteceps/processing/WorkoutManager.java
+++ src/main/java/byteceps/processing/WorkoutManager.java
@@ -21,16 +21,18 @@ public class WorkoutManager extends ActivityManager {
             Exceptions.ActivityExistsException,
             Exceptions.InvalidInput,
             Exceptions.ActivityDoesNotExists {
+
+        assert parser != null : "Parser must not be null";
         switch (parser.getAction()) {
         case "create":
-            Workout newWorkout = processCreateWorkout(parser);
+            Workout newWorkout = processWorkout(parser);
             add(newWorkout);
             UserInterface.printMessage(String.format(
                     "Added Workout Plan: %s\n", newWorkout.getActivityName()
             ));
             break;
         case "delete":
-            Workout existingWorkout = processCreateWorkout(parser);
+            Workout existingWorkout = processWorkout(parser);
             delete(existingWorkout);
             UserInterface.printMessage(String.format(
                     "Deleted Exercise: %s", existingWorkout.getActivityName()
@@ -48,9 +50,6 @@ public class WorkoutManager extends ActivityManager {
                     "Unassigned Exercise '%s' from Workout Plan '%s'\n", parser.getActionParameter(), workoutName
             ));
             break;
-        case "samples":
-            //list();
-            break;
         case "info":
             list(parser.getActionParameter());
             break;
@@ -63,8 +62,9 @@ public class WorkoutManager extends ActivityManager {
     }
 
     //@@author V4vern
-    public Workout processCreateWorkout(Parser parser) throws Exceptions.InvalidInput {
+    public Workout processWorkout(Parser parser) throws Exceptions.InvalidInput {
         String workoutName = parser.getActionParameter();
+        assert !workoutName.isEmpty(): "Workout name cannot be empty";
         if (workoutName.isEmpty()) {
             throw new Exceptions.InvalidInput("Workout name cannot be empty");
         }
@@ -76,13 +76,17 @@ public class WorkoutManager extends ActivityManager {
             Exceptions.ActivityDoesNotExists {
 
         String exerciseName = parser.getActionParameter();
+        assert exerciseName != null : "Exercise name cannot be null";
         String workoutPlanName = parser.getAdditionalArguments("to");
+        assert workoutPlanName != null : "Workout plan name cannot be null";
         if (workoutPlanName == null) {
             throw new Exceptions.InvalidInput("assign command not complete");
         }
 
         Exercise exercise = (Exercise) exerciseManager.retrieve(exerciseName);
+        assert exercise != null : "Exercise does not exist";
         Workout workoutPlan = (Workout) retrieve(workoutPlanName);
+        assert workoutPlan != null : "Workout plan does not exist";
 
         if (workoutPlan.getExerciseList().contains(exercise)) {
             throw new Exceptions.InvalidInput("Exercise already assigned to workout plan");
@@ -95,7 +99,9 @@ public class WorkoutManager extends ActivityManager {
 
     //@@author V4vern
     public void list(String workoutPlanName) throws Exceptions.ActivityDoesNotExists {
+        assert workoutPlanName != null : "Workout plan name cannot be null";
         Workout workout = (Workout) retrieve(workoutPlanName);
+        assert workout != null : "Workout plan does not exist";
         StringBuilder message = new StringBuilder();
         ArrayList<Exercise> workoutList = workout.getExerciseList();
 
@@ -113,12 +119,15 @@ public class WorkoutManager extends ActivityManager {
             Exceptions.ActivityDoesNotExists {
 
         String workoutPlanName = parser.getAdditionalArguments("from");
+        assert workoutPlanName != null : "Workout plan name cannot be null";
         String exerciseName = parser.getActionParameter();
+        assert exerciseName != null : "Exercise name cannot be null";
         if (workoutPlanName == null) {
             throw new Exceptions.InvalidInput("unassign command not complete");
         }
 
         Workout workoutPlan = (Workout) retrieve(workoutPlanName);
+        assert workoutPlan != null : "Workout plan does not exist";
         ArrayList<Exercise> workoutList = workoutPlan.getExerciseList();
 
         workoutList.removeIf(exercise -> exercise.getActivityName().equalsIgnoreCase(exerciseName));
