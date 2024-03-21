66633ad639e7d7548a27c2faf100a24de2b8d100
Yeo Zong Yao <yeozongyao@gmail.com>
2024-03-21 23:38:27 +0800

    Disable assertions for jUnit tests

--- src/main/java/seedu/bookbuddy/BookList.java
+++ src/main/java/seedu/bookbuddy/BookList.java
@@ -5,7 +5,6 @@ import exceptions.BookNotFoundException;
 
 import java.util.ArrayList;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import static seedu.bookbuddy.BookBuddy.LOGGER;
 
