b7c43a78cd79da4e902e345f190ff3a19bd7bbbd
cyhjason29 <jackplok29@gmail.com>
2024-03-21 21:47:22 +0800

    Update Helper list of commands

--- src/main/java/seedu/duke/Helper.java
+++ src/main/java/seedu/duke/Helper.java
@@ -6,10 +6,15 @@ public class Helper {
     private static final ArrayList<Command> commandList = new ArrayList<Command>();
 
     public Helper() {
-        commandList.add(new Command("bye", "Terminate the programme", "bye"));
-        commandList.add(new Command("help", "View available commands or check a one", "help, help [command]"));
+        commandList.add(new Command("topic", "Start the round of questions for the chosen " +
+                "topic","topic [TOPIC_INDEX]"));
+        commandList.add(new Command("help", "View available commands or check one", "help, help [COMMAND]"));
         commandList.add(new Command("solution", "View solution to the question", "solution [QUESTION_INDEX]"));
-        commandList.add(new Command("explain", "View explaination for the solution", "explain [QUESTION_INDEX]"));
+        commandList.add(new Command("explain", "View explanation for the solution",
+                "explain [QUESTION_INDEX]"));
+        commandList.add(new Command("results", "View results for each topic played",
+                "results [details] [RESULTS_INDEX]"));
+        commandList.add(new Command("bye", "Terminate the program", "bye"));
     }
 
     public static String[][] listAllCommands() {
