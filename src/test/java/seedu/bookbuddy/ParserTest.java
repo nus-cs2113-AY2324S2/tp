f600430b50eb0c8e0ff6f4dc18a9ee4c548b6e01
Yeo Zong Yao <yeozongyao@gmail.com>
2024-03-21 23:24:50 +0800

    Bug fixes - getbook logic

--- src/test/java/seedu/bookbuddy/ParserTest.java
+++ src/test/java/seedu/bookbuddy/ParserTest.java
@@ -21,12 +21,12 @@ public class ParserTest {
         books.addBook("Gulliver's Travels");
         assertEquals(2, books.getSize());
         books.markDoneByIndex(1);
-        assertEquals("[R] Don Quixote", books.getBook(0).toString());
-        assertEquals("[U] Gulliver's Travels", books.getBook(1).toString());
+        assertEquals("[R] Don Quixote", books.getBook(1).toString());
+        assertEquals("[U] Gulliver's Travels", books.getBook(2).toString());
         books.deleteBook(1);
         books.markDoneByIndex(1);
         assertTrue(books.getBook(0).isRead);
-        assertEquals("[R] Gulliver's Travels", books.getBook(0).toString());
+        assertEquals("[R] Gulliver's Travels", books.getBook(1).toString());
     }
 
     @Test
@@ -34,7 +34,7 @@ public class ParserTest {
         BookList testBookList = new BookList();
         Parser.parseCommand("add The Great Gatsby", testBookList);
         assertEquals(1, testBookList.getSize());
-        assertEquals("The Great Gatsby", testBookList.getBook(0).getTitle());
+        assertEquals("The Great Gatsby", testBookList.getBook(1).getTitle());
     }
 
     @Test
@@ -59,7 +59,7 @@ public class ParserTest {
         books.addBook("The Great Gatsby");
         Parser.parseCommand("mark 1", books);
         Parser.parseCommand("unmark 1", books);
-        assertFalse(books.getBook(0).isRead());
+        assertFalse(books.getBook(1).isRead());
     }
 
     @Test
