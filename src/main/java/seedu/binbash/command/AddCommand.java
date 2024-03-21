1ceb3e4ace944f475565dc6458c07bdcfa9d14c8
nur-haziq <nhaziq01@gmail.com>
2024-03-22 01:46:25 +0800

    Modify comment format to be in line with coding standards

--- src/main/java/seedu/binbash/command/AddCommand.java
+++ src/main/java/seedu/binbash/command/AddCommand.java
@@ -5,30 +5,38 @@ import seedu.binbash.ItemList;
 
 public class AddCommand extends Command {
 
-//    public static final Pattern COMMAND_FORMAT = Pattern.compile(
-//            "add\\s" + "n/(?<itemName>.+?)\\s" + "d/(?<itemDescription>.+?)\\s" + "(q/(?<itemQuantity>.+?))?"
-//                    + "(e/(?<itemExpirationDate>.+?))?" + "s/(?<itemSalePrice>.+?)\\s" + "c/(?<itemCostPrice>.+)"
-//    );
+
+    //    public static final Pattern COMMAND_FORMAT = Pattern.compile(
+    //            "add\\s" + "n/(?<itemName>.+?)\\s" + "d/(?<itemDescription>.+?)\\s" + "(q/(?<itemQuantity>.+?))?"
+    //                    + "(e/(?<itemExpirationDate>.+?))?" + "s/(?<itemSalePrice>.+?)\\s" + "c/(?<itemCostPrice>.+)"
+    //    );
 
     /**
-     * TODO: I understand the formatting of this is cancer. The trailing comments are exceeding the wraparound
-     * TODO: limit. We have to fix it in the future, but I hope they help clarify the regex for future rectifications.
+     * TODO: I understand the formatting of this is cancer. I'll fix it in the future, but I hope they help clarify
+     * TODO: the regex format for future rectifications.
      *
      * As of now, only itemQuantity(q/) and and itemExpirationDate(e/) are optional groups.
      */
     public static final Pattern COMMAND_FORMAT = Pattern.compile(
 
-            "add\\s+" +                                             // Match the 'add' command followed by one or more whitespace characters.
+            "add\\s+" +                                             // Match the 'add' command followed by one or more
+                                                                    // whitespace characters.
 
-                    "n/(?<itemName>.+?)(?=d/)" +                    // Match 'n/' followed by any characters for `itemName`, lazy match, until seeing 'd/'.
+                    "n/(?<itemName>.+?)(?=d/)" +                    // Match 'n/' followed by any characters for
+                                                                    // `itemName`, lazy match, until seeing 'd/'.
 
-                    "d/(?<itemDescription>.+?)(?=(q/|e/|s/))" +     // Match 'd/' followed by any characters for `itemDescription`, lazy match, until seeing 'q/', 'e/', or 's/'.
+                    "d/(?<itemDescription>.+?)(?=(q/|e/|s/))" +     // Match 'd/' followed by any characters for
+                                                                    // `itemDescription`, lazy match, until seeing
+                                                                    // 'q/', 'e/', or 's/'.
 
-                    "(q/(?<itemQuantity>.+?)(?=(e/|s/)))?\\s*" +    // Optionally match 'q/' followed by the item quantity.
+                    "(q/(?<itemQuantity>.+?)(?=(e/|s/)))?\\s*" +    // Optionally match 'q/' followed by the
+                                                                    // item quantity.
 
-                    "(e/(?<itemExpirationDate>.+?)(?=s/))?\\s*" +   // Optionally match 'e/' followed by the expiration date.
+                    "(e/(?<itemExpirationDate>.+?)(?=s/))?\\s*" +   // Optionally match 'e/' followed by the
+                                                                    // expiration date.
 
-                    "(s/(?<itemSalePrice>.+?))(?=c/)" +             // Match 's/' followed by the sale price, until seeing 'c/'.
+                    "(s/(?<itemSalePrice>.+?))(?=c/)" +             // Match 's/' followed by the sale price, until
+                                                                    // seeing 'c/'.
 
                     "c/(?<itemCostPrice>.+)"                        // Finally, match 'c/' followed by the cost price.
     );
