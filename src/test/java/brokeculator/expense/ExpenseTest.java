48b036f276f71a4a51eea0cd12a4ea56b8ca34a3
mihirheda02 <87570498+mihirheda02@users.noreply.github.com>
2024-03-21 12:54:27 +0800

    Add tests for Expense and ExpenseManager classes

--- src/test/java/brokeculator/expense/ExpenseTest.java
+++ src/test/java/brokeculator/expense/ExpenseTest.java
@@ -25,4 +25,23 @@ public class ExpenseTest {
     void testGetCategory() {
         assertEquals("food", testExpense.getCategory());
     }
+
+    @Test
+    void testGetStringRepresentation() {
+        assertEquals("--expense--Lunch $12.50 (today) [FOOD]", testExpense.getStringRepresentation());
+    }
+
+    @Test
+    void testGetExpenseFromFile() {
+        String fileString = "Lunch $12.50 (today) [FOOD]";
+        try {
+            Expense expenseFromFile = Expense.getExpenseFromFile(fileString);
+            assertEquals("Lunch", expenseFromFile.getDescription());
+            assertEquals(12.50, expenseFromFile.getAmount());
+            assertEquals("today", expenseFromFile.getDate());
+            assertEquals("FOOD", expenseFromFile.getCategory());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
