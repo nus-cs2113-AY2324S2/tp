fffab70f6eba27875edbb4ea1a300742e94ab559
PradeepM <pradeepmanimaran@gmail.com>
2024-03-22 16:42:51 +0800

    Find-Command functionality

--- src/main/java/activeedge/ui/CommandUi.java
+++ src/main/java/activeedge/ui/CommandUi.java
@@ -7,6 +7,9 @@ import activeedge.task.LogMeals;
 
 public class CommandUi {
 
+    final static String LINE = "____________________________________________________________\n";
+
+
     public static void printMealList() {
         System.out.println("Here are your logged meals for today");
         int j = 1;
@@ -77,4 +80,39 @@ public class CommandUi {
         System.out.println("Total water consumed today: " + totalWaterIntake +
                 " ml (" + String.format("%.0f%%", percentage) + " of " + waterGoal + "ml goal).");
     }
+
+    public static void printMatchingTasks(String word) {
+        System.out.println(LINE + " Here are the matching tasks in your list:");
+        int matchingTasksIndex = 1;
+        boolean found = false;
+
+        // Search in the food section
+        for (int i = 0; i < tasksList.size(); i++) {
+            if (tasksList.get(i).toString().startsWith("Meal") && tasksList.get(i).toString().contains(word)) {
+                System.out.print(matchingTasksIndex + ". ");
+                System.out.println(tasksList.get(i).toString().substring(5) + " kcal");
+                matchingTasksIndex++;
+                found = true; // Indicate that a match was found
+            }
+        }
+
+        // Search in the water section
+        for (int i = 0; i < tasksList.size(); i++) {
+            if (tasksList.get(i).toString().startsWith("Water") && tasksList.get(i).toString().contains(word)) {
+                System.out.print(matchingTasksIndex + ". ");
+                System.out.println(tasksList.get(i).toString().substring(6) + " ml");
+                matchingTasksIndex++;
+                found = true; // Indicate that a match was found
+            }
+        }
+
+        // If no matching tasks were found, print a message
+        if (!found) {
+            System.out.println("No matching tasks found.");
+        }
+
+        System.out.println(LINE);
+    }
+
+
 }
