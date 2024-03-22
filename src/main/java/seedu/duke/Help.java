1496657778c7e203c8e9f74381ddd54b1541fd8c
Hafizuddin Bin Aminuddin <hafizuddinaminuddin1@gmail.com>
2024-03-23 00:22:11 +0800

    Update help

--- src/main/java/seedu/duke/Help.java
+++ src/main/java/seedu/duke/Help.java
@@ -6,7 +6,7 @@ public class Help {
                     "help: Access help menu.\n" +
                     "create <name>: Create a group.\n" +
                     "exit <name>: Exit current group.\n" +
-                    "member <name> /group <name>: Add a member to the group.\n" +
+                    "member <name> : Add a member to the group.\n" +
                     "expense /amount <amount> /paid <paid_by> /user <user_1> /user <user_2> ...: Add an expense.\n" +
                     "list: List all expenses in the group.\n" +
                     "balance <user_name>: Show user's balance.";
