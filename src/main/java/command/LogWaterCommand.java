eff47808a6171ce9f9d6bebe472c16458b8af9bc
alvinwongster <alvinwongster@gmail.com>
2024-03-23 01:34:36 +0800

    no message

--- src/main/java/command/LogWaterCommand.java
+++ src/main/java/command/LogWaterCommand.java
@@ -28,17 +28,15 @@ public class LogWaterCommand {
         int quantity = 0;
         try {
             quantity = Integer.parseInt(quantityString);
-            assert quantity > 0 : "Water quantity must be above 0";
-            WaterTask waterTask = new WaterTask(quantity);
-            TaskList.tasksList.add(waterTask);
-            CommandUi.printWaterLogMessage(waterTask);
         } catch (NumberFormatException e) {
             throw new ActiveEdgeException("Invalid water quantity. Please provide a valid integer.");
         }
-//        if (quantity <= 0) {
-//            System.out.println("Water quantity must be above 0. Please try again.");
-//        } else {
-//
-//        }
+        if (quantity <= 0) {
+            System.out.println("Water quantity must be above 0. Please try again.");
+        } else {
+            WaterTask waterTask = new WaterTask(quantity);
+            TaskList.tasksList.add(waterTask);
+            CommandUi.printWaterLogMessage(waterTask);
+        }
     }
 }
