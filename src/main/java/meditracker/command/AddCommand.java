25d3e37862d293322148bfbee9569d832a2609e1
nickczh <nickchua@u.nus.edu>
2024-03-22 13:36:40 +0800

    chore: Add assertion to Add command

--- src/main/java/meditracker/command/AddCommand.java
+++ src/main/java/meditracker/command/AddCommand.java
@@ -63,6 +63,7 @@ public class AddCommand extends Command {
         DailyMedication dailyMedication = new DailyMedication(medicationName);
         medicationManager.addMedication(medication);
         dailyMedicationManager.addDailyMedication(dailyMedication);
+        assert medicationManager.getTotalMedications() != 0 : "Total medications in medication manager should not be 0!";
         ui.showAddCommandMessage();
     }
 }
