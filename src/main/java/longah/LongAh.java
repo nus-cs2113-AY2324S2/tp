0fa242ffc846c7cdc4cf33e8b0dd0c6fee7c4af6
Sim Justin <1sim.justin@gmail.com>
2024-03-24 00:35:36 +0800

    Add logging class

--- src/main/java/longah/LongAh.java
+++ src/main/java/longah/LongAh.java
@@ -1,13 +1,9 @@
 package longah;
 
-import java.io.IOException;
 import java.util.Scanner;
-import java.util.logging.Logger;
-import java.util.logging.FileHandler;
-import java.util.logging.SimpleFormatter;
-import java.util.logging.Level;
 
 import longah.node.Group;
+import longah.util.Logging;
 import longah.exception.ExceptionMessage;
 import longah.exception.LongAhException;
 import longah.handler.InputHandler;
@@ -17,16 +13,9 @@ import longah.commands.Command;
  * LongAh class manages debts between members.
  */
 public class LongAh {
-    private static final Logger LongAhLogger = Logger.getLogger("LongAh");
+    private static final Logging log = new Logging();
     private static Group group;
-    private Scanner scanner;
-
-    /**
-     * Constructs a new LongAh instance.
-     */
-    public LongAh() {
-        this.scanner = new Scanner(System.in);
-    }
+    private Scanner scanner = new Scanner(System.in);
 
     /**
      * The main method to run the LongAh application.
@@ -34,28 +23,18 @@ public class LongAh {
      * @param args The command-line arguments.
      */
     public static void main(String[] args) {
-        try {
-            FileHandler handler = new FileHandler("./log/LongAh.log");
-            handler.setFormatter(new SimpleFormatter());
-            LongAhLogger.addHandler(handler);
-            LongAhLogger.setUseParentHandlers(false);
-        } catch (IOException e) {
-            LongAhLogger.log(Level.WARNING, "Log data may not be saved due to permission.");
-        }
-
-        LongAhLogger.log(Level.INFO, "Starting Pre-program preparations.");
+        Logging.logInfo("Starting Pre-program preparations.");
         System.out.println("Welcome to LongAh!");
         LongAh app = new LongAh();
         try {
-            LongAhLogger.log(Level.INFO, "Loading previous member and transaction info.");
             group = new Group("group"); // Give a temporary name for now
         } catch (LongAhException e) {
-            LongAhLogger.log(Level.WARNING, "Loading process fails! Unable to create file or " +
+            Logging.logWarning("Loading process fails! Unable to create file or " +
                     "file could not be access.");
             LongAhException.printException(e);
         }
 
-        LongAhLogger.log(Level.INFO, "Entering main program body. Begin accepting user commands.");
+        Logging.logInfo("Entering main program body. Begin accepting user commands.");
         while (true) {
             try {
                 System.out.print("Enter command: ");
@@ -76,7 +55,7 @@ public class LongAh {
                 if (e.getMessage().equals(ExceptionMessage.TRANSACTIONS_SUMMED_UP.getMessage()) ||
                         e.getMessage().equals(ExceptionMessage.NO_DEBTS_FOUND.getMessage()) || 
                         e.getMessage().equals(ExceptionMessage.NO_TRANSACTION_FOUND.getMessage())) {
-                    LongAhLogger.log(Level.WARNING, "The previous user command caused an error. " +
+                    Logging.logWarning("The previous user command caused an error. " +
                             "Check the returned error message for details");
                 }
             }
