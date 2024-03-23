bfee706000bbc01777616661bb93c0b95e9d84e8
LAPTOP-D31L41D6\elyovs <elyovsun@gmail.com>
2024-03-24 02:21:10 +0800

    Add creating file and read txt file in the beginning of program

--- src/main/java/bookmarked/BookMarked.java
+++ src/main/java/bookmarked/BookMarked.java
@@ -2,17 +2,22 @@ package bookmarked;
 
 import bookmarked.parser.Parser;
 import bookmarked.ui.Ui;
+import bookmarked.storage.BookStorage;
 
+import java.io.File;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class BookMarked {
-
+    private static final String BOOK_FILE_PATH = "./book.txt";
     public static ArrayList<Book> listOfBooks = new ArrayList<>();
     public static int numberOfBooks = 0;
 
     public static void main(String[] args) {
         Ui.greetings();
+
+        File bookDataFile = BookStorage.createFile(BOOK_FILE_PATH);
+        listOfBooks = BookStorage.readFileStorage(bookDataFile);
 
         Scanner in = new Scanner(System.in);
         String newItem = in.nextLine();
