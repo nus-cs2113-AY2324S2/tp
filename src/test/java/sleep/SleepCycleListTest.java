1a09603090380724ed25afcb6b3df9ddcad006e3
OKW32 <e0969674@u.nus.edu>
2024-03-24 01:42:47 +0800

    Edit tests for Sleep Tracker

--- src/test/java/sleep/SleepCycleListTest.java
+++ src/test/java/sleep/SleepCycleListTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,7 +20,7 @@ public class SleepCycleListTest {
     @Test
     public void addSleepCycle_addCycle_success() {
         LocalDate date;
-        date = LocalDate.parse("25/01/2022");
+        date = LocalDate.parse("25/01/2022", DateTimeFormatter.ofPattern("dd/MM/yyyy") );
         SleepCycle cycle = new SleepCycle(2, date);
         sleepCycleList.addSleepCycle(cycle);
         assertEquals(1, sleepCycleList.getNumberOfCycles());
@@ -29,8 +30,8 @@ public class SleepCycleListTest {
     public void addTwoSleepCycle_getTotalHoursSlept_success() {
         LocalDate date1;
         LocalDate date2;
-        date1 = LocalDate.parse("25/01/2022");
-        date2 = LocalDate.parse("26/01/2022");
+        date1 = LocalDate.parse("25/01/2022", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        date2 = LocalDate.parse("26/01/2022", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         SleepCycle cycle = new SleepCycle(2, date1);
         sleepCycleList.addSleepCycle(cycle);
         cycle = new SleepCycle(3, date2);
