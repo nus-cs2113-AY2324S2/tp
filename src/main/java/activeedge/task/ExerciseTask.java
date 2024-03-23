bfc02b5a890442de8c10942d1c34c0496f15c993
SuveenE <suveen.te1@gmail.com>
2024-03-23 21:25:54 +0800

    Add documentation for ExerciseTask class

--- src/main/java/activeedge/task/ExerciseTask.java
+++ src/main/java/activeedge/task/ExerciseTask.java
@@ -1,10 +1,22 @@
 package activeedge.task;
 
+/**
+ * Represents an exercise task that extends the functionality of a basic task
+ * to include exercise-specific details. It captures the name, duration, and
+ * calories burnt of an exercise activity.
+ */
 public class ExerciseTask extends Task{
     protected String exerciseName;
     protected int duration;
     protected Integer caloriesBurnt;
 
+    /**
+     * Constructs a new exercise log with the specified exercise name, duration, and calories burnt.
+     *
+     * @param exerciseName the name of the exercise
+     * @param duration the duration of the exercise, in minutes
+     * @param caloriesBurnt the number of calories burnt during the exercise
+     */
     public ExerciseTask(String exerciseName, int duration, int caloriesBurnt){
         super(exerciseName);
         this.duration = duration;
@@ -20,6 +32,10 @@ public class ExerciseTask extends Task{
         return caloriesBurnt;
     }
 
+    /**
+     * Returns a string representation of the exercise task, including its name,
+     * duration, and calories burnt.
+     */
     @Override
     public String toString() {
         return "Exercise " + this.getExerciseName() + " " + this.getDuration() + " " + this.getCaloriesBurnt();
