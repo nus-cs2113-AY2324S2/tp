4aa5577dc4f0a3896695aee9efcdf7bc2b601538
sweijie24 <e0958828@u.nus.edu>
2024-03-23 16:32:59 +0800

    Add getters and setters for currency

--- src/main/java/seedu/budgetbuddy/Transaction.java
+++ src/main/java/seedu/budgetbuddy/Transaction.java
@@ -26,4 +26,11 @@ public abstract class Transaction {
         this.amount = amount;
     }
 
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
 }
