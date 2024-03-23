95ed2af054e92bf92e66791723bde2ba1fcf9051
sweijie24 <e0958828@u.nus.edu>
2024-03-23 16:38:02 +0800

    Create command class to handle parameters and execute the conversion code

---
+++ src/main/java/seedu/budgetbuddy/command/ChangeCurrencyCommand.java
@@ -0,0 +1,28 @@
+package seedu.budgetbuddy.command;
+
+import seedu.budgetbuddy.CurrencyConverter;
+import seedu.budgetbuddy.ExpenseList;
+import seedu.budgetbuddy.SavingList;
+
+import java.util.Currency;
+
+public class ChangeCurrencyCommand extends Command {
+
+    private Currency newCurrency;
+    private SavingList savings;
+    private ExpenseList expenses;
+    private CurrencyConverter currencyConverter;
+
+    public ChangeCurrencyCommand(Currency newCurrency, SavingList savings, ExpenseList expenses, CurrencyConverter currencyConverter) {
+        this.newCurrency = newCurrency;
+        this.savings = savings;
+        this.expenses = expenses;
+        this.currencyConverter = currencyConverter;
+    }
+
+    @Override
+    public void execute() {
+        currencyConverter.convertCurrency(newCurrency, savings);
+        currencyConverter.convertCurrency(newCurrency, expenses);
+    }
+}
