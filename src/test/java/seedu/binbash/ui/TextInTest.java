108914eaeecdff921d882627aae4fc2f3f232e54
nkotaa <e0957774@u.nus.edu>
2024-03-24 15:22:48 +0800

    Update to meet checkstyle

--- src/test/java/seedu/binbash/ui/TextInTest.java
+++ src/test/java/seedu/binbash/ui/TextInTest.java
@@ -2,11 +2,6 @@ package seedu.binbash.ui;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.ByteArrayInputStream;
-import java.io.PrintWriter;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TextInTest {
