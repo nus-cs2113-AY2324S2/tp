e3231fb8bf3e978fdd027e3fb71f53ddb0c18424
nickczh <nickchua@u.nus.edu>
2024-03-25 00:30:01 +0800

    feat: Add attributes for more dosage timings and change type to double

--- src/main/java/meditracker/medication/Medication.java
+++ src/main/java/meditracker/medication/Medication.java
@@ -9,29 +9,42 @@ public class Medication {
 
     // Medication attributes are intentionally declared String. To be modified later.
     private String name;
-    private String quantity;
-    private String dosage;
+    private Double quantity;
+    private Double dosage;
+    private Double dosageMorning;
+    private Double dosageAfternoon;
+    private Double dosageEvening;
     private String expiryDate;
     private String intakeFreq;
     private String remarks;
+    private String repeat;
 
     /**
      * Constructs a Medication object with the specified information.
      * @param name The name of the medication.
      * @param quantity The quantity of the medication.
      * @param dosage The dosage of the medication.
+     * @param dosageMorning The morning dosage of the medication.
+     * @param dosageAfternoon The afternoon dosage of the medication.
+     * @param dosageEvening The evening dosage of the medication.
      * @param expiryDate The expiry date of the medication.
-     * @param intakeFreq The intake frequency of the medication.
+     * @param intakeFreq The intake frequency of the medication in a day.
      * @param remarks Any remarks or notes about the medication.
+     * @param repeat The repeat frequency of the medication.
      */
-    public Medication(String name, String quantity, String dosage, String expiryDate,
-                      String intakeFreq, String remarks) {
+    public Medication(String name, Double quantity, Double dosage, Double dosageMorning, Double dosageAfternoon,
+                      Double dosageEvening, String expiryDate,
+                      String intakeFreq, String remarks, String repeat) {
         this.name = name;
         this.quantity = quantity;
         this.dosage = dosage;
+        this.dosageMorning = dosageMorning;
+        this.dosageAfternoon = dosageAfternoon;
+        this.dosageEvening = dosageEvening;
         this.expiryDate = expiryDate;
         this.intakeFreq = intakeFreq;
         this.remarks = remarks;
+        this.repeat = repeat;
     }
 
     public String getName() {
@@ -42,20 +55,44 @@ public class Medication {
         this.name = name;
     }
 
-    public String getQuantity() {
+    public Double getQuantity() {
         return quantity;
     }
 
     public void setQuantity(String quantity) {
-        this.quantity = quantity;
+        this.quantity = Double.parseDouble(quantity);
     }
 
-    public String getDosage() {
+    public Double getDosage() {
         return dosage;
     }
 
     public void setDosage(String dosage) {
-        this.dosage = dosage;
+        this.dosage = Double.parseDouble(dosage);
+    }
+
+    public Double getDosageMorning() {
+        return dosageMorning;
+    }
+
+    public void setDosageMorning(Double dosageMorning) {
+        this.dosageMorning = dosageMorning;
+    }
+
+    public Double getDosageAfternoon() {
+        return dosageAfternoon;
+    }
+
+    public void setDosageAfternoon(Double dosageAfternoon) {
+        this.dosageAfternoon = dosageAfternoon;
+    }
+
+    public Double getDosageEvening() {
+        return dosageEvening;
+    }
+
+    public void setDosageEvening(Double dosageEvening) {
+        this.dosageEvening = dosageEvening;
     }
 
     public String getExpiryDate() {
@@ -80,6 +117,14 @@ public class Medication {
 
     public void setRemarks(String remarks) {
         this.remarks = remarks;
+    }
+
+    public String getRepeat() {
+        return repeat;
+    }
+
+    public void setRepeat(String repeat) {
+        this.repeat = repeat;
     }
 
     @Override
