3782e7e43507737b8c44f85f623b6265b032f8d7
nickczh <nickchua@u.nus.edu>
2024-03-25 00:32:33 +0800

    feat: Handle exceptions for various invalid user input

--- src/main/java/meditracker/MediTracker.java
+++ src/main/java/meditracker/MediTracker.java
@@ -5,7 +5,7 @@ import meditracker.exception.ArgumentNotFoundException;
 import meditracker.exception.MediTrackerException;
 import meditracker.logging.MediLogger;
 import meditracker.medication.MedicationManager;
-import meditracker.parser.Parser;
+import meditracker.command.CommandParser;
 import meditracker.storage.FileReaderWriter;
 import meditracker.ui.Ui;
 
@@ -46,9 +46,11 @@ public class MediTracker {
      * This method displays a welcome message, reads user commands, and processes them until the user exits the
      * application.
      * @throws MediTrackerException If an error occurs during the execution of the application.
-     * @throws ArgumentNotFoundException Argument required not found
+     * @throws ArgumentNotFoundException Argument required not found.
+     * @throws NullPointerException When the command does not exist.
+     * @throws NumberFormatException If the argument of type double is not supplied as type double.
      */
-    public void run() throws MediTrackerException, ArgumentNotFoundException {
+    public void run() {
         //@@author nickczh-reused
         //Reused from https://github.com/nickczh/ip
         //with minor modifications
@@ -58,9 +60,17 @@ public class MediTracker {
         while (!isExit) {
             String fullCommand = ui.readCommand();
             ui.showLine();
-            Command command = Parser.parse(fullCommand);
-            command.execute(medicationManager, dailyMedicationManager, ui);
-            isExit = command.isExit();
+            try {
+                Command command = CommandParser.parse(fullCommand);
+                command.execute(medicationManager, dailyMedicationManager, ui);
+                isExit = command.isExit();
+            } catch (ArgumentNotFoundException | MediTrackerException ex) {
+                System.out.println(ex.getMessage());
+            } catch (NullPointerException ex) {
+                System.out.println("Invalid MediTracker command! Please refer to the user guide.");
+            } catch (NumberFormatException ex) {
+                System.out.println("Dosage/Quantity should be of type double!");
+            }
         }
     }
 
@@ -68,10 +78,8 @@ public class MediTracker {
      * Starts the MediTracker application.
      * It creates a new MediTracker object and calls its run() method.
      * @param args Command-line arguments.
-     * @throws MediTrackerException If an error occurs during the execution of the application.
-     * @throws ArgumentNotFoundException Argument required not found
      */
-    public static void main(String[] args) throws MediTrackerException, ArgumentNotFoundException {
+    public static void main(String[] args) {
         MediLogger.initialiseLogger();
 
         List<String> dailyMedicationList = FileReaderWriter.loadDailyMedicationData();
