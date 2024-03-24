6b37cc40ac9309e37e0b78c7bfac898e4c7972d0
JiahaoGan <u3585924@connect.hku.hk>
2024-03-24 16:08:25 +0800

    Add Duke Class back

---
+++ src/main/java/seedu/duke/Duke.java
@@ -0,0 +1,28 @@
+package seedu.duke;
+
+import seedu.duke.exception.ProcessInputException;
+import seedu.duke.ui.Ui;
+
+public class Duke {
+
+    public Duke() {
+    }
+
+    /**
+     * Main entry-point for the java.duke.Duke application.
+     */
+    public static void main(String[] args) {
+        Formatter.printWelcomeMsg();
+
+        while (Ui.getIsRunning()) {
+            Formatter.printGoalBeforeShot(Ui.roundCount);
+            try {
+                Ui.beginListening();
+                Ui.processInput();
+                Ui.executeCommand();
+            } catch (ProcessInputException e) {
+                Formatter.printErrorExecutionFail();
+            }
+        }
+    }
+}
