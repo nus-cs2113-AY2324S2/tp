2bd1b1fe71fdba9175ea2a5e4beadc756a684369
a-wild-chocolate <69574098+a-wild-chocolate@users.noreply.github.com>
2024-03-26 03:49:35 +0800

    Add sleep feature

---
+++ src/test/java/seedu/lifetrack/ParserSleepTest.java
@@ -0,0 +1,70 @@
+package seedu.lifetrack;
+
+import org.junit.jupiter.api.Test;
+import seedu.lifetrack.system.exceptions.InvalidInputException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.lifetrack.system.parser.ParserSleep.parseSleepInput;
+
+public class ParserSleepTest {
+    @Test
+    public void parseSleepInput_inputContains2Duration_invalidInputExceptionThrown() {
+        // setup test
+        String invalidInput = "sleep add t/8.0 t/9.2";
+        // Call methods to test
+        try {
+            parseSleepInput(invalidInput);
+        } catch (InvalidInputException e) {
+            assertEquals("Please ensure that you have keyed in the correct format: " +
+                    "sleep add t/<duration> d/<date>", e.getMessage());
+        }
+    }
+    @Test
+    public void parseSleepInput_inputContains2Date_invalidInputExceptionThrown() {
+        // setup test
+        String invalidInput = "sleep add d/110324 d/280524";
+        // Call methods to test
+        try {
+            parseSleepInput(invalidInput);
+        } catch (InvalidInputException e) {
+            assertEquals("Please ensure that you have keyed in the correct format: " +
+                    "sleep add t/<duration> d/<date>", e.getMessage());
+        }
+    }
+    @Test
+    public void parseSleepInput_inputMissingDuration_invalidInputExceptionThrown() {
+        // setup test
+        String invalidInput = "sleep add d/110324";
+        // Call methods to test
+        try {
+            parseSleepInput(invalidInput);
+        } catch (InvalidInputException e) {
+            assertEquals("Please ensure that you have keyed in the correct format: " +
+                    "sleep add t/<duration> d/<date>", e.getMessage());
+        }
+    }
+
+    @Test
+    public void parseSleepInput_inputNonPositiveValueForDuration_invalidInputExceptionThrown() {
+        // setup test
+        String invalidInput = "sleep add t/-2 d/110324";
+
+        // Call methods to test
+        try {
+            parseSleepInput(invalidInput);
+        } catch (InvalidInputException e) {
+            assertEquals("\t Please input only positive real number into the sleep duration field!"
+                    , e.getMessage());
+        }
+    }
+    @Test
+    public void parseLiquidInput_missingKeywords_exceptionThrown() {
+
+        try {
+            parseSleepInput("sleep add");
+        } catch (InvalidInputException e) {
+            assertEquals("Please ensure that you have keyed in the correct format: " +
+                    "sleep add t/<duration> d/<date>", e.getMessage());
+        }
+    }
+}
