83bc7098baa56b5fd0550d5e03322a1af790659d
liuy1103 <e0968020@u.nus.edu>
2024-03-25 22:25:23 +0800

    Add GiftList and view history of gifts

--- src/main/java/seedu/duke/ViewHistoryCommand.java
+++ src/main/java/seedu/duke/ViewHistoryCommand.java
@@ -8,6 +8,8 @@ public class ViewHistoryCommand extends Command {
                         Storage storage, UserDetails userDetails, GiftList gifts) throws FlirtForkException {
         int foodCount = 0;
         int activityCount = 0;
+        int giftCount = 0;
+
         System.out.println("These are the activities you have marked:");
         for (int i=0; i<activities.size(); i++) {
             Activity oneActivity = activities.get(i);
@@ -15,12 +17,22 @@ public class ViewHistoryCommand extends Command {
                 System.out.println(++activityCount + ". " + oneActivity.description);
             }
         }
+
         System.out.println("");
         System.out.println("These are the restaurants you have marked:");
         for (int i=0; i<foods.size(); i++) {
             Food oneFood = foods.get(i);
             if (oneFood.completionStatus.equals("C")) {
                 System.out.println(++foodCount + ". " + oneFood.description);
+            }
+        }
+
+        System.out.println("");
+        System.out.println("These are the gifts you've marked:");
+        for (int i=0; i<gifts.size(); i++) {
+            Gift oneGift = gifts.get(i);
+            if (oneGift.completionStatus.equals("C")) {
+                System.out.println(++giftCount + ". " + oneGift.description);
             }
         }
     }
