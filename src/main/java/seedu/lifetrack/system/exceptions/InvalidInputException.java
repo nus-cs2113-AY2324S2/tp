03abd95d480c39d2f07094db7b42b6eceda0b820
a-wild-chocolate <69574098+a-wild-chocolate@users.noreply.github.com>
2024-03-25 15:55:18 +0800

    fix logger

--- src/main/java/seedu/lifetrack/system/exceptions/InvalidInputException.java
+++ src/main/java/seedu/lifetrack/system/exceptions/InvalidInputException.java
@@ -1,11 +1,16 @@
 package seedu.lifetrack.system.exceptions;
+import seedu.lifetrack.calories.calorielist.CalorieList;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class InvalidInputException extends Exception {
 
     public final String heythere = "";
-
+    private static Logger logr = Logger.getLogger(CalorieList.class.getName());
     public InvalidInputException(){
         super("\t Please ensure that you have keyed in the correct format!");
+        logr.setLevel(Level.SEVERE);
+        logr.log(Level.WARNING,"\t Please ensure that you have keyed in the correct format!");
     }
 
     public InvalidInputException(String exception) {
