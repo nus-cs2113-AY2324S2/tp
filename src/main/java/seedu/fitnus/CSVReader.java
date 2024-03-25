a7ab8b442fb4ff9d09273c7874535c6b8594f85d
tinaliu27 <27tinaliu@gmail.com>
2024-03-26 06:04:44 +0800

    added methods for reading and writing

--- src/main/java/seedu/fitnus/CSVReader.java
+++ src/main/java/seedu/fitnus/CSVReader.java
@@ -3,12 +3,18 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class CSVReader {
+    private static HashMap<String, String[]> foodItems = new HashMap<>();
+
     public static final String DELIMITER = ",";
     public static void main(String[] args){
-        String csvFile = "./db/Meal_db.csv";
-        CSVReader.read(csvFile);
+        String mealCsvFile = "./db/Meal_db.csv";
+        String drinkCSVFile = "./db/Drink_db.csv";
+        CSVReader.read(mealCsvFile);
+        printInfo();
+        readMealInfo(mealCsvFile, "Pepper lunch ");
     }
     public static void read(String filename) {
         try{
@@ -20,14 +26,52 @@ public class CSVReader {
             String[] tempArr;
             while ((line = br.readLine()) != null) {
                 tempArr = line.split(DELIMITER);
-                for (String tempStr : tempArr) {
-                    System.out.print(tempStr + " ");
+                String itemFullName = tempArr[0];
+                String[] itemDetails = new String[tempArr.length-1];
+                for(int i = 1; i < tempArr.length; i++) {
+                    itemDetails[i-1] = tempArr[i];
                 }
-                System.out.println();
+                foodItems.put(itemFullName, itemDetails);
+
             }
             br.close();
         } catch (IOException ex) {
             ex.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void readMealInfo(String filename, String foodName) {
+        read(filename);
+        boolean found = false;
+        String[] nutrientInfo = null;
+        if (foodItems.containsKey(foodName)) {
+            nutrientInfo = foodItems.get(foodName);
+            found = true;
+        } else {
+            System.out.println("Error! Food not found. Please input a valid item.");
+        }
+        if(found && nutrientInfo != null)
+            for(String nutrients: nutrientInfo) {
+                System.out.println(nutrients);
+            }
+    }
+
+
+
+    public static void printInfo() {
+        for (HashMap.Entry<String, String[]> entry : foodItems.entrySet()) {
+            String key = entry.getKey();
+            String[] value = entry.getValue();
+            System.out.print("Key: " + key + ", Value: [");
+            for (int i = 0; i < value.length; i++) {
+                System.out.print(value[i]);
+                if (i < value.length - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println("]");
         }
     }
 }
+
---
+++ src/main/java/seedu/fitnus/CSVWriter.java
@@ -0,0 +1,37 @@
+package seedu.fitnus;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class CSVWriter {
+    public static void main(String[] args) {
+        writeIntoFile("hi", "FOOD");
+    }
+    public static void writeIntoFile(String foodItem, String fileName) {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        File file = null;
+        if (fileName.toLowerCase().contains("food")) {
+            file = new File("./db/Output_Food_" + df.format(new Date())+".csv");
+        } else {
+            file = new File("./db/Output_Drink_" + df.format(new Date())+".csv");
+        }
+        if (!file.exists()) {
+            try{
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("error with creating a file");
+            }
+        }
+        try (FileWriter fw = new FileWriter(file)){
+            BufferedWriter writer = new BufferedWriter(fw);
+            writer.write(foodItem);
+            writer.newLine();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("error trying to add something into the file");
+        }
+    }
+}
