46581d0919be57223d212bbba293a12a1858fb47
JackieNeoCEG <e0959140@u.nus.edu>
2024-03-25 18:18:57 +0800

    Add Help menu

--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -14,30 +14,30 @@ public class Ui {
     };
 
     private String logo = "\n\n\n" + LINE + "\n" +
-            "               _______  _______  ______    _______    \n" +
-            "              |   _   ||       ||    _ |  |       |   \n" +
-            "  __|____|__  |  |_|  ||    ___||   | ||  |   _   |   \n" +
-            "    |  X |    |       ||   |___ |   |_||_ |  | |  |   \n" +
-            "  __|____|__  |       ||    ___||    __  ||  |_|  |   \n" +
-            "   O|  X |O   |   _   ||   |___ |   |  | ||       |   \n" +
-            "              |__| |__||_______||___|  |_||_______|   \n" +
-            "               _______  _______  ______   _______     \n" +
-            "              |       ||   _   ||      | |       |    \n" +
-            "              |       ||  |_|  ||  _    ||    ___|    \n" +
+            "               _______  _______  ______    _______              \n" +
+            "              |   _   ||       ||    _ |  |       |             \n" +
+            "  __|____|__  |  |_|  ||    ___||   | ||  |   _   |             \n" +
+            "    |  X |    |       ||   |___ |   |_||_ |  | |  |             \n" +
+            "  __|____|__  |       ||    ___||    __  ||  |_|  |             \n" +
+            "   O|  X |O   |   _   ||   |___ |   |  | ||       |             \n" +
+            "              |__| |__||_______||___|  |_||_______|             \n" +
+            "               _______  _______  ______   _______               \n" +
+            "              |       ||   _   ||      | |       |              \n" +
+            "              |       ||  |_|  ||  _    ||    ___|              \n" +
             "              |       ||       || | |   ||   |___     |  @      \n" +
             "              |      _||       || |_|   ||    ___|    | /|\\    \n" +
             "              |     |_ |   _   ||       ||   |___     |  |      \n" +
             "              |_______||__| |__||______| |_______|    | / \\  \n\n" ;
 
     private String boeing =
-            "                                |\n" +
-            "                              .-'-.\n" +
-            "                             ' ___ '\n" +
-            "                   ---------'  .-.  '---------\n" +
-            "   _________________________'  '-'  '_________________________\n" +
+            "                                |                              \n" +
+            "                              .-'-.                            \n" +
+            "                             ' ___ '                           \n" +
+            "                   ---------'  .-.  '---------                 \n" +
+            "   _________________________'  '-'  '_________________________ \n" +
             "    ''''''-|---|--/    \\==][^',_m_,'^][==/    \\--|---|-''''''\n" +
-            "                  \\    /  ||/   H   \\||  \\    /\n" +
-            "                   '--'   OO   O|O   OO   '--' \n\n";
+            "                  \\    /  ||/   H   \\||  \\    /             \n" +
+            "                   '--'   OO   O|O   OO   '--' \n              \n";
     public void println(String s) {
         System.out.println(s);
     }
@@ -47,27 +47,32 @@ public class Ui {
         println(logo + boeing + LINE +
             "\n              [      Welcome to the AeroCade      ]\n" + LINE);
         println(
-            "              [          Choose your game:        ]\n" +
-            "                                |                        \n" +
-            "        X  |  -  |  -           |          _______       \n" +
-            "       ____|_____|____          |          |     |       \n" +
-            "           |     |              |          |     @       \n" +
-            "        -  |  O  |  X           |          |    /|\\     \n" +
-            "       ____|_____|____          |          |     |       \n" +
-            "           |     |              |          |    /|\\     \n" +
-            "        -  |  O  |  -           |        ==============  \n" +
-            "                                |                        \n" +
-            "         TIC-TAC-TOE            |           HANGMAN        " +
-                    "\n\n\nType 'help' for a list of available commands!\n");
+            "              [          Choose your game:        ]                \n" +
+            "                                |                                  \n" +
+            "        X  |  -  |  -           |          _______                 \n" +
+            "       ____|_____|____          |          |     |                 \n" +
+            "           |     |              |          |     @                 \n" +
+            "        -  |  O  |  X           |          |    /|\\               \n" +
+            "       ____|_____|____          |          |     |                 \n" +
+            "           |     |              |          |    / \\               \n" +
+            "        -  |  O  |  -           |        ==============            \n" +
+            "________________________________|________________________________  \n" +
+            "         TIC-TAC-TOE            |           HANGMAN                \n" +
+            "          Type 'TTT'            |        Type 'hangman'            \n" +
+            "\n\nType 'help' for a list of available commands!                  \n");
     }
 
     public void byeUser() {
         int randomIndex = new Random().nextInt(byeSentences.length);
-        println("\n" + byeSentences[randomIndex] + "\n");
+        println(LINE + "\n\n" + boeing + byeSentences[randomIndex] + "\n");
     }
 
     public void printHelp() {
-        println("help menu to be added");
+        println(LINE + "\nHere are the list of commands:\n" +
+                "'TTT' - Starts Tic-tac-toe Game\n\n" +
+                "type 'help TTT' for Tic-tac-toe commands\n" +
+                "'hangman' - Starts Hangman Game\n" +
+                "type 'help hangman' for Hangman commands\n" + LINE);
     }
 }
 
