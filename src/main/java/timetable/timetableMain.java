18c7787acfc3159e0ef1367828bad4eec59a3afd
hikohiko <duliyan@u.nus.edu>
2024-03-26 15:22:39 +0800

    added 2.0 timetable function draft

---
+++ src/main/java/timetable/timetableMain.java
@@ -0,0 +1,27 @@
+package timetable;
+
+
+import seedu.duke.Expenditure;
+
+import java.util.ArrayList;
+
+public class timetableMain {
+
+    public static int classCount;
+
+    private static ArrayList<days> mon;
+    private static ArrayList<days> tue;
+    private static ArrayList<days> wed;
+
+    private static ArrayList<days> thurs;
+    private static ArrayList<days> fri;
+
+    public timetableMain() {
+        mon = new ArrayList<>();
+        classCount = 0;
+    }
+
+
+
+
+}
