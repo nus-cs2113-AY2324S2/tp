9a291054e91ecc2acf0826c4b9d5ad227d5e79eb
jensonjenkins <dave.martheen@gmail.com>
2024-03-27 00:33:52 +0800

    add MCs to viewing table

--- src/main/java/seedu/duke/enums/CEGModules.java
+++ src/main/java/seedu/duke/enums/CEGModules.java
@@ -1,34 +1,54 @@
 package seedu.duke.enums;
 
 public enum CEGModules {
-    GESS,
-    GEC,
-    GEN,
-    ES2631,
-    CS1010,
-    GEA1000,
-    DTK1234,
-    EG1311,
-    IE2141,
-    EE2211,
-    EG2501,
-    CDE2000,
-    PF1101,
-    CG4002,
-    MA1511,
-    MA1508E,
-    EG2401A,
-    CP3880,
-    EG3611A,
-    CG1111A,
-    CG2111A,
-    CS1231,
-    CG2023,
-    CG2027,
-    CG2028,
-    CG2271,
-    CS2040C,
-    CS2113,
-    EE2026,
-    EE4204
+    GESS(4),
+    GEC(4),
+    GEN(4),
+    ES2631(4),
+    CS1010(4),
+    GEA1000(4),
+    DTK1234(4),
+    EG1311(4),
+    IE2141(4),
+    EE2211(4),
+    EG2501(4),
+    CDE2000(4),
+    PF1101(4),
+    CG4002(8),
+    MA1511(2),
+    MA1512(2),
+    MA1508E(4),
+    EG2401A(2),
+    CP3880(12),
+    EG3611A(10),
+    CG1111A(4),
+    CG2111A(4),
+    CS1231(4),
+    CG2023(4),
+    CG2027(2),
+    CG2028(2),
+    CG2271(4),
+    CS2040C(4),
+    CS2113(4),
+    EE2026(4),
+    EE4204(4);
+
+    private final int moduleMC;
+
+    CEGModules(int moduleMC){
+        this.moduleMC = moduleMC;
+    }
+
+    public int getModuleMC(){
+        return moduleMC;
+    }
+
+    public static CEGModules mapStringToEnum(String moduleCode) {
+        for (CEGModules cegModule : CEGModules.values()) {
+            if (cegModule.name().equalsIgnoreCase(moduleCode)) {
+                return cegModule;
+            }
+        }
+        throw new IllegalArgumentException("No module code " + moduleCode + " found in CEGModules");
+    }
 }
