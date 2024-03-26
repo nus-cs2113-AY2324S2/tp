85a52c122f190b896c52ba8b2df4ed701e1f1666
PureUsagi <ngjunhancanoe@gmail.com>
2024-03-26 21:36:22 +0800

    Add initialisation and test code for logging funtionality to BinBash class

--- src/main/java/seedu/binbash/BinBash.java
+++ src/main/java/seedu/binbash/BinBash.java
@@ -1,25 +1,42 @@
 package seedu.binbash;
 
+import seedu.binbash.logger.MainLogger;
 import seedu.binbash.storage.Storage;
 import seedu.binbash.ui.Ui;
 import seedu.binbash.command.Command;
 import seedu.binbash.command.ByeCommand;
 import seedu.binbash.exceptions.BinBashException;
 
+import javax.imageio.stream.FileImageOutputStream;
+
 public class BinBash {
     private Ui userInterface;
     private ItemList itemList;
     private Parser inputParser;
     private Storage storage;
+    private MainLogger logger;
 
     public BinBash() {
         userInterface = new Ui();
         storage = new Storage();
         itemList = new ItemList(storage.loadData());
         inputParser = new Parser(itemList);
+
+        try {
+            logger = new MainLogger(BinBash.class.getName());
+        } catch (BinBashException e) {
+            userInterface.talk(e.getMessage());
+        }
     }
 
     private void run() {
+        try {
+            logger.createLogFile();
+        } catch (BinBashException e) {
+            userInterface.talk(e.getMessage());
+        }
+        logger.info("Program started");
+
         userInterface.greet();
 
         while (userInterface.isUserActive()) {
