2fe86d9995d3ffe8b9bfbb79f29a4ea8bf2d82e7
luozihui2003 <e0969601@u.nus.edu>
2024-03-26 16:17:48 +0800

    Fix Assertion

--- src/test/java/grocery/GroceryTest.java
+++ src/test/java/grocery/GroceryTest.java
@@ -8,14 +8,14 @@ class GroceryTest {
     @Test
     public void printGrocery_noAmountNoExpiration_leaveEmpty() {
         Grocery grocery = new Grocery("apple", 0, null, "fruit", 0);
-        String message = "apple expiration date not set cost not set";
+        String message = "apple (fruit) pieces expiration date not set cost not set";
         assertEquals(message, grocery.printGrocery());
     }
 
     @Test
     public void printGrocery_costWrongFormat_formattedCost() {
         Grocery grocery = new Grocery("chicken", 1, LocalDate.now().plusDays(1), "meat",1);
-        String message = "chicken" + ", amount: 1" + ", expiration: "
+        String message = "chicken (meat) " + ", amount: 1grams" + ", expiration: "
                 + LocalDate.now().plusDays(1) + ", cost: $1.00";
         assertEquals(message, grocery.printGrocery());
     }
@@ -23,7 +23,7 @@ class GroceryTest {
     @Test
     public void printGrocery_correctAmtAndExpAndCost() {
         Grocery grocery = new Grocery("chicken", 1, LocalDate.now().plusDays(1), "meat",1.20);
-        String message = "chicken" + ", amount: 1" + ", expiration: "
+        String message = "chicken (meat) " + ", amount: 1grams" + ", expiration: "
                 + LocalDate.now().plusDays(1) + ", cost: $1.20";
         assertEquals(message, grocery.printGrocery());
     }
