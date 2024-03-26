0253a28f1d0764238f26bf36709ac202cfcf4637
JianJiaT <tjjtjn@gmail.com>
2024-03-26 22:32:28 +0800

    Change SummariseCommand to take expense name, date and category as fields

--- src/main/java/brokeculator/command/SummariseCommand.java
+++ src/main/java/brokeculator/command/SummariseCommand.java
@@ -3,11 +3,19 @@ package brokeculator.command;
 import brokeculator.dashboard.Dashboard;
 import brokeculator.frontend.UI;
 
+import java.time.LocalDateTime;
+
 public class SummariseCommand extends Command {
+    private String name;
+    private LocalDateTime date;
+    private String category;
     private int beginIndex;
     private int endIndex;
 
-    public SummariseCommand(int beginIndex, int endIndex) {
+    public SummariseCommand(String name, LocalDateTime date, String category, int beginIndex, int endIndex) {
+        this.name = name;
+        this.date = date;
+        this.category = category;
         this.beginIndex = beginIndex;
         this.endIndex = endIndex;
     }
