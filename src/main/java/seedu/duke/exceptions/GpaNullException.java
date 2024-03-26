37e10429331b48c5fe7f1653cac945255626b2ee
Tan Dexter <dextboy@outlook.com>
2024-03-26 21:54:08 +0800

    Fix Checkstyle warning in GpaNullException

--- src/main/java/seedu/duke/exceptions/GpaNullException.java
+++ src/main/java/seedu/duke/exceptions/GpaNullException.java
@@ -1,6 +1,8 @@
 package seedu.duke.exceptions;
 
-public class GpaNullException extends Exception{
+public class GpaNullException extends Exception {
 
-    public GpaNullException(String message) {super(message);}
+    public GpaNullException(String message) {
+        super(message);
+    }
 }
