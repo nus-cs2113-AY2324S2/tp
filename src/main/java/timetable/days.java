6c2484c96910558a5aa26ab8ce858fa260f99468
Haotian199 <110802843+Haotian199@users.noreply.github.com>
2024-03-26 19:00:25 +0800

    corrected the name in Days class

--- src/main/java/timetable/days.java
+++ src/main/java/timetable/days.java
@@ -1,15 +1,30 @@
 package timetable;
 
-public class days {
+public class Days {
 
     protected String classDescription;
     protected String classTime;
+    protected String classDuration;
     protected String classLocation;
 
-    public days(String classDescription, String classTime, String classLocation){
+    public Days(String classDescription, String classTime, String classDuration, String classLocation){
         this.classDescription = classDescription;
         this.classTime = classTime;
+        this.classDuration = classDuration;
         this.classLocation = classLocation;
-
     }
+
+    public String getClassDescription() {
+        return classDescription;
+    }
+
+    public String getClassTime() {return classTime;}
+
+    public String getClassDuration() {return classDuration;}
+
+    public String getClassLocation() {
+        return classLocation;
+    }
+
+
 }
