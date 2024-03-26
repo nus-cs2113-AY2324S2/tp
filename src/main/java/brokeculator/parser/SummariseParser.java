8d03c80bcba3461d307844059959f464adda2a04
JianJiaT <tjjtjn@gmail.com>
2024-03-26 22:30:58 +0800

    Change SummariseParser to allow summarising by expense name

--- src/main/java/brokeculator/parser/SummariseParser.java
+++ src/main/java/brokeculator/parser/SummariseParser.java
@@ -4,6 +4,8 @@ import brokeculator.command.Command;
 import brokeculator.command.InvalidCommand;
 import brokeculator.command.SummariseCommand;
 
+import java.time.LocalDateTime;
+
 public class SummariseParser {
     private static final String[] SUMMARISE_COMMAND_OPTIONS = {" /n ", " /d ", " /c ", " /from ", " /to "};
     private static final int NAME_INDEX = 0;
@@ -17,55 +19,81 @@ public class SummariseParser {
 
     public static Command parseInput(String userInput) {
         String[] userInputAsArray = userInput.trim().split("\\s+");
+        String nameToSummariseBy = null;
+        LocalDateTime dateToSummariseBy = null;
+        String categoryToSummariseBy = null;
         int beginIndex = 0;
         int endIndex = -1;
-        if (userInputAsArray.length == 1) {
-            return new SummariseCommand(beginIndex, endIndex);
+
+        if (userInput.contains(SUMMARISE_COMMAND_OPTIONS[NAME_INDEX])) {
+            nameToSummariseBy = getOptionField(userInputAsArray, SUMMARISE_COMMAND_OPTIONS[NAME_INDEX]);
+            nameToSummariseBy = nameToSummariseBy.isBlank() ? null : nameToSummariseBy;
         }
-        if (userInputAsArray.length == 3) {
-            if (userInputAsArray[1].equals("/from")) {
-                try {
-                    beginIndex = Integer.parseInt(userInputAsArray[2]) - 1;
-                } catch (NumberFormatException e) {
-                    return new InvalidCommand("Start index cannot be non-integer");
-                }
-                if (beginIndex < 0) {
-                    return new InvalidCommand("Start index must be one or greater");
-                }
-                // endIndex == -1;
-                return new SummariseCommand(beginIndex, endIndex);
-            } else if (userInputAsArray[1].equals("/to")) {
-                // beginIndex = 0;
-                try {
-                    endIndex = Integer.parseInt(userInputAsArray[2]) - 1;
-                } catch (NumberFormatException e) {
-                    return new InvalidCommand("End index cannot be non-integer");
-                }
-                if (endIndex < 0) {
-                    return new InvalidCommand("End index must be one or greater");
-                }
-                return new SummariseCommand(beginIndex, endIndex);
-            }
-            return new InvalidCommand(INVALID_FORMAT_MESSAGE);
-        }
-        if (userInputAsArray.length == 5) {
-            if (!(userInputAsArray[1].equals("/from")) || !(userInputAsArray[3].equals("/to"))) {
-                return new InvalidCommand(INVALID_FORMAT_MESSAGE);
-            }
+
+        // TODO implement date processing
+
+        // TODO implement category processing
+
+        if (userInput.contains(SUMMARISE_COMMAND_OPTIONS[FROM_INDEX])) {
             try {
-                beginIndex = Integer.parseInt(userInputAsArray[2]) - 1;
-                endIndex = Integer.parseInt(userInputAsArray[4]) - 1;
+                String beginIndexAsString = getOptionField(userInputAsArray, SUMMARISE_COMMAND_OPTIONS[FROM_INDEX]);
+                beginIndexAsString = beginIndexAsString.isBlank() ? "0" : beginIndexAsString;
+                beginIndex = Integer.parseInt(beginIndexAsString.trim()) - 1;
             } catch (NumberFormatException e) {
-                return new InvalidCommand("Start and end index cannot be non-integer");
+                return new InvalidCommand("Start index cannot be non-integer");
             }
-            if (beginIndex < 0 || endIndex < 0) {
-                return new InvalidCommand("Start and end index must be one or greater");
+            if (beginIndex < 0) {
+                return new InvalidCommand("Start index must be one or greater");
             }
-            if (beginIndex > endIndex) {
-                return new InvalidCommand("Start index cannot be greater than end index");
-            }
-            return new SummariseCommand(beginIndex, endIndex);
         }
-        return new InvalidCommand(INVALID_FORMAT_MESSAGE);
+
+        if (userInput.contains(SUMMARISE_COMMAND_OPTIONS[TO_INDEX])) {
+            try {
+                String endIndexAsString = getOptionField(userInputAsArray, SUMMARISE_COMMAND_OPTIONS[TO_INDEX]);
+                endIndexAsString = endIndexAsString.isBlank() ? "0" : endIndexAsString;
+                endIndex = Integer.parseInt(endIndexAsString.trim()) - 1;
+            } catch (NumberFormatException e) {
+                return new InvalidCommand("End index cannot be non-integer");
+            }
+            if (endIndex < 0) {
+                return new InvalidCommand("End index must be one or greater");
+            }
+        }
+
+        if (endIndex != -1 && beginIndex > endIndex) {
+            return new InvalidCommand("Start index cannot be greater than end index");
+        }
+
+        return new SummariseCommand(nameToSummariseBy, dateToSummariseBy, categoryToSummariseBy,
+                beginIndex, endIndex);
+        
     }
+
+    private static String getOptionField(String[] userInputArray, String option) {
+        StringBuilder optionField = new StringBuilder();
+        boolean startAppending = false;
+        for (String s : userInputArray) {
+            if (s.equals(option.trim())) {
+                startAppending = true;
+                continue;
+            }
+            if (isWordOption(s) && startAppending) {
+                return optionField.toString().trim();
+            }
+            if (startAppending) {
+                optionField.append(s).append(" ");
+            }
+        }
+        return optionField.toString().trim();
+    }
+
+    private static boolean isWordOption(String word) {
+        for (String option : SUMMARISE_COMMAND_OPTIONS) {
+            if (word.equals(option.trim())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
