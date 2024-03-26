d687e4e35972ea3be3c240d3390a22b737533ab5
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-03-26 17:20:11 +0800

    Add attribute categoryName to enum class Category.java

--- src/main/java/budgetbuddy/categories/Category.java
+++ src/main/java/budgetbuddy/categories/Category.java
@@ -1,19 +1,21 @@
 package budgetbuddy.categories;
 
 public enum Category {
-    DINING(1),
-    GROCERIES(2),
-    UTILITIES(3),
-    TRANSPORTATION(4),
-    HEALTHCARE(5),
-    ENTERTAINMENT(6),
-    RENT(7),
-    SALARY(8),
-    OTHER(9);
+    DINING(1, "Dining"),
+    GROCERIES(2, "Groceries"),
+    UTILITIES(3, "Utilities"),
+    TRANSPORTATION(4, "Transportation"),
+    HEALTHCARE(5, "Healthcare"),
+    ENTERTAINMENT(6, "Entertainment"),
+    RENT(7, "Rent"),
+    SALARY(8, "Salary"),
+    OTHER(9, "Other");
 
     private int categoryNum;
+    private String categoryName;
 
-    Category(int categoryNum) {
+    Category(int categoryNum, String categoryName) {
         this.categoryNum = categoryNum;
+        this.categoryName = categoryName;
     }
 }
