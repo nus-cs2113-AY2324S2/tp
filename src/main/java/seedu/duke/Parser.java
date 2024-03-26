60924e6e2baa188eb8409c8f185eca038aed1dce
MonkeScripts <shaolianghe0.0@gmail.com>
2024-03-27 02:03:16 +0800

    Fixed Checkstyle again and again

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -1,7 +1,11 @@
 package seedu.duke;
 
 
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class Parser {
 
