1d4a49687d77dd27481389af592f1c1b955d6407
SharlynLui <sharlynluitm@gmail.com>
2024-03-27 16:13:50 +0800

    Edited Test Case

--- src/test/java/grocery/GroceryTest.java
+++ src/test/java/grocery/GroceryTest.java
@@ -7,14 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GroceryTest {
     @Test
     public void printGrocery_noAmountNoExpiration_leaveEmpty() {
-        Grocery grocery = new Grocery("apple", 0, null, "fruit", 0);
+        Grocery grocery = new Grocery("apple", 0, null, "fruit", 0,"Pantry");
         String message = "apple (fruit) pieces expiration date not set cost not set";
         assertEquals(message, grocery.printGrocery());
     }
 
     @Test
     public void printGrocery_costWrongFormat_formattedCost() {
-        Grocery grocery = new Grocery("chicken", 1, LocalDate.now().plusDays(1), "meat",1);
+        Grocery grocery = new Grocery("chicken", 1, LocalDate.now().plusDays(1), "meat",1,"Pantry");
         String message = "chicken (meat) " + ", amount: 1grams" + ", expiration: "
                 + LocalDate.now().plusDays(1) + ", cost: $1.00";
         assertEquals(message, grocery.printGrocery());
@@ -22,7 +22,7 @@ class GroceryTest {
 
     @Test
     public void printGrocery_correctAmtAndExpAndCost() {
-        Grocery grocery = new Grocery("chicken", 1, LocalDate.now().plusDays(1), "meat",1.20);
+        Grocery grocery = new Grocery("chicken", 1, LocalDate.now().plusDays(1), "meat",1.20,"Pantry");
         String message = "chicken (meat) " + ", amount: 1grams" + ", expiration: "
                 + LocalDate.now().plusDays(1) + ", cost: $1.20";
         assertEquals(message, grocery.printGrocery());
