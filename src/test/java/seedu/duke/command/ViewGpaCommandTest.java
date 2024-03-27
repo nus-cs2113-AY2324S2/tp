4b46cfbe9ae3eccf42a87a96fb9776211b8e8ce6
fungg0 <fungfunwenwu@gmail.com>
2024-03-27 16:02:39 +0800

    Amendments to JUnit test

--- src/test/java/seedu/duke/command/ViewGpaCommandTest.java
+++ src/test/java/seedu/duke/command/ViewGpaCommandTest.java
@@ -1,11 +1,17 @@
 package seedu.duke.command;
 
+import seedu.duke.modules.Module;
 import seedu.duke.modules.ModuleList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ViewGpaCommandTest {
-    ModuleList testList = new ModuleList(0);
-    
+    public void viewGpaCommandTest() {
+        ModuleList testList = new ModuleList(0);
+        testList.addModule(new Module("CG2111A", 4, true,2));
+        testList.addModule(new Module("CG1111A", 4, true,1));
+
+
+    }
 
 }
