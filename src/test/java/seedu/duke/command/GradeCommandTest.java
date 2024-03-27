ff73e89039517fd1444f2ec0ed32e86f5cee9556
fungg0 <fungfunwenwu@gmail.com>
2024-03-27 15:47:14 +0800

    Create classes for JUnit test for grades and GPA

---
+++ src/test/java/seedu/duke/command/GradeCommandTest.java
@@ -0,0 +1,12 @@
+package seedu.duke.command;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GradeCommandTest {
+
+    @Test
+    void execute() {
+    }
+}
---
+++ src/test/java/seedu/duke/command/ViewGpaCommandTest.java
@@ -0,0 +1,11 @@
+package seedu.duke.command;
+
+import seedu.duke.modules.ModuleList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ViewGpaCommandTest {
+    ModuleList testList = new ModuleList(0);
+    
+
+}
