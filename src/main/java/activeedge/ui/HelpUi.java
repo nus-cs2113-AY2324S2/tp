313e5a14c2bbf6450af85284c8fd65dba05b2777
SuveenE <suveen.te1@gmail.com>
2024-03-27 20:43:40 +0800

    Fix: indentation error

--- src/main/java/activeedge/ui/HelpUi.java
+++ src/main/java/activeedge/ui/HelpUi.java
@@ -12,21 +12,20 @@ public class HelpUi {
      */
     public static void printHelpMessage() {
         System.out.println(" Welcome to Active Edge! Here are the available commands:\n" +
-                "\n" +
                 "Log meals: log m/FOOD s/NUMBER_OF_SERVINGS\n" +
-        "View daily calories: show c\n" +
-        "Log water: log w/VOLUME_OF_WATER\n" +
-        "View water intake: show w\n" +
-        "Set daily calorie goal: set goal c/CALORIE_GOAL\n" +
-        "Set daily water goal: set goal w/WATER_GOAL\n" +
-        "View goals: show g\n" +
-        "Log exercise: log e/EXERCISE_NAME d/DURATION\n" +
-        "View exercises: list exercises\n" +
-        "Help: help\n" +
-        "Find: find 'keyword'\n" +
-        "Delete items from list: delete 'Quantity of water/Food name'\n" +
-        "List calorie and water intake: list\n" +
-        "Clear: clear\n" +
-        "Start tracking your health goals now! If you have any questions, feel free to ask." );
+                "View daily calories: show c\n" +
+                "Log water: log w/VOLUME_OF_WATER\n" +
+                "View water intake: show w\n" +
+                "Set daily calorie goal: set goal c/CALORIE_GOAL\n" +
+                "Set daily water goal: set goal w/WATER_GOAL\n" +
+                "View goals: show g\n" +
+                "Log exercise: log e/EXERCISE_NAME d/DURATION\n" +
+                "View exercises: list exercises\n" +
+                "Help: help\n" +
+                "Find: find 'keyword'\n" +
+                "Delete items from list: delete 'Quantity of water/Food name'\n" +
+                "List calorie and water intake: list\n" +
+                "Clear: clear\n" +
+                "Start tracking your health goals now! If you have any questions, feel free to ask." );
     }
 }
