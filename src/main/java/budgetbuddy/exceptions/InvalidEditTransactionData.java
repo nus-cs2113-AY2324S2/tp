2ae267fb22396af56d6f798a915c6cb2d2791e1f
Vavinan <vavithamarai2000@gmail.com>
2024-03-27 22:51:08 +0800

    Add InvalidEditTransactionData exception

---
+++ src/main/java/budgetbuddy/exceptions/InvalidEditTransactionData.java
@@ -0,0 +1,7 @@
+package budgetbuddy.exceptions;
+
+public class InvalidEditTransactionData extends Exception{
+    public InvalidEditTransactionData(String message) {
+        super(message);
+    }
+}
