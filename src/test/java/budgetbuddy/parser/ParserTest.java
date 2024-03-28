ca8f45513071ecff23a9f31a5706689798af1c93
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-03-28 10:59:54 +0800

    Update ParserTest.java with new changes

--- src/test/java/budgetbuddy/parser/ParserTest.java
+++ src/test/java/budgetbuddy/parser/ParserTest.java
@@ -17,10 +17,10 @@ public class ParserTest {
         Parser parser = new Parser();
         Account account = new Account();
         Transaction transaction = parser.parseTransaction("add /t/Income /n/Shopping /$/50 /d/14-03-2024 " +
-                "/c/Personal", account);
+                "/c/1", account);
         assertEquals("Shopping", transaction.getDescription());
         assertEquals(50.0f, transaction.getAmount(), 0.001);
         assertEquals(LocalDate.parse("14-03-2024", DateTimeFormatter.ofPattern("dd-MM-yyyy")), transaction.getDate());
-        assertEquals("Personal", transaction.getCategory().getCategoryName());
+        assertEquals("Dining", transaction.getCategory().getCategoryName());
     }
 }
