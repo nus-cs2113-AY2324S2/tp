8b3a57ede6a229c8afa6cc8663bc90e06413ac17
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-03-28 10:47:58 +0800

    Update JUnit test cases to match new changes in Category

--- src/test/java/budgetbuddy/transaction/type/TransactionTest.java
+++ src/test/java/budgetbuddy/transaction/type/TransactionTest.java
@@ -1,6 +1,7 @@
 package budgetbuddy.transaction.type;
 
 import budgetbuddy.account.Account;
+import budgetbuddy.categories.Category;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -20,39 +21,39 @@ public class TransactionTest {
 
     @Test
     public void testTransactionConstructor() {
-        Transaction transaction = new Income("Groceries", 50.0f, "Food", "14-03-2024",
+        Transaction transaction = new Income("Groceries", 50.0f, "14-03-2024",
                 new Account());
         assertEquals("Groceries", transaction.getDescription());
         assertEquals(50.0f, transaction.getAmount(), 0.001);
-        assertEquals("Food", transaction.getCategory().getCategoryName());
         LocalDate date = LocalDate.parse("14-03-2024", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         assertEquals(date, transaction.getDate());
     }
 
     @Test
     public void testGetDescription() {
-        Transaction transaction = new Income("Groceries", 50.0f, "Food", "14-03-2024",
+        Transaction transaction = new Income("Groceries", 50.0f, "14-03-2024",
                 account);
         assertEquals("Groceries", transaction.getDescription());
     }
 
     @Test
     public void testGetAmount() {
-        Transaction transaction = new Income("Groceries", 50.0f, "Food", "14-03-2024",
+        Transaction transaction = new Income("Groceries", 50.0f, "14-03-2024",
                 account);
         assertEquals(50.0f, transaction.getAmount(), 0.001);
     }
 
     @Test
     public void testGetCategory() {
-        Transaction transaction = new Income("Groceries", 50.0f, "Food", "14-03-2024",
+        Transaction transaction = new Income("Groceries", 50.0f, "14-03-2024",
                 account);
-        assertEquals("Food", transaction.getCategory().getCategoryName());
+        transaction.setCategory(Category.fromNumber(1));
+        assertEquals("Dining", transaction.getCategory().getCategoryName());
     }
 
     @Test
     public void testGetDate() {
-        Transaction transaction = new Income("Groceries", 50.0f, "Food", "14-03-2024",
+        Transaction transaction = new Income("Groceries", 50.0f, "14-03-2024",
                 account);
         LocalDate date = LocalDate.parse("14-03-2024", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         assertEquals(date, transaction.getDate());
@@ -60,10 +61,11 @@ public class TransactionTest {
 
     @Test
     public void testToString() {
-        Transaction transaction = new Income("Groceries", 50.0f, "Food", "14-03-2024",
+        Transaction transaction = new Income("Groceries", 50.0f, "14-03-2024",
                 account);
+        transaction.setCategory(Category.fromNumber(1));
         String expected = " Transaction Type: Income |  Description: Groceries |  Date: 2024-03-14 |  Amount: 50.0 | " +
-                " Category: Food";
+                " Category: Dining";
         assertEquals(expected, transaction.toString());
     }
 }
